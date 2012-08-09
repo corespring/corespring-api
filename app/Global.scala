8b05bc12aa8ce070e1950f0a583810155ddf090d
bleezmo <joshuarogers10@gmail.com>
2012-08-09 15:02:22 -0400

    added orgs.json and collections.json for mongoimport

--- app/Global.scala
+++ app/Global.scala
@@ -1,3 +1,5 @@
+import _root_.controllers.auth.Permission
+import _root_.controllers.services.OrgService
 import _root_.models.{ContentCollection, Organization}
 import com.mongodb.casbah.commons.MongoDBObject
 import org.bson.types.ObjectId
@@ -9,21 +11,21 @@ object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
     if ( Play.isDev(app) ) {
-      Logger.info("Loading data for development")
-      val parentId = new ObjectId("5019921244ae551130b4b28e")
-      val childId = new ObjectId("5019921244ae551130b4b28f")
-      if ( Organization.findOneById(parentId).isEmpty) {
-        val o = Organization(id = parentId, name = "Acme")
-        Organization.insert(o,None)
-      }
-
-      if ( Organization.findOneById(childId).isEmpty ) {
-        val c = Organization(id = childId, name = "Branch 1")
-        Organization.insert(c,Some(parentId))
-      }
-
-      Logger.info("Acme id = %s".format(parentId.toString))
-      Logger.info("Branch id = %s".format(childId.toString))
+//      Logger.info("Loading data for development")
+//      val parentId = new ObjectId("5019921244ae551130b4b28e")
+//      val childId = new ObjectId("5019921244ae551130b4b28f")
+//      if ( Organization.findOneById(parentId).isEmpty) {
+//        val o = Organization(id = parentId, name = "Acme")
+//        Organization.insert(o,None)
+//      }
+//
+//      if ( Organization.findOneById(childId).isEmpty ) {
+//        val c = Organization(id = childId, name = "Branch 1")
+//        Organization.insert(c,Some(parentId))
+//      }
+//
+//      Logger.info("Acme id = %s".format(parentId.toString))
+//      Logger.info("Branch id = %s".format(childId.toString))
     }
   }
 }
