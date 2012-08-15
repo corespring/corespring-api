0440ec2e38c73d5611e4e0b126e936eb5612c41d
bleezmo <joshuarogers10@gmail.com>
2012-08-15 13:52:05 -0400

    i think i've finally fixed it

--- runtask.sh
+++ runtask.sh
@@ -11,10 +11,9 @@ else
         else
             if [[ ":$PATH:" = *":.sbt_home/bin:"* ]]; then
                 #HEROKU FIX: check if the path to sbt is .sbt_home/bin. if it is, add ~/.sbt_home/bin to path to make sbt executable from any folder
-                export PATH=${PATH}:~/.sbt_home/bin;
-                command -v sbt;
+                command -v ~/.sbt_home/sbt;
                 if [ "$?" = "0" ]; then
-                    sbt run
+                    ~/.sbt_home/sbt run
                 else
                     echo "error: could not run sbt. sbt command unavailable"
                 fi
