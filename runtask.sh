9fe9e4028861890b437ed38d8b1c901ecb86ade2
bleezmo <joshuarogers10@gmail.com>
2012-08-15 13:13:07 -0400

    added logic to runtask.sh to check for heroku's retarded path variable

--- runtask.sh
+++ runtask.sh
@@ -2,10 +2,27 @@
 if [ -z "$1" ]; then
     echo "you must supply a task to execute"
 else
+    #change directory to the task given then run sbt
     cd tasks/$1
     if [ "$?" = "0" ]; then
-        sbt run
+        command -v sbt
+        if [ "$?" = "0" ]; then
+            sbt run
+        else
+            if [[ ":$PATH:" = *":.sbt_home/bin:"* ]]; then
+                #HEROKU FIX: check if the path to sbt is .sbt_home/bin. if it is, add ~/.sbt_home/bin to path to make sbt executable from any folder
+                export PATH=${PATH}:~/.sbt_home/bin
+                command -v sbt
+                if [ "$?" = "0" ]; then
+                    sbt run
+                else
+                    echo "error: could not run sbt. sbt command unavailable"
+                fi
+            else
+                echo "error: could not run sbt. sbt command unavailable"
+            fi
+        fi
     else
-        echo "error: task $1 not run"
+        echo "error: task $1 not run because the directory tasks/$1 could not be found"
     fi
 fi
