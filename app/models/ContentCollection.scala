4afcccb1506064a875b308a1374ce139d7698ddc
bleezmo <joshuarogers10@gmail.com>
2012-08-24 17:09:36 -0400

    fixed collection serialization

--- app/models/ContentCollection.scala
+++ app/models/ContentCollection.scala
@@ -124,7 +124,7 @@ object ContentCollection extends ModelCompanion[ContentCollection, ObjectId] {
   implicit object CollectionWrites extends Writes[ContentCollection] {
     def writes(coll: ContentCollection) = {
       var list = List[(String, JsString)]()
-      if ( coll.name.nonEmpty ) ("name" -> JsString(coll.name)) :: list
+      list = ("name" -> JsString(coll.name)) :: list
       list = ("id" -> JsString(coll.id.toString)) :: list
       JsObject( list )
     }
