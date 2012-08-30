fb65882123b610e3c64ab9bf97abdf0f6bad655d
edeustace <ed.eustace@gmail.com>
2012-08-30 16:11:05 +0200

    moved some of the partials around so i can use relative naming

--- app/web/controllers/Partials.scala
+++ app/web/controllers/Partials.scala
@@ -6,7 +6,7 @@ import web.models.QtiTemplate
 object Partials extends Controller{
 
   def itemCollection = Action{ Ok(web.views.html.partials.itemCollection( QtiTemplate.all() )) }
-  def editMetadata = Action{ Ok(web.views.html.partials.editMetadata() ) }
+  def editMetadata = Action{ Ok(web.views.html.partials.editItem() ) }
   def createItem = Action{ Ok(web.views.html.partials.createItem() ) }
   def home = Action{ Ok(web.views.html.partials.home() )}
   def viewItem = Action{ Ok(web.views.html.partials.viewItem() )}
