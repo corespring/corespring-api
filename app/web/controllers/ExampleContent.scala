2c24858defa985eade95bc62920ee49066dfc983
edeustace <ed.eustace@gmail.com>
2012-09-20 13:54:22 +0200

    Some tidy up

--- app/web/controllers/ExampleContent.scala
+++ app/web/controllers/ExampleContent.scala
@@ -1,12 +1,8 @@
 package web.controllers
 
-import org.bson.types.ObjectId
 import play.api.mvc.{Action, Controller}
 import models.{Item, ContentCollection, Content}
 
-import com.mongodb.casbah.commons.MongoDBObject
-import api.QueryHelper
-import com.mongodb.casbah.commons.ValidBSONType.BasicDBObject
 import com.mongodb.BasicDBObject
 import play.api.libs.json.Json
 import com.novus.salat.dao.SalatMongoCursor
@@ -18,26 +14,20 @@ object ExampleContent extends Controller {
       Ok(web.views.html.exampleContent())
   }
 
+  val EXAMPLE_CONTENT_COLLECTION_NAME = "Beta Items"
+
   def items = Action {
     request =>
-
       val search = new BasicDBObject()
-      search.put("name", "Beta Items")
+      search.put("name", EXAMPLE_CONTENT_COLLECTION_NAME)
       ContentCollection.findOne(search) match {
         case Some(contentCollection) => {
-          println("contentCollection: " + contentCollection)
-          println("contentCollection: " + contentCollection.id.toString)
           val itemSearch = new BasicDBObject()
-
           itemSearch.put("collectionId", contentCollection.id.toString)
           val out = new BasicDBObject()
           out.put("title", 1)
-          out.put("collectionId", 1)
-
           Item.find(itemSearch, out) match {
-
             case cursor: SalatMongoCursor[Item] => {
-              println(cursor)
               Ok(Json.toJson(cursor.toList))
             }
             case _ => NotFound
