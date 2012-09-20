f94e20632e47de7041563d6e60339366f9115186
Attila Miklosi <attila.miklosi@gmail.com>
2012-09-20 09:54:01 +0100

    Redone tabs directive based on the angularjs-bootstrap tab component. Transcluding now works, tabs can contain further angular directives

--- public/js/corespring/qti/tabs-print.js
+++ public/js/corespring/qti/tabs-print.js
@@ -1,46 +1,24 @@
-qtiDirectives.directive("tabs", function() {
-        var tabTemplate = [
-                        '<div class="tab-panel">',
-                        '<div ng:repeat="item in items" class="tab-box" id="tab{{$index}}">',
-                            '<div class="tab-header">{{item.tabTitle}}</div>',
-                            '<div class="paragraph-style">',
-                            '{{item.content}}',
-                        '</div></div></div>',
-                        ''].join('');
-
-
-    return {
+qtiDirectives.directive('csTabs', function() {
+      return {
         restrict: 'E',
-        replace: true,
-        scope: true,
-        transclude: false,
-        controller: function($scope, $element, $attrs) {
-            var scope = $scope;
-            scope.selectedIndex = 0;
+        transclude: true,
+        controller: function($scope, $element, $attrs) {},
+        template: '<div class="tab-panel"><div ng-transclude /></div>'
 
-        },
-        compile: function(tElement, tAttrs, transclude) {
-            var tabs = [];
-            var elements = [];
-            console.log('compile bla function');
-            var tabElements = angular.element(tElement).find("tab");
-            for (var i = 0; i < tabElements.length; i++)  {
-                var elem = angular.element(tabElements[i]);
-                var tabTitle = elem.attr('name');
-                tabs.push({content: elem.html(), tabTitle: tabTitle});
-                elements.push(tabElements[i]);
-            }
-
-            // now modify the DOM
-            tElement.html(tabTemplate);
-
-            // linking function
-            return function(scope, element, attrs) {
-                scope.items = tabs;
-                scope.onClick = function(idx) {
-                    scope.selectedIndex = idx;
-                }
-            };
-        }
+      };
     }
-});
+  );
+
+qtiDirectives.directive('csTab', function() {
+      return {
+        restrict: 'E',
+        transclude: true,
+        require: '^csTabs',
+        link: function(scope, elm, attrs, container) {
+                scope.title = attrs['title'];
+              },
+        scope: true,
+        template: "<div class=\"tab-title\">{{title}}</div><div class=\"tab-box\" ng-transclude></div>"
+      };
+    }
+);
--- public/js/corespring/qti/tabs.js
+++ public/js/corespring/qti/tabs.js
@@ -1,50 +1,118 @@
-qtiDirectives.directive("tabs", function() {
-        var tabTemplate = [
-                        '<div class="tab-panel">',
-                        '<div class="tabs"><ul>',
-                            '<li ng:repeat="item in items" obj="{{item}}"> ',
-                            '<a href="#tab{{$index}}" ng-class="{active: $index==selectedIndex}" ng-click="onClick($index)">{{item.tabTitle}}</a>',
-                            '</li>',
-                        '</div>',
-                        '<div ng:repeat="item in items" class="tab-box" id="tab{{$index}}" ng-show="selectedIndex == $index">',
-                            '<div class="paragraph-style">',
-                            '{{item.content}}',
-                        '</div></div></div>',
-                        ''].join('');
-
-
-    return {
-        restrict: 'E',
-        replace: true,
-        scope: true,
-        transclude: false,
-        controller: function($scope, $element, $attrs) {
-            var scope = $scope;
-            scope.selectedIndex = 0;
-
-        },
-        compile: function(tElement, tAttrs, transclude) {
-            var tabs = [];
-            var elements = [];
-            console.log('compile bla function');
-            var tabElements = angular.element(tElement).find("tab");
-            for (var i = 0; i < tabElements.length; i++)  {
-                var elem = angular.element(tabElements[i]);
-                var tabTitle = elem.attr('name');
-                tabs.push({content: elem.html(), tabTitle: tabTitle});
-                elements.push(tabElements[i]);
-            }
-
-            // now modify the DOM
-            tElement.html(tabTemplate);
-
-            // linking function
-            return function(scope, element, attrs) {
-                scope.items = tabs;
-                scope.onClick = function(idx) {
-                    scope.selectedIndex = idx;
-                }
-            };
+qtiDirectives.directive('csTabs', [
+    '$timeout', function($timeout) {
+      var controllerFn;
+      controllerFn = function($scope, $element, $attrs) {
+        var _this = this;
+        
+        if ($attrs['ngShow']) {
+          $scope.$watch($attrs['ngShow'], function(newValue, oldValue) {
+            return _this.tabsAreVisible = newValue;
+          });
+        } else {
+          this.tabsAreVisible = true;
         }
+        $scope.tabs = [];
+        $scope.$watch('tabs.length', function(tabsL, oldL) {
+          if (tabsL > 0 && tabsL < oldL) {
+            if ($scope.tabs.indexOf($scope.selectedTab === -1)) {
+              return $scope.selectTab($scope.tabs[Math.max($scope.selectedIdx - 1, 0)]);
+            }
+          }
+        });
+        $scope.selectTab = function(tab, $event) {
+          var _i, _len, _ref, _tab;
+          if (!(tab != null)) {
+            return;
+          }
+          _ref = $scope.tabs;
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            _tab = _ref[_i];
+            _tab.selected(false);
+          }
+          $timeout(function() {
+            return tab.selected(true);
+          });
+          $scope.selectedTab = tab;
+          $scope.selectedIdx = $scope.tabs.indexOf(tab);
+          if ($scope.onTabSelect != null) {
+            $scope.onTabSelect(tab);
+          }
+
+          $event.preventDefault();
+          $event.stopPropagation();
+          return null;
+        };
+        $scope.changeTab = function(index) {
+          try {
+            return $scope.selectTab($scope.tabs[index]);
+          } catch (e) {
+            console.error("could not change tab, probably array out of bounds");
+            throw e;
+          }
+        };
+        this.addTab = function(tab, index) {
+          $scope.tabs.push(tab);
+          if ($scope.tabs.length === 1) {
+            return $scope.selectTab(tab);
+          }
+        };
+        this.removeTab = function(tab) {
+          return $timeout(function() {
+            return $scope.tabs.splice($scope.tabs.indexOf(tab, 1));
+          });
+        };
+        this.nextTab = function() {
+          var newIdx;
+          newIdx = $scope.selectedIdx + 1;
+          return $scope.changeTab(newIdx);
+        };
+        return this.previousTab = function() {
+          var newIdx;
+          newIdx = $scope.selectedIdx - 1;
+          return $scope.changeTab(newIdx);
+        };
+      };
+      return {
+        restrict: 'E',
+        transclude: true,
+        controller: controllerFn,
+        template: ['<div class="tab-panel">',
+                    '<div class="tabs">',
+                    '<ul><li ng-repeat="tab in tabs" >',
+                    '<a href="" ng-class="{active: tab.selected()}" ng-click="selectTab(tab, $event)">{{tab.title}}</a>',
+                    '</li></ul></div><div class="tab-box" ng-transclude></div></div>'].join('')
+
+      };
     }
-});
+  ]);
+
+qtiDirectives.directive('csTab', [
+    function() {
+      var linkFn, nextTab;
+      nextTab = 0;
+      linkFn = function(scope, elm, attrs, container) {
+        var tab;
+        tab = {
+          title: attrs['title'],
+          selected: function(newVal) {
+            if (newVal == null) {
+              return scope.selected;
+            }
+            return scope.selected = newVal;
+          }
+        };
+        container.addTab(tab);
+        return scope.$on('$destroy', function() {
+          return container.removeTab(tab);
+        });
+      };
+      return {
+        restrict: 'E',
+        transclude: true,
+        require: '^csTabs',
+        link: linkFn,
+        scope: true,
+        template: "<div class=\"paragraph-style\" ng-class=\"{active:selected}\" ng-show=\"selected\" ng-transclude></div>"
+      };
+    }
+  ]);
