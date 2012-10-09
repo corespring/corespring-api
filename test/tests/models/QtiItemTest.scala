cb8df9b5880337a6910e5d9cd2bfd40034abf1ed
edeustace <ed.eustace@gmail.com>
2012-10-09 13:45:36 +0200

    Added support for correct responses with a 'single' cardinality but with multiple values

---
+++ test/tests/models/QtiItemTest.scala
@@ -0,0 +1,40 @@
+package tests.models.bleezmo
+
+import org.specs2.mutable.Specification
+import models.bleezmo.{CorrectResponseMultiple, QtiItem}
+import scala.Some
+
+class QtiItemTest extends Specification{
+
+
+  "QtiItem" should {
+    "parse a response that has a single cardinality but has multiple values as a CorrectResponseMutliple" in {
+
+      val SINGLE_RESPONSE_WITH_MULTIPLE_VALUES = <assessmentItem >
+        <responseDeclaration identifier="Q_01" cardinality="single" baseType="string">
+          <correctResponse>
+            <value>14</value>
+            <value>fourteen</value>
+            <value>14.0</value>
+            <value>Fourteen</value>
+            <value>FOURTEEN</value>
+          </correctResponse>
+        </responseDeclaration>
+        <itemBody>
+           <textEntryInteraction responseIdentifier="Q_01" expectedLength="5"/>
+          <feedbackBlock
+          outcomeIdentifier="responses.Q_01.value"
+          identifier="fourteen">
+            <div class="feedback-block-correct">Nice work, that's correct!</div>
+          </feedbackBlock>
+        </itemBody>
+      </assessmentItem>
+
+      val item = QtiItem(SINGLE_RESPONSE_WITH_MULTIPLE_VALUES)
+      item.responseDeclarations.size must equalTo(1)
+      val response = item.responseDeclarations(0).correctResponse.get
+      response.isInstanceOf[CorrectResponseMultiple] must be equalTo(true)
+    }
+  }
+
+}
