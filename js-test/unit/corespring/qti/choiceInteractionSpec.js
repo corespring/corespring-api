e4dd1541b6f4d556637a0603bb4d3eb55abae93e
edeustace <ed.eustace@gmail.com>
2012-10-12 15:38:03 +0200

    try with dot notation on describe

--- js-test/unit/corespring/qti/choiceInteractionSpec.js
+++ js-test/unit/corespring/qti/choiceInteractionSpec.js
@@ -1,74 +1,80 @@
-describe('qtiDirectives', function () {
-    describe('choiceinteraction', function () {
-        'use strict';
+describe('qtiDirectives.choiceinteraction', function () {
+    'use strict';
 
-        var prepareBackend = function ($backend) {
+    var prepareBackend = function ($backend) {
 
-            var urls = [
-                {
-                    method:'GET',
-                    url:'/api/v1/items/itemId/sessions/itemSessionId?access_token=34dj45a769j4e1c0h4wb',
-                    response:{"id":"itemSessionId", "itemId":"itemId", "start":1349970769197, "responses":[]}
-                }
-            ];
-
-            for (var i = 0; i < urls.length; i++) {
-                var definition = urls[i];
-                $backend.when(definition.method, definition.url).respond(200, definition.response);
+        var urls = [
+            {
+                method:'GET',
+                url:'/api/v1/items/itemId/sessions/itemSessionId?access_token=34dj45a769j4e1c0h4wb',
+                response:{"id":"itemSessionId", "itemId":"itemId", "start":1349970769197, "responses":[]}
             }
-        };
+        ];
 
-        var helper = new com.qti.helpers.QtiHelper();
+        for (var i = 0; i < urls.length; i++) {
+            var definition = urls[i];
+            $backend.when(definition.method, definition.url).respond(200, definition.response);
+        }
+    };
 
-        var wrap = function (content) {
-            return helper.wrap(content);
-        };
+    var helper = new com.qti.helpers.QtiHelper();
 
-
-        var getInteraction = function () {
-
-            var node = ['<choiceInteraction responseIdentifier="question" maxChoices="1">',
-                '<simpleChoice identifier="a">A</simpleChoice>',
-                '<simpleChoice identifier="b">B</simpleChoice>',
-                '</choiceInteraction>'
-            ].join("\n");
-
-            return compileAndGetScope(rootScope, compile, node);
-        };
-
-        var compileAndGetScope = function ($rootScope, $compile, node) {
-            var element = $compile(wrap(node))($rootScope);
-            console.log("element: " + element.html());
-            return { element:element.children(), scope:$rootScope.$$childHead};
-        };
+    var wrap = function (content) {
+        return helper.wrap(content);
+    };
 
 
-        beforeEach(module('qti'));
+    var basicNode = ['<choiceInteraction responseIdentifier="question" maxChoices="${maxChoices}">',
+        '<simpleChoice identifier="a">A</simpleChoice>',
+        '<simpleChoice identifier="b">B</simpleChoice>',
+        '</choiceInteraction>'
+    ].join("\n");
 
-        var rootScope, compile;
+    var getInteraction = function (node) {
 
-        beforeEach(inject(function ($compile, $rootScope, _$httpBackend_) {
-            prepareBackend(_$httpBackend_);
-            rootScope = $rootScope.$new();
-            compile = $compile;
-        }));
+        node = (node || basicNode.replace("${maxChoices}", 1));
 
-        describe("compilation", function () {
-            it('inits correctly', function () {
-                var interaction = getInteraction();
-                expect(interaction.scope).not.toBe(null);
+        return compileAndGetScope(rootScope, compile, node);
+    };
 
-                var element = interaction.element;
+    var compileAndGetScope = function ($rootScope, $compile, node) {
+        var element = $compile(wrap(node))($rootScope);
+        console.log("element: " + element.html());
+        return { element:element.children(), scope:$rootScope.$$childHead};
+    };
 
-                expect(element.find('simplechoice').length).toBe(2);
-                expect(element.find('input').attr('type')).toBe('radio');
-            });
+
+    beforeEach(module('qti'));
+
+    var rootScope, compile;
+
+    beforeEach(inject(function ($compile, $rootScope, _$httpBackend_) {
+        prepareBackend(_$httpBackend_);
+        rootScope = $rootScope.$new();
+        compile = $compile;
+    }));
+
+    describe("compilation", function () {
+        it('inits checkboxes correctly', function () {
+            var interaction = getInteraction(basicNode.replace("${maxChoices}", "0"));
+            expect(interaction.scope).not.toBe(null);
+            var element = interaction.element;
+            expect(element.find('simplechoice').length).toBe(2);
+            expect(element.find('input').attr('type')).toBe('checkbox');
         });
 
-        describe("behaviour", function () {
-            it('dummy', function () {
-                expect(true).toBe(true);
-            });
+        it('inits radios correctly', function () {
+            var interaction = getInteraction();
+            expect(interaction.scope).not.toBe(null);
+            var element = interaction.element;
+            expect(element.find('simplechoice').length).toBe(2);
+            expect(element.find('input').attr('type')).toBe('radio');
+        });
+    });
+
+    describe("behaviour", function () {
+        it('dummy', function () {
+            expect(true).toBe(true);
         });
     });
 });
--- js-test/unit/corespring/qti/textEntryInteractionSpec.js
+++ js-test/unit/corespring/qti/textEntryInteractionSpec.js
@@ -1,136 +1,134 @@
-describe('qtiDirectives', function () {
-    describe('textentryinteraction', function () {
-        'use strict';
+describe('qtiDirectives.textentryinteraction', function () {
+    'use strict';
 
-        var prepareBackend = function ($backend) {
+    var prepareBackend = function ($backend) {
 
-            var urls = [
-                {
-                    method:'GET',
-                    url:'/api/v1/items/itemId/sessions/itemSessionId?access_token=34dj45a769j4e1c0h4wb',
-                    response:{"id":"itemSessionId", "itemId":"itemId", "start":1349970769197, "responses":[]}
-                }
-            ];
-
-            for (var i = 0; i < urls.length; i++) {
-                var definition = urls[i];
-                $backend.when(definition.method, definition.url).respond(200, definition.response);
+        var urls = [
+            {
+                method:'GET',
+                url:'/api/v1/items/itemId/sessions/itemSessionId?access_token=34dj45a769j4e1c0h4wb',
+                response:{"id":"itemSessionId", "itemId":"itemId", "start":1349970769197, "responses":[]}
             }
-        };
+        ];
 
-        var helper = new com.qti.helpers.QtiHelper();
+        for (var i = 0; i < urls.length; i++) {
+            var definition = urls[i];
+            $backend.when(definition.method, definition.url).respond(200, definition.response);
+        }
+    };
 
-        var wrap = function (content) {
-            return helper.wrap(content);
-        };
+    var helper = new com.qti.helpers.QtiHelper();
+
+    var wrap = function (content) {
+        return helper.wrap(content);
+    };
 
 
-        var getInteraction = function () {
+    var getInteraction = function () {
+        var node = '<textentryinteraction responseIdentifier="rid" expectedLength="1"/>';
+        return compileAndGetScope(rootScope, compile, node);
+    };
+
+    var compileAndGetScope = function ($rootScope, $compile, node) {
+        var element = $compile(wrap(node))($rootScope);
+
+        console.log("element: " + element.html());
+        return { element:element.children(), scope:$rootScope.$$childHead};
+    };
+
+
+    beforeEach(module('qti'));
+
+    var rootScope, compile;
+
+    beforeEach(inject(function ($compile, $rootScope, _$httpBackend_) {
+        prepareBackend(_$httpBackend_);
+        rootScope = $rootScope.$new();
+        compile = $compile;
+    }));
+
+    describe("compilation", function () {
+
+        it('inits correctly', function () {
             var node = '<textentryinteraction responseIdentifier="rid" expectedLength="1"/>';
-            return compileAndGetScope(rootScope, compile, node);
-        };
-
-        var compileAndGetScope = function ($rootScope, $compile, node) {
-            var element = $compile(wrap(node))($rootScope);
-
-            console.log("element: " + element.html());
-            return { element:element.children(), scope:$rootScope.$$childHead};
-        };
-
-
-        beforeEach(module('qti'));
-
-        var rootScope, compile;
-
-        beforeEach(inject(function ($compile, $rootScope, _$httpBackend_) {
-            prepareBackend(_$httpBackend_);
-            rootScope = $rootScope.$new();
-            compile = $compile;
-        }));
-
-        describe("compilation", function () {
-
-            it('inits correctly', function () {
-                var node = '<textentryinteraction responseIdentifier="rid" expectedLength="1"/>';
-                var interaction = compileAndGetScope(rootScope, compile, node);
-                expect(interaction.scope.expectedLength).toBe('1');
-            });
-
+            var interaction = compileAndGetScope(rootScope, compile, node);
+            expect(interaction.scope.expectedLength).toBe('1');
         });
 
-        describe("behaviour", function () {
-            it('interacts with controller', function () {
-                var node = '<textentryinteraction responseIdentifier="rid" expectedLength="1"/>';
-                var interaction = compileAndGetScope(rootScope, compile, node);
-                var scope = interaction.scope;
-                var response = "here's a response";
+    });
 
-                scope.$apply(function () {
-                    scope.textResponse = response;
-                });
+    describe("behaviour", function () {
+        it('interacts with controller', function () {
+            var node = '<textentryinteraction responseIdentifier="rid" expectedLength="1"/>';
+            var interaction = compileAndGetScope(rootScope, compile, node);
+            var scope = interaction.scope;
+            var response = "here's a response";
 
-                expect(scope.textResponse).toBe(response);
-                expect(scope.controller.findItemByKey("rid").value).toBe(response);
+            scope.$apply(function () {
+                scope.textResponse = response;
             });
 
-            it('shows/hides no response feedback', function () {
-                var node = '<textentryinteraction responseIdentifier="rid" expectedLength="1"/>';
-                var interaction = compileAndGetScope(rootScope, compile, node);
-                var scope = interaction.scope;
-                scope.textResponse = "";
-                scope.$apply(function () {
-                    scope.showNoResponseFeedback = true;
-                });
+            expect(scope.textResponse).toBe(response);
+            expect(scope.controller.findItemByKey("rid").value).toBe(response);
+        });
 
-                expect(scope.noResponse).toBe(true);
+        it('shows/hides no response feedback', function () {
+            var node = '<textentryinteraction responseIdentifier="rid" expectedLength="1"/>';
+            var interaction = compileAndGetScope(rootScope, compile, node);
+            var scope = interaction.scope;
+            scope.textResponse = "";
+            scope.$apply(function () {
+                scope.showNoResponseFeedback = true;
             });
 
-            it('updates the ui on response received', function () {
-                var interaction = getInteraction();
-                console.log("itemSession: " + rootScope.itemSession);
+            expect(scope.noResponse).toBe(true);
+        });
 
-                var element = interaction.element;
-                var scope = interaction.scope;
+        it('updates the ui on response received', function () {
+            var interaction = getInteraction();
+            console.log("itemSession: " + rootScope.itemSession);
 
-                scope.$apply(function () {
-                    scope.textResponse = "correct";
-                });
+            var element = interaction.element;
+            var scope = interaction.scope;
 
-                rootScope.$apply(function () {
-                    rootScope.itemSession.sessionData = {};
-                    rootScope.itemSession.sessionData.correctResponses = { rid:"correct" }
-                });
-
-                expect(element.attr('class').contains(scope.CSS.correct)).toBe(true);
-
-                scope.$apply(function () {
-                    scope.textResponse = "incorrect";
-                });
-
-                rootScope.$apply(function () {
-                    rootScope.itemSession.sessionData = {};
-                    rootScope.itemSession.sessionData.correctResponses = { rid:"correct" }
-                });
-
-                expect(element.attr('class').contains(' ' + scope.CSS.correct)).toBe(false);
+            scope.$apply(function () {
+                scope.textResponse = "correct";
             });
 
-            it('resets the ui', function () {
-                var interaction = getInteraction();
-                var element = interaction.element;
-                var scope = interaction.scope;
-
-                element
-                    .addClass(scope.CSS.correct)
-                    .addClass(scope.CSS.incorrect);
-
-                rootScope.$broadcast('resetUI');
-
-                expect(element.attr('class').contains(' ' + scope.CSS.correct)).toBe(false);
-                expect(element.attr('class').contains(' ' + scope.CSS.incorrect)).toBe(false);
-
-
+            rootScope.$apply(function () {
+                rootScope.itemSession.sessionData = {};
+                rootScope.itemSession.sessionData.correctResponses = { rid:"correct" }
             });
+
+            expect(element.attr('class').contains(scope.CSS.correct)).toBe(true);
+
+            scope.$apply(function () {
+                scope.textResponse = "incorrect";
+            });
+
+            rootScope.$apply(function () {
+                rootScope.itemSession.sessionData = {};
+                rootScope.itemSession.sessionData.correctResponses = { rid:"correct" }
+            });
+
+            expect(element.attr('class').contains(' ' + scope.CSS.correct)).toBe(false);
+        });
+
+        it('resets the ui', function () {
+            var interaction = getInteraction();
+            var element = interaction.element;
+            var scope = interaction.scope;
+
+            element
+                .addClass(scope.CSS.correct)
+                .addClass(scope.CSS.incorrect);
+
+            rootScope.$broadcast('resetUI');
+
+            expect(element.attr('class').contains(' ' + scope.CSS.correct)).toBe(false);
+            expect(element.attr('class').contains(' ' + scope.CSS.incorrect)).toBe(false);
+
+
         });
     });
 });
