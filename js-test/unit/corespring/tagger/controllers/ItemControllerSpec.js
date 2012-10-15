c94b0ce00c2e35353503b23a2456e8f042c31519
edeustace <ed.eustace@gmail.com>
2012-10-15 15:27:55 +0200

    more ItemController specs

--- js-test/unit/corespring/tagger/controllers/ItemControllerSpec.js
+++ js-test/unit/corespring/tagger/controllers/ItemControllerSpec.js
@@ -19,16 +19,9 @@ describe('ItemController should', function () {
         var urls = [
             {method:'PUT', url:/.*/, response:{ ok:true }},
             {method:'POST', url:/.*/, data:{}, response:{ ok:true }},
-            {method: 'GET', url: "/api/v1/collections?access_token=1", response: {
-
-            }},
-            {method: 'GET', url: "/api/v1/items?access_token=1", response: {
-
-            }},
-
-            {method: 'GET', url: "/assets/web/standards_tree.json", response: {
-
-            }}
+            {method:'GET', url:"/api/v1/collections?access_token=1", response:{}},
+            {method:'GET', url:"/api/v1/items?access_token=1", response:{}},
+            {method:'GET', url:"/assets/web/standards_tree.json", response:{}}
         ];
 
         for (var i = 0; i < urls.length; i++) {
@@ -55,20 +48,89 @@ describe('ItemController should', function () {
         expect(ctrl).not.toBeNull();
     }));
 
-    it('calculates pValue as string', function(){
-      expect(scope.getPValueAsString(0)).toBe("");
-      expect(scope.getPValueAsString(1)).toBe("Very Hard");
-      expect(scope.getPValueAsString(19)).toBe("Very Hard");
-      expect(scope.getPValueAsString(20)).toBe("Very Hard");
-      expect(scope.getPValueAsString(21)).toBe("Moderately Hard");
-      expect(scope.getPValueAsString(40)).toBe("Moderately Hard");
-      expect(scope.getPValueAsString(41)).toBe("Moderate");
-      expect(scope.getPValueAsString(60)).toBe("Moderate");
-      expect(scope.getPValueAsString(61)).toBe("Easy");
-      expect(scope.getPValueAsString(80)).toBe("Easy");
-      expect(scope.getPValueAsString(81)).toBe("Very Easy");
-      expect(scope.getPValueAsString(99)).toBe("Very Easy");
-      expect(scope.getPValueAsString(100)).toBe("Very Easy");
+    it('calculates pValue as string', function () {
+        expect(scope.getPValueAsString(0)).toBe("");
+        expect(scope.getPValueAsString(1)).toBe("Very Hard");
+        expect(scope.getPValueAsString(19)).toBe("Very Hard");
+        expect(scope.getPValueAsString(20)).toBe("Very Hard");
+        expect(scope.getPValueAsString(21)).toBe("Moderately Hard");
+        expect(scope.getPValueAsString(40)).toBe("Moderately Hard");
+        expect(scope.getPValueAsString(41)).toBe("Moderate");
+        expect(scope.getPValueAsString(60)).toBe("Moderate");
+        expect(scope.getPValueAsString(61)).toBe("Easy");
+        expect(scope.getPValueAsString(80)).toBe("Easy");
+        expect(scope.getPValueAsString(81)).toBe("Very Easy");
+        expect(scope.getPValueAsString(99)).toBe("Very Easy");
+        expect(scope.getPValueAsString(100)).toBe("Very Easy");
+    });
+
+    it('creates mongo queries for standards', function () {
+
+        scope.standardAdapter.subjectOption = "all";
+
+        var query = scope.createStandardMongoQuery("ed", ["name", "title"]);
+
+        var queryObject = JSON.parse(query);
+        expect(queryObject).toEqual({ $or:[
+                { subject:{ $regex:'\\bed', $options:'i' } },
+                { name:{ $regex:'\\bed', $options:'i' } },
+                { title:{ $regex:'\\bed', $options:'i' } }
+            ] }
+        );
+
+        scope.standardAdapter.subjectOption = { name:"ELA" };
+
+        var elaQuery = JSON.parse(scope.createStandardMongoQuery("ed", ["dotNotation"]));
+
+        expect(elaQuery).toEqual(
+
+            { $and:[
+                { subject:'ELA' },
+                { $or:[
+                    { dotNotation:{ $regex:'\\bed', $options:'i' } }
+                ] }
+            ] }
+        );
+
+        scope.standardAdapter.categoryOption = { name: "Maths"};
+
+
+        expect(JSON.parse(scope.createStandardMongoQuery("ed", ["dotNotation"]))).toEqual(
+
+            { $and:[
+                { subject:'ELA' },
+                { category: 'Maths'},
+                { $or:[
+                    { dotNotation:{ $regex:'\\bed', $options:'i' } }
+                ] }
+            ] }
+        );
+
+        scope.standardAdapter.subCategoryOption = "Arithmetic";
+
+        expect(JSON.parse(scope.createStandardMongoQuery("ed", ["dotNotation"]))).toEqual(
+
+            { $and:[
+                { subject:'ELA' },
+                { category: 'Maths'},
+                { subCategory: 'Arithmetic'},
+                { $or:[
+                    { dotNotation:{ $regex:'\\bed', $options:'i' } }
+                ] }
+            ] }
+        );
+
+    });
+
+    it('creates key skills summary', function(){
+
+        expect( scope.getKeySkillsSummary( ["a"])).toEqual( "1 Key Skill selected");
+        expect( scope.getKeySkillsSummary( ["a", "b"])).toEqual( "2 Key Skills selected");
+        expect( scope.getKeySkillsSummary( ["a", "b", "c"])).toEqual( "3 Key Skills selected");
+        expect( scope.getKeySkillsSummary( [])).toEqual( "No Key Skills selected");
+        expect( scope.getKeySkillsSummary( null )).toEqual( "No Key Skills selected");
+        expect( scope.getKeySkillsSummary( undefined )).toEqual( "No Key Skills selected");
+
     });
 
 });
