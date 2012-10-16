fc89bd3f9dfdaf14985cb7f6fd684451c9e1019d
edeustace <ed.eustace@gmail.com>
2012-10-16 16:44:17 +0200

    made db seeder a lib: https://github.com/thesib/mongo-db-seeder

--- app/Global.scala
+++ app/Global.scala
@@ -1,12 +1,16 @@
 import _root_.controllers.S3Service
+import com.ee.seeder.MongoDbSeeder
+import _root_.models.auth.AccessToken
 import com.mongodb.casbah.commons.conversions.scala.RegisterJodaTimeConversionHelpers
+import org.bson.types.ObjectId
+import org.joda.time.DateTime
 import play.api._
 import mvc._
 import mvc.SimpleResult
 import play.api.Play.current
 import play.api.Application
 import web.controllers.utils.ConfigLoader
-import seed.SeedDb._
+
 /**
   */
 object Global extends GlobalSettings {
@@ -70,26 +74,47 @@ object Global extends GlobalSettings {
 
     val initData = ConfigLoader.get(INIT_DATA).getOrElse("true") == "true"
 
-    if (Play.isTest(app)) {
-      seedTestData()
+    val mongoUri = ConfigLoader.get("mongodb.default.uri")
+
+    if (Play.isTest(app) && mongoUri.isDefined) {
+      seedTestData(mongoUri.get)
     } else {
-      if (initData) seedDevData()
+      if (initData && mongoUri.isDefined) seedDevData(mongoUri.get)
     }
   }
 
-  private def seedTestData()  {
-    emptyData()
-    seedData("conf/seed-data/common")
-    seedData("conf/seed-data/test")
+
+  private def seedTestData(uri:String)  {
+
+    val folders = List(
+      "conf/seed-data/common",
+      "conf/seed-data/test"
+    )
+
+    Logger.info("reseed: \n" + folders.mkString("\n"))
+    MongoDbSeeder.emptyDb(uri, folders)
+    MongoDbSeeder.seed(uri, folders)
     addMockAccessToken(MOCK_ACCESS_TOKEN)
   }
 
-  private def seedDevData()  {
-    emptyData()
-    seedData("conf/seed-data/common")
-    seedData("conf/seed-data/dev")
-    seedData("conf/seed-data/exemplar-content")
+  private def seedDevData(uri:String)  {
+
+    val folders = List(
+      "conf/seed-data/common",
+      "conf/seed-data/dev",
+      "conf/seed-data/exemplar-content"
+    )
+    Logger.info("reseed: \n" + folders.mkString("\n"))
+    MongoDbSeeder.emptyDb(uri, folders)
+    MongoDbSeeder.seed(uri, folders)
     addMockAccessToken(MOCK_ACCESS_TOKEN)
+  }
+
+  def addMockAccessToken(token: String) = {
+    AccessToken.collection.drop()
+    val creationDate = DateTime.now()
+    val accessToken = AccessToken(new ObjectId("502404dd0364dc35bb393397"), None, token, creationDate, creationDate.plusHours(24))
+    AccessToken.insert(accessToken)
   }
 
 }
