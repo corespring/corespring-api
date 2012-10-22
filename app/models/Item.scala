866b0f214f72f6b5147c766f9f28dd4319946902
edeustace <ed.eustace@gmail.com>
2012-10-22 13:13:35 +0200

    Small tidy up :

--- app/models/Item.scala
+++ app/models/Item.scala
@@ -433,8 +433,6 @@ object Item extends DBQueryable[Item] {
     QueryFieldString[Item](bloomsTaxonomy, _.bloomsTaxonomy, queryValueFn(bloomsTaxonomy, fieldValues.bloomsTaxonomy)),
     QueryFieldString[Item](licenseType, _.contributorDetails.map(_.licenseType), queryValueFn(licenseType, fieldValues.licenseTypes)),
 
-    //QueryFieldString[Item](primarySubject + ".subject", _.subjects.map(_.primary)),
-
     QueryFieldObject[Item](primarySubject, _.subjects.map(_.primary), _ match {
       case x: String => try {
         Right(new ObjectId(x))
