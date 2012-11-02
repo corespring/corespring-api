ad8b35ce7e83e7a0286e4e48769200888b0faf4d
bleezmo <joshuarogers10@gmail.com>
2012-11-02 02:55:15 -0400

    had to merge other commits. basically removed the stuff i had previously done manually

--- public/js/corespring/items.js
+++
@@ -1,9 +0,0 @@
-angular.module('itemResource', ['ngResource']).
-    factory('Items', function($resource) {
-        return $resource('items')
-    });
-
-angular.module('fieldValuesResource',['ngResource']).
-    factory('FieldValues', function($resource) {
-        return $resource('api/v1/field_values/:fieldValue')
-    })
