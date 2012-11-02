248cf46b1113acc6cbca51685a8e307905a0b987
bleezmo <joshuarogers10@gmail.com>
2012-11-02 02:36:02 -0400

    refactored public site stuff. got it working with /public as root

---
+++ app/publicsite/controllers/Application.scala
@@ -0,0 +1,31 @@
+package publicsite.controllers
+
+import play.api.mvc._
+import play.api.Play
+import play.api.libs.json.Json
+import play.api.Play.current
+import java.nio.charset.Charset
+import scala.io.Codec
+import controllers.Log
+
+object Application extends Controller{
+
+  def index = Action {
+    Ok(publicsite.views.html.index())
+  }
+  def contact = Action {
+    Ok(publicsite.views.html.contact())
+  }
+  def collection = Action {
+    Ok(publicsite.views.html.collection())
+  }
+  def partnerships = Action {
+    Ok(publicsite.views.html.partnerships())
+  }
+  def team = Action {
+    Ok(publicsite.views.html.team())
+  }
+  def getItems = Action {
+    Ok(Json.parse(io.Source.fromFile(Play.getFile("public/public/conf/items.json"))(new Codec(Charset.forName("UTF-8"))).mkString))
+  }
+}
--- public/public/js/angular/items.js
+++ public/public/js/angular/items.js
@@ -1,9 +1,9 @@
 angular.module('itemResource', ['ngResource']).
     factory('Items', function($resource) {
-        return $resource('conf/items.json')
+        return $resource('http://localhost:port/public/items',{port:':9000'})
     });
 
 angular.module('fieldValuesResource',['ngResource']).
     factory('FieldValues', function($resource) {
-        return $resource('conf/fieldValues.json')
+        return $resource('http://localhost:port/api/v1/field_values/:fieldValue',{port:':9000'})
     })
