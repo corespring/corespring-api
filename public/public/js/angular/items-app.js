75ff23573abde8246c0a638cbedee4324fa0c76c
Attila Miklosi <attila.miklosi@gmail.com>
2012-11-08 15:50:35 +0000

    if no standards show not assigned tooltip for standards

--- public/public/js/angular/items-app.js
+++ public/public/js/angular/items-app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['itemResource','fieldValuesResource']);
+var app = angular.module('app', ['itemResource','fieldValuesResource','ui']);
 
 
 function ItemsCtrl($scope, FieldValues, Items) {
