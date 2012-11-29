e69a7172217aaa379c9d9489d4b3810d9e0b9d38
edeustace <ed.eustace@gmail.com>
2012-11-29 13:52:37 +0100

    Added releases resolver

--- project/Build.scala
+++ project/Build.scala
@@ -12,15 +12,15 @@ object ApplicationBuild extends Build {
     "com.typesafe" %% "play-plugins-util" % "2.0.3",
     "com.typesafe" %% "play-plugins-mailer" % "2.0.4",
     "org.mindrot" % "jbcrypt" % "0.3m",
-    //"securesocial" % "securesocial_2.9.1" % "2.0.7",
-    "securesocial" % "securesocial_2.9.1" % "master"
+    "securesocial" % "securesocial_2.9.1" % "2.0.7"
+    //"securesocial" % "securesocial_2.9.1" % "master"
   )
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
     routesImport += "se.radley.plugin.salat.Binders._",
     templatesImport += "org.bson.types.ObjectId",
     resolvers += "Sonatype" at "https://oss.sonatype.org/content/repositories/snapshots/",
-    //resolvers += Resolver.url("SecureSocial Repository", url("http://securesocial.ws/repository/releases/"))(Resolver.ivyStylePatterns),
+    resolvers += Resolver.url("SecureSocial Repository", url("http://securesocial.ws/repository/releases/"))(Resolver.ivyStylePatterns),
     resolvers += Resolver.url("SecureSocial Repository", url("http://securesocial.ws/repository/snapshots/"))(Resolver.ivyStylePatterns)
   )
 
