3788d839299efc1d76209cb3117f7ccfbcc09ddd
edeustace <ed.eustace@gmail.com>
2012-12-04 14:23:25 +0100

    First run at /developer/orgs page

--- app/developer/controllers/Developer.scala
+++ app/developer/controllers/Developer.scala
@@ -2,11 +2,15 @@ package developer.controllers
 
 import play.api.mvc.Controller
 import controllers.Assets
+import securesocial.core.SecureSocial
 
-object Developer extends Controller{
+object Developer extends Controller with SecureSocial{
 
   def at(path:String,file:String) = Assets.at(path,file)
 
 
+  def organizations = SecuredAction(){ request =>
+    Ok(developer.views.html.organizations(request.user))
+  }
 }
 
