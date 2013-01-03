d270168c089ea9a122ba13e3eea4c048ef707d31
edeustace <ed.eustace@gmail.com>
2013-01-03 14:13:00 +0000

    item formatting utils test

---
+++ js-test/unit/corespring/lti-chooser/controllers/SearchControllerSpec.js
@@ -0,0 +1,51 @@
+describe('lti-chooser.SearchController', function () {
+  'use strict';
+
+  var ctrl, scope, rootScope, $httpBackend;
+
+  beforeEach(module('corespring-utils'));
+  beforeEach(module('tagger.services'));
+  beforeEach(module('corespring-services'));
+
+  var prepareBackend = function ($backend) {
+
+        var urls = [
+            {method:'GET', url:/\/api\/v1\/collections.*/, response:{ ok:true }},
+            {method:'POST', url:/.*/, data: {}, response:{ ok:true }}
+        ];
+
+        for (var i = 0; i < urls.length; i++) {
+            var definition = urls[i];
+            $backend.when(definition.method, definition.url).respond(200, definition.response);
+        }
+    };
+
+
+  beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
+    $httpBackend = _$httpBackend_;
+    rootScope = $rootScope;
+    prepareBackend($httpBackend);
+    scope = $rootScope.$new();
+
+    try {
+      ctrl = $controller(SearchController, {$scope: scope});
+    } catch (e) {
+      throw("Error with the controller: " + e);
+    }
+  }));
+
+
+  describe('SearchController', function () {
+
+    it('update isSearching', function () {
+       rootScope.$broadcast('onNetworkLoading');
+       expect(rootScope.isSearching).toBe(true);
+       rootScope.$broadcast('onSearchCountComplete');
+       expect(rootScope.isSearching).toBe(false);
+    });
+
+    it('should return item count', function(){
+      expect(scope.getItemCountLabel(1)).toBe("1 results");
+    })
+ });
+});
