5408d1c1baa6e51de463704cb4f3fd558552968e
edeustace <ed.eustace@gmail.com>
2013-01-03 13:31:09 +0000

    some docs

--- public/js/corespring/common/services/MessageBridge.js
+++ public/js/corespring/common/services/MessageBridge.js
@@ -1,7 +1,15 @@
+
+/**
+ * Service for page to frame communication using the postMessage api.
+ */
 angular.module('corespring-services', []).factory('MessageBridge', [function () {
 
   return {
 
+    /**
+     * Add handler for 'message'
+     * @param fn
+     */
     addMessageListener: function (fn) {
       if (window.addEventListener) {
         window.addEventListener('message', fn, true);
@@ -13,6 +21,10 @@ angular.module('corespring-services', []).factory('MessageBridge', [function () 
       }
     },
 
+    /**
+     * @param id - target either an iframe id or 'parent'
+     * @param msg - a string or object - gets JSON.stringified
+     */
     sendMessage: function (id, msg) {
 
       function getParent(){ return (parent && parent != window) ? parent : null; }
@@ -24,6 +36,7 @@ angular.module('corespring-services', []).factory('MessageBridge', [function () 
           return null;
         }
       }
+
       var target = (!id || id === "parent" ) ? getParent() : getIframe(id);
 
       if(target){
