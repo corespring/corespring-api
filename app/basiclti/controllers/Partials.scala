c6923ee00d2fd665fb7572bc235e8a4324700ec6
edeustace <ed.eustace@gmail.com>
2013-01-10 19:39:09 +0100

    grey out Unassign button: https://trello.com/c/STdCAWTv

--- app/basiclti/controllers/Partials.scala
+++ app/basiclti/controllers/Partials.scala
@@ -4,6 +4,8 @@ import play.api.mvc.{Controller, Action}
 
 object Partials extends Controller{
   def main = Action(Ok(basiclti.views.html.partials.main()))
-  def view = Action(Ok(basiclti.views.html.partials.view()))
+  def view = Action{ request =>
+    Ok(basiclti.views.html.partials.view())
+  }
   def browse = Action(Ok(basiclti.views.html.partials.browse()))
 }
