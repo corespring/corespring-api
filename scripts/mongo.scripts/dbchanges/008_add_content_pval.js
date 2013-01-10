36fc2b9634e6034c26ef0e66e85d2f9ae93f3394
edeustace <ed.eustace@gmail.com>
2013-01-11 00:19:56 +0100

    add write permissions for root org

---
+++ scripts/mongo.scripts/dbchanges/008_add_content_pval.js
@@ -0,0 +1,13 @@
+function applyPVal(o){
+
+  if(o.contentcolls){
+    for(var i = 0 ; i < o.contentcolls.length; i++){
+      if(!o.contentcolls[i].pval){
+        o.contentcolls[i].pval = NumberLong("3")
+        db.orgs.save(o);
+      }
+    }
+  }
+}
+
+db.orgs.find({name: "Root Org"}).forEach(applyPVal);
