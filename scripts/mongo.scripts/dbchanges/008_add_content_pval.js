c642a1bc16c63af761ccd86d79f1b56ef47ced76
edeustace <ed.eustace@gmail.com>
2013-01-11 14:06:57 +0100

    small format fix

--- scripts/mongo.scripts/dbchanges/008_add_content_pval.js
+++ scripts/mongo.scripts/dbchanges/008_add_content_pval.js
@@ -3,7 +3,7 @@ function applyPVal(o){
   if(o.contentcolls){
     for(var i = 0 ; i < o.contentcolls.length; i++){
       if(!o.contentcolls[i].pval){
-        o.contentcolls[i].pval = NumberLong("3")
+        o.contentcolls[i].pval = NumberLong("3");
         db.orgs.save(o);
       }
     }
