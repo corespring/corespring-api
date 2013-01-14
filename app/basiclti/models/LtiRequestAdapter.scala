d05f3c78b702acc5d7fb82e8b79220f09a5531af
edeustace <ed.eustace@gmail.com>
2013-01-14 15:43:11 +0100

    get Lti TestHarness working in https

--- app/basiclti/models/LtiRequestAdapter.scala
+++ app/basiclti/models/LtiRequestAdapter.scala
@@ -4,22 +4,21 @@ import play.api.mvc.{AnyContent, Request}
 import oauth.signpost.http.HttpRequest
 import java.net.URLEncoder
 import common.controllers.utils.BaseUrl
+import play.Logger
 
 case class LtiRequestAdapter(request: Request[_], params:Map[String, String]) extends HttpRequest {
   var headers = Map[String,String]()
 
   def getMethod = request.method
 
-
   def getRequestUrl : String = {
 
     def e(s:String) = URLEncoder.encode(s,"utf-8")
 
-    //TODO: How to decide on http/https?
     val base = BaseUrl(request.asInstanceOf[Request[AnyContent]])
-    //val host = if(request.host == null || request.host.isEmpty) "localhost:9000" else request.host
-    //val url = "http://" + request.host + request.path
+
     val url = base + request.path
+
     if (params.isEmpty){
       url
     } else {
