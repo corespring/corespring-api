1d702b229c92020c4ecaedd44f5db845c8a78d7d
Joshua Rogers <joshuarogers10@gmail.com>
2013-01-16 10:41:08 -0500

    added ItemQueryTest

---
+++ test/tests/models/ItemQueryTest.scala
@@ -0,0 +1,27 @@
+package tests.models
+
+import tests.BaseTest
+
+
+class ItemQueryTest extends BaseTest{
+
+  "filter search for contributorDetails" in {
+    pending
+  }
+  "filter search by gradeLevel" in {
+    pending
+  }
+  "filter search by standards" in {
+
+  }
+  "filter search by subjects" in {
+
+  }
+  "use regex to filter search by title" in {
+
+  }
+  "filtering by supportingMaterials or data results in error" in {
+    pending
+  }
+
+}
