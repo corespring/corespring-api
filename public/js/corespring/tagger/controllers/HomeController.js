4b5aac9d22d72595aa71d74269a61be8336dd071
Attila Miklosi <attila.miklosi@gmail.com>
2013-01-18 09:10:58 +0000

    Minor fixes for content filtering (https://trello.com/card/implement-filtering-search-for-content-tagger/507d8bab884ea01d319389c1/150)

--- public/js/corespring/tagger/controllers/HomeController.js
+++ public/js/corespring/tagger/controllers/HomeController.js
@@ -1,169 +1,166 @@
-function HomeController(
-  $scope,
-  $rootScope,
-  $http,
-  $location,
-  ItemService,
-  SearchService,
-  Collection,
-  Contributor,
-  ItemFormattingUtils) {
+function HomeController($scope, $rootScope, $http, $location, ItemService, SearchService, Collection, Contributor, ItemFormattingUtils) {
 
-    //Mixin ItemFormattingUtils
-    angular.extend($scope, ItemFormattingUtils);
+  //Mixin ItemFormattingUtils
+  angular.extend($scope, ItemFormattingUtils);
 
-    $http.defaults.headers.get = ($http.defaults.headers.get || {});
-    $http.defaults.headers.get['Content-Type'] = 'application/json';
+  $http.defaults.headers.get = ($http.defaults.headers.get || {});
+  $http.defaults.headers.get['Content-Type'] = 'application/json';
 
-    $scope.$root.mode = "home";
+  $scope.$root.mode = "home";
 
-    $scope.searchParams = $rootScope.searchParams ? $rootScope.searchParams : ItemService.createWorkflowObject();
-    $rootScope.$broadcast('onListViewOpened');
+  $scope.searchParams = $rootScope.searchParams ? $rootScope.searchParams : ItemService.createWorkflowObject();
+  $rootScope.$broadcast('onListViewOpened');
 
 
-    var init = function(){
+  var init = function () {
 
-        $scope.search();
-        loadCollections();
-        loadContributors();
+    $scope.search();
+    loadCollections();
+    loadContributors();
 
-        var defaultsFactory = new com.corespring.model.Defaults();
-        $scope.gradeLevelDataProvider = defaultsFactory.buildNgDataProvider("gradeLevels");
-        $scope.itemTypeDataProvider = defaultsFactory.buildNgDataProvider("itemTypes");
-        $scope.statuses = [
-            {label: "Setup", key: "setup"},
-            {label: "Tagged", key: "tagged"},
-            {label: "QA Review", key: "qaReview"},
-            {label: "Standards Aligned", key: "standardsAligned"},
-            {label: "Exact Match", key: "exactMatch"}
-        ];
+    var defaultsFactory = new com.corespring.model.Defaults();
+    $scope.gradeLevelDataProvider = defaultsFactory.buildNgDataProvider("gradeLevels");
+    $scope.itemTypeDataProvider = defaultsFactory.buildNgDataProvider("itemTypes");
+    $scope.statuses = [
+      {label: "Setup", key: "setup"},
+      {label: "Tagged", key: "tagged"},
+      {label: "Standards Aligned", key: "standardsAligned"},
+      {label: "QA Review", key: "qaReview"},
+      {label: "Exact Match", key: "exactMatch"}
+    ];
 
-    };
+  };
 
-    $scope.getContributorTitle = function(c){
+  $scope.getContributorTitle = function (c) {
 
-        return c.name;
-    };
+    return c.name;
+  };
 
-    $scope.getContributorSelectedTitle = function(items){
-        if(!items || items.length == 0){
-                return "None Selected";
-              }
-        return items.length+" Selected";
+  $scope.getContributorSelectedTitle = function (items) {
+    if (!items || items.length == 0) {
+      return "None Selected";
     }
+    return items.length + " Selected";
+  }
 
-    $scope.getCollectionTitle = function(c){
-      return c.name.replace("CoreSpring", "");
-    };
+  $scope.getCollectionTitle = function (c) {
+    return c.name.replace("CoreSpring", "");
+  };
 
-    $scope.getTitle = function(o){ return o.key.replace(/^0/, "") };
-    $scope.getLabel = function(o){ return o.label };
+  $scope.getTitle = function (o) {
+    return o.key.replace(/^0/, "")
+  };
+  $scope.getLabel = function (o) {
+    return o.label
+  };
 
-    $scope.getCollectionSelectedTitle = function(items){
-      if(!items || items.length == 0){
-        return "None Selected";
+  $scope.getCollectionSelectedTitle = function (items) {
+    if (!items || items.length == 0) {
+      return "None Selected";
+    }
+    var out = _.map(items, function (i) {
+      return i.name
+    });
+    return out.join(", ").replace(/CoreSpring/g, "");
+  };
+
+  $scope.getSelectedTitle = function (items) {
+    if (!items || items.length == 0) {
+      return "None Selected";
+    }
+    var out = _.map(items, function (i) {
+      return i.key
+    });
+    return out.join(", ").replace(/0/g, "");
+  };
+
+
+  $scope.search = function () {
+    SearchService.search($scope.searchParams, function (res) {
+      $rootScope.items = res;
+    });
+  };
+
+  $scope.loadMore = function () {
+    SearchService.loadMore(function () {
+        // re-bind the scope collection to the services model after result comes back
+        $rootScope.items = SearchService.itemDataCollection;
+        //Trigger MathJax
+        setTimeout(function () {
+          MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+        }, 200);
+
       }
-      var out = _.map(items, function(i){ return i.name});
-      return out.join(", ").replace(/CoreSpring/g, "");
-    };
-
-    $scope.getSelectedTitle = function(items){
-      if(!items || items.length == 0){
-        return "None Selected";
-      }
-      var out = _.map(items, function(i){return i.key});
-      return out.join(", ").replace(/0/g, "");
-    };
+    );
+  };
 
 
-    $scope.search = function() {
-        SearchService.search($scope.searchParams, function(res){
-            $rootScope.items = res;
-        });
-    };
+  function loadCollections() {
+    Collection.get({}, function (data) {
+        $scope.collections = data;
+      },
+      function () {
+        console.log("load collections: error: " + arguments);
+      });
+  }
 
-    $scope.loadMore = function () {
-        SearchService.loadMore(function () {
-                // re-bind the scope collection to the services model after result comes back
-                $rootScope.items = SearchService.itemDataCollection;
-                //Trigger MathJax
-                setTimeout(function(){
-                    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-                }, 200);
-
-            }
-        );
-    };
-
+  function loadContributors() {
+    Contributor.get({}, function (data) {
+        $scope.contributors = data;
+      },
+      function () {
+        console.log("load contributors: error: " + arguments);
+      });
+  }
 
 
-    function loadCollections() {
-        Collection.get({}, function (data) {
-                $scope.collections = data;
-            },
-            function () {
-                console.log("load collections: error: " + arguments);
-            });
-    }
-
-    function loadContributors() {
-        Contributor.get({}, function (data) {
-                $scope.contributors = data;
-            },
-            function () {
-                console.log("load contributors: error: " + arguments);
-            });
-    }
+  $scope.showGradeLevel = function () {
+    return $scope.createGradeLevelString(this.item.gradeLevel);
+  };
 
 
-    $scope.showGradeLevel = function () {
-        return $scope.createGradeLevelString(this.item.gradeLevel);
-    };
+  $scope.deleteItem = function (item) {
+    $scope.itemToDelete = item;
+    $scope.showConfirmDestroyModal = true;
+  };
 
-
-
-    $scope.deleteItem = function(item) {
-        $scope.itemToDelete = item;
-        $scope.showConfirmDestroyModal = true;
-    };
-
-    $scope.deleteConfirmed = function(){
-        var deletingId = $scope.itemToDelete.id;
-        ItemService.remove({id: $scope.itemToDelete.id},
-            function(result) {
-                $scope.itemToDelete = null;
-                $scope.search();
-            }
-        );
+  $scope.deleteConfirmed = function () {
+    var deletingId = $scope.itemToDelete.id;
+    ItemService.remove({id: $scope.itemToDelete.id},
+      function (result) {
         $scope.itemToDelete = null;
-        $scope.showConfirmDestroyModal = false;
-    };
+        $scope.search();
+      }
+    );
+    $scope.itemToDelete = null;
+    $scope.showConfirmDestroyModal = false;
+  };
 
-    $scope.deleteCancelled = function(){
-        console.log("Item Delete Cancelled");
-       $scope.itemToDelete = null;
-       $scope.showConfirmDestroyModal = false;
-    };
+  $scope.deleteCancelled = function () {
+    console.log("Item Delete Cancelled");
+    $scope.itemToDelete = null;
+    $scope.showConfirmDestroyModal = false;
+  };
 
 
-    /*
-     * called from the repeater. scope (this) is the current item
-     */
-    $scope.openEditView = function () {
-        SearchService.currentItem = this.item;
-        $location.url('/edit/' + this.item.id + "?panel=metadata");
-    };
+  /*
+   * called from the repeater. scope (this) is the current item
+   */
+  $scope.openEditView = function () {
+    SearchService.currentItem = this.item;
+    $location.url('/edit/' + this.item.id + "?panel=metadata");
+  };
 
-    init();
+  init();
 }
 
 HomeController.$inject = ['$scope',
-    '$rootScope',
-    '$http',
-    '$location',
-    'ItemService',
-    'SearchService',
-    'Collection',
-    'Contributor',
-    'ItemFormattingUtils'];
+  '$rootScope',
+  '$http',
+  '$location',
+  'ItemService',
+  'SearchService',
+  'Collection',
+  'Contributor',
+  'ItemFormattingUtils'];
 
