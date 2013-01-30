0c7c4a1b4d20b24840129316579bb903af708726
edeustace <ed.eustace@gmail.com>
2013-01-30 14:27:52 +0100

    first run deploy.rb script

---
+++ deployment/deploy.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+remote = ARGV[0]
+
+puts "--------------------------------------------------"
+puts "deploy.rb - remote: #{remote}"
+puts "--------------------------------------------------"
+
+raise "you must specify - #{remote}" if remote == nil
+
+`git remote add #{remote} git@heroku.com:#{remote}.git`
+
+raise "error adding git remote" unless $?.to_i == 0
+
+`java -jar deployment/libs/heroku-helper.jar push #{remote} master`
+
+raise "error running helper" unless $?.to_i == 0
