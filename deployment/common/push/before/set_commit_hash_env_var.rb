547e49475a5cd14069c7d00980cd7a0e943376ea
edeustace <ed.eustace@gmail.com>
2013-02-04 21:09:43 +0100

    trim the version string

--- deployment/common/push/before/set_commit_hash_env_var.rb
+++ deployment/common/push/before/set_commit_hash_env_var.rb
@@ -3,8 +3,10 @@ require 'json'
 
 begin
   commit_hash = `git rev-parse --short HEAD`
-  app = ARGV[1]
-  `heroku config:set ENV_CORESPRING_API_COMMIT_HASH=#{commit_hash} --app #{app}` unless app.nil?
+  app = ARGV[1].chomp
+  puts "0: #{ARGV[0]}"
+  puts "app name: #{app}"
+  `heroku config:set ENV_CORESPRING_API_COMMIT_HASH="#{commit_hash}" --app #{app}` unless app.nil?
 rescue
   puts "error setting hash as env var"
 end
