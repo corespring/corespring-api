ea1f1c0698bab5c8dabd3e08d8e0aea9cb000ee3
Attila Miklosi <attila.miklosi@gmail.com>
2013-02-04 14:10:50 +0000

    fixed resizing mechanism of the player to fix issues where the bottom got cut off

--- public/public/js/angular/items-app.js
+++ public/public/js/angular/items-app.js
@@ -1,9 +1,10 @@
 var app = angular.module('app', ['itemResource', 'fieldValuesResource', 'tagger.services', 'preview.services', 'ui', 'corespring-utils']);
 
+
 angular.module('app')
   .directive('iframeAutoHeight', function () {
     return {
-      link:function ($scope, element, attr, ctrl) {
+      link: function ($scope, element) {
         $(element).iframeAutoHeight({});
       }
     }
@@ -11,21 +12,21 @@ angular.module('app')
 
 function ItemsCtrl($scope, $timeout) {
 
-  $scope.hidePopup = function() {
-      $scope.showPopup = false;
-    };
+  $scope.hidePopup = function () {
+    $scope.showPopup = false;
+  };
 
-    $scope.openItem = function(id) {
-      $timeout(function() {
-        $scope.showPopup = true;
-        $scope.previewingId = id;
-        $scope.$broadcast("requestLoadItem", id);
-      }, 50);
-      $timeout(function() {
-        $('.window-overlay').scrollTop(0);
-      }, 100);
+  $scope.openItem = function (id) {
+    $timeout(function () {
+      $scope.showPopup = true;
+      $scope.previewingId = id;
+      $scope.$broadcast("requestLoadItem", id);
+    }, 50);
+    $timeout(function () {
+      $('.window-overlay').scrollTop(0);
+    }, 100);
 
-    };
+  };
 
 }
 ItemsCtrl.$inject = ['$scope', '$timeout'];
