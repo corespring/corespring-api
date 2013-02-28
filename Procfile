aea24c759ab080314c7c07fced0c5f41320eabc6
Joshua Rogers <joshuarogers10@gmail.com>
2013-02-28 12:16:57 -0500

    made scheduler a separate project

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: target/start -Dhttp.port=${PORT} ${JAVA_OPTS} -Dconfig.resource=heroku.conf
+worker: scheduler/target/start ${JAVA_OPTS}
--- scheduler/src/main/scala/scheduler/tasks/SessionAggregateTask.scala
+++ scheduler/src/main/scala/scheduler/tasks/SessionAggregateTask.scala
@@ -6,6 +6,8 @@ import controllers.Log
 import akka.util.Duration
 import models.itemSession.ItemSession
 import com.mongodb.casbah.Imports._
+import models.item.Item
+import play.api.libs.json.Json
 
 
 class SessionAggregateTask extends RabbitMQTask{
@@ -13,7 +15,9 @@ class SessionAggregateTask extends RabbitMQTask{
   override val frequency = 1 minute
 
   def run() {
+    val item = Item.findOne(MongoDBObject())
     println("running session aggregate task at "+System.currentTimeMillis())
+    println("got item: "+Json.toJson(item).toString())
 //    val mapJS:JSFunction = """
 //      function () {
 //          emit(this.itemId,this)
