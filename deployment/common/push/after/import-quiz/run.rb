dff5558ffd0af5626718118b8d43224aa0c5e448
Evan Eustace <evan.eustace@gmail.com>
2013-04-03 17:15:50 -0400

    fix argument for dump path

--- deployment/common/push/after/import-quiz/run.rb
+++ deployment/common/push/after/import-quiz/run.rb
@@ -19,7 +19,7 @@ config = JSON.parse(IO.read(ARGV[0]))
 target_db_uri = config["ENV_MONGO_URI"]
 raise "no target db specified" if target_db_uri == nil
 
-cmd = "./deployment/common/push/after/import-quiz/import.rb #{target_db_uri} dump"
+cmd = "./deployment/common/push/after/import-quiz/import.rb #{target_db_uri} deployment/common/push/after/import-quiz/dump"
 puts "command: "
 puts "#{cmd}"
 `#{cmd}`
