62c8cbf56a2f0eaae725def82685c6f47debc65c
Evan Eustace <evan.eustace@gmail.com>
2013-04-03 16:54:40 -0400

    heroku helper sample data + importer

--- deployment/common/push/after/import-quiz/run.rb
+++ deployment/common/push/after/import-quiz/run.rb
@@ -1,14 +1,6 @@
 #!/usr/bin/env ruby
 
-puts "------------------"
-puts "which ruby?"
-puts `which ruby`
-
-puts "gem list?"
-puts `gem list`
-puts "------------------"
 require 'json'
-puts "json: #{JSON}"
 require_relative '../../../../libs/ruby/db'
 
 puts "Db: #{Db}"
@@ -18,13 +10,22 @@ puts "MongoTools: #{MongoTools}"
 puts "-------------------------------------"
 puts "common/push/after/import-quiz/run.rb"
 puts "-------------------------------------"
+puts ""
 
 raise "no config file specified" if ARGV[0] == nil
 
 config = JSON.parse(IO.read(ARGV[0]))
 
-puts "_____________XXXXxx________Xxx_______________________"
-
-
 target_db_uri = config["ENV_MONGO_URI"]
 raise "no target db specified" if target_db_uri == nil
+
+cmd = "./deployment/common/push/after/import-quiz/import.rb #{target_db_uri} dump"
+puts "command: "
+puts "#{cmd}"
+`#{cmd}`
+puts ""
+puts "-------------------------------------"
+
+
+
+
