fb847b9b4461c0cebdeb05ee7939937b4dd72d58
Attila Miklosi <attila.miklosi@gmail.com>
2013-04-04 11:57:34 +0200

    aggregate player: show grey box Open Response for extended text interaction

--- public/js/corespring/qti/directives/aggregate/extendedTextInteraction.js
+++ public/js/corespring/qti/directives/aggregate/extendedTextInteraction.js
@@ -1,22 +1,11 @@
-qtiDirectives.directive("extendedtextinteraction", function (QtiUtils) {
+qtiDirectives.directive("extendedtextinteraction", function () {
   return {
     restrict: 'E',
     replace: true,
     scope: true,
     require: '^assessmentitem',
-    template: '<div ng-class="{noResponse: noResponse}"><textarea rows="{{rows}}" cols="{{cols}}" ng-model="extResponse" ng-disabled="formSubmitted"></textarea></div>',
-    link: function (scope, element, attrs, AssessmentItemController) {
-
-      $(element).tooltip({ title: "Open Response" });
-
-      scope.controller = AssessmentItemController;
-      scope.rows = 4; // default # of rows
-      scope.cols = 60; // default # of cols
-
-      if (attrs.expectedlines) {
-        scope.rows = attrs.expectedlines;
-      }
-
+    template: '<table class="open-response-table"><tr><td>Open Response</table>',
+    link: function (scope, element, attrs) {
     }
 
   };
