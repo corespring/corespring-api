3901bb81d66efe97d56051f07bf1cb84f2c464a0
edeustace <ed.eustace@gmail.com>
2013-04-26 20:01:40 +0200

    small tidy up

--- test/tests/basiclti/accessControl/auth/ValidateQuizIdAndOrgIdTest.scala
+++ test/tests/basiclti/accessControl/auth/ValidateQuizIdAndOrgIdTest.scala
@@ -12,32 +12,28 @@ import player.accessControl.cookies.PlayerCookieKeys
 import tests.PlaySingleton
 
 object MockImpl extends ValidateQuizIdAndOrgId[FakeRequest[AnyContent]] {
-
-  /** Build the request to be passed into the block */
   def makeRequest(orgId: ObjectId, r: Request[AnyContent]): Option[FakeRequest[AnyContent]] = Some(r.asInstanceOf[FakeRequest[AnyContent]])
 }
-
 
 class ValidateQuizIdAndOrgIdTest extends Specification {
 
   PlaySingleton.start()
 
-  def mockQuery(quizId: String, orgId: String): Boolean = true
-
-  def mockBlock(request: FakeRequest[AnyContent]): Result = Ok("hello")
-
   "validate quiz id and org id" should {
 
     def run(keys: (String, String)*): Result = {
+      def mockQuery(quizId: String, orgId: String): Boolean = true
+      def mockBlock(request: FakeRequest[AnyContent]): Result = Ok("hello")
       val q: MockImpl.OrgIdAndQuizIdAreValid = mockQuery
       MockImpl.ValidatedAction(q)(mockBlock)(FakeRequest().withSession(keys: _*))
     }
+
     def oid(): String = new ObjectId().toString
 
-    "return unauthorized if no quiz id or org id in session" in status(run()) === UNAUTHORIZED
-    "no quiz id" in status(run(PlayerCookieKeys.ORG_ID -> oid)) === UNAUTHORIZED
-    "no org id" in status(run(LtiCookieKeys.QUIZ_ID -> oid)) === UNAUTHORIZED
-    "quiz id and org id" in status(run(LtiCookieKeys.QUIZ_ID -> oid, PlayerCookieKeys.ORG_ID -> oid)) === OK
+    "fail for nothing" in status(run()) === UNAUTHORIZED
+    "fail for no quiz id" in status(run(PlayerCookieKeys.ORG_ID -> oid)) === UNAUTHORIZED
+    "fail for no org id" in status(run(LtiCookieKeys.QUIZ_ID -> oid)) === UNAUTHORIZED
+    "ok for quiz id and org id" in status(run(LtiCookieKeys.QUIZ_ID -> oid, PlayerCookieKeys.ORG_ID -> oid)) === OK
 
   }
 }
