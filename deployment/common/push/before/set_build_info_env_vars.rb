547a22e741967afa3e9894f495e81429d17378cf
edeustace <ed.eustace@gmail.com>
2013-05-13 17:35:08 +0200

    fix for commit hashes not matching the origin hashes

--- deployment/common/push/before/set_build_info_env_vars.rb
+++ deployment/common/push/before/set_build_info_env_vars.rb
@@ -2,13 +2,17 @@
 require 'json'
 
 begin
-  commit_hash_short = `git rev-parse --short HEAD`.chomp
-  commit_hash = `git rev-parse HEAD`.chomp
-  commit_msg = `git rev-list --format=%s%B --oneline --max-count=1 HEAD`
-  push_date = `date`
+  puts "-------- set build info env vars -------------"
+  ### Args: 0: tmp json file with heroku config, 1: app name, 2: branch name
   app = ARGV[1].chomp
-  puts "0: #{ARGV[0]}"
+  puts "config json: #{ARGV[0]}"
   puts "app name: #{app}"
+  branch = ARGV[2]
+  puts "branch : #{branch}"
+  commit_hash_short = `git rev-parse --short origin/#{branch}`.chomp
+  commit_hash = `git rev-parse origin/#{branch}`.chomp
+  commit_msg = `git rev-list --format=%s%B --oneline --max-count=1 origin/#{branch}`
+  push_date = `date`
   `heroku config:set ENV_CORESPRING_API_COMMIT_HASH="#{commit_hash}" --app #{app}` unless app.nil?
   `heroku config:set ENV_CORESPRING_API_COMMIT_HASH_SHORT="#{commit_hash_short}" --app #{app}` unless app.nil?
   `heroku config:set ENV_CORESPRING_API_COMMIT_MSG="#{commit_msg}" --app #{app}` unless app.nil?
