c46f1a53ea5d772577a4b1ad29a764774597d968
edeustace <ed.eustace@gmail.com>
2013-05-20 22:51:36 +0200

    Validate object Id - don't allow it to throw a silent expception as this will be seen as a CORS issue on the client

--- app/controllers/auth/AuthController.scala
+++ app/controllers/auth/AuthController.scala
@@ -3,19 +3,25 @@ package controllers.auth
 import play.api.data._
 import play.api.data.Forms._
 import play.api.libs.json.Json
-import play.api.mvc.{Results, Result, Action, Controller}
+import play.api.mvc.{Action, Controller}
 import api.ApiError
 import org.bson.types.ObjectId
 import securesocial.core.SecureSocial
 import models.User
 import controllers.Log
+import play.api.data.validation._
+import scala.Left
+import scala.Some
+import scala.Right
+import play.api.data.validation.ValidationError
+import web.controllers.ObjectIdParser
 
 
 /**
  * A controller to handle the OAuth flow and consumer registration
  */
 
-object AuthController extends Controller with SecureSocial{
+object AuthController extends Controller with SecureSocial with ObjectIdParser{
 
   case class AccessTokenRequest(grant_type: String, client_id: String, client_secret: String, scope: Option[String])
 
@@ -30,13 +36,24 @@ object AuthController extends Controller with SecureSocial{
   val accessTokenForm = Form(
     mapping(
       OAuthConstants.GrantType -> optional(text),
-      OAuthConstants.ClientId -> nonEmptyText,
+      OAuthConstants.ClientId ->  text.verifying(validObjectIdConstraint),
       OAuthConstants.ClientSecret -> nonEmptyText,
       OAuthConstants.Scope -> optional(text)
     )((grantType,clientId,clientSecret,scope) =>
       AccessTokenRequest.apply(grantType.getOrElse(OAuthConstants.ClientCredentials),clientId,clientSecret,scope))
       (AccessTokenRequest.unapply(_).map((atrtuple => (Some(atrtuple._1),atrtuple._2,atrtuple._3,atrtuple._4))))
   )
+
+  def validObjectIdConstraint : Constraint[String] = {
+    def validOid(s:String) = objectId(s).isDefined
+    Constraint[String]({
+     s : String => s match {
+       case _ if s.length == 0 => Invalid(ValidationError("empty"))
+       case oid if !validOid(oid) => Invalid(ValidationError("Invalid object id"))
+       case _ => Valid
+     }
+    })
+  }
 
   /**
    * {
