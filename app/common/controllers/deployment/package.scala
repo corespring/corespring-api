912afa58a50ac276675b15b84c93c4deb7400a72
edeustace <ed.eustace@gmail.com>
2013-06-12 13:47:46 +0200

    update isProd

--- app/common/controllers/deployment/package.scala
+++ app/common/controllers/deployment/package.scala
@@ -2,18 +2,20 @@ package common.controllers
 
 import com.ee.assets.Loader
 import com.ee.assets.deployment.Deployer
-import controllers.ConcreteS3Service
 import common.views.helpers.Defaults
+import controllers.ConcreteS3Service
 import play.api.{Play, Mode}
-import play.api.Play.current
 
 package object deployment {
 
-  lazy val loader : Loader = new Loader(Some(s3Deployer))//if(Play.application.mode == Mode.Prod) new Loader( Some(s3Deployer) ) else new Loader()
+  lazy val loader: Loader = if (isProd) new Loader(Some(s3Deployer)) else new Loader()
 
-  lazy val s3Deployer : Deployer = new S3Deployer(ConcreteS3Service.getAmazonClient, publicBucket)
+  lazy val s3Deployer: Deployer = new S3Deployer(ConcreteS3Service.getAmazonClient, publicBucket)
 
-  private def isProd : Boolean = Play.application.mode == Mode.Prod
+  private def isProd: Boolean = {
+    import play.api.Play.current
+    Play.application.mode == Mode.Prod
+  }
 
-  lazy val publicBucket : String = "corespring-public-assets" + (if(isProd) "-" + Defaults.commitHashShort else "")
+  lazy val publicBucket: String = "corespring-public-assets" + (if (isProd) "-" + Defaults.commitHashShort else "")
 }
