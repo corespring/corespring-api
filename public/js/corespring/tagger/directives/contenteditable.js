ab4bf1e277dfff6277559a37415a52587b3318ff
edeustace <ed.eustace@gmail.com>
2013-06-17 20:04:58 +0200

    fix general lint errors in file

--- public/js/corespring/tagger/directives/contenteditable.js
+++ public/js/corespring/tagger/directives/contenteditable.js
@@ -1,31 +1,33 @@
- angular.module('tagger').directive('contenteditable', function(Collection){ return {
+angular.module('tagger').directive('contenteditable', function (Collection) {
+  "use strict";
+  return {
     restrict: 'A',
     scope: {collectionId: '@collectionId', collectionName: '=collectionName', newAlert: '=newAlert', createSortedCollection: '=createSortedCollection'},
-    link: function(scope,element,attrs){
+    link: function (scope, element, attrs) {
 
-      element.html(scope.collectionName)
+      element.html(scope.collectionName);
 
       // view -> model
-      element.bind('blur', function() {
-        if(scope.collectionName != element.html()){
-          Collection.update({id: scope.collectionId},{name: element.html()},function(data){
-              scope.collectionName = data.name
-              scope.newAlert("alert alert-success", "Successfully edited collection name");
-              scope.createSortedCollection();
-          },function(err){
-              scope.newAlert("alert alert-error", "Error editing collection name");
-              console.log("error update collection name: "+err);
-              element.html(scope.collectionName);
-          })
+      element.bind('blur', function () {
+        if (scope.collectionName !== element.html()) {
+          Collection.update({id: scope.collectionId}, {name: element.html()}, function (data) {
+            scope.collectionName = data.name;
+            scope.newAlert("alert alert-success", "Successfully edited collection name");
+            scope.createSortedCollection();
+          }, function (err) {
+            scope.newAlert("alert alert-error", "Error editing collection name");
+            element.html(scope.collectionName);
+          });
         }
       });
 
-      element.bind('keyup', function(evt){
-        if(evt.which == 13){  //enter key was pressed
-            element.html(element.context.innerText);
-            element.blur();
+      element.bind('keyup', function (evt) {
+        if (evt.which === 13) {  //enter key was pressed
+          element.html(element.context.innerText);
+          element.blur();
         }
-      })
+      });
 
     }
- }});
+  };
+});
