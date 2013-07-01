5caf00aee5fa41fa8961f10e9b33453075eb2849
edeustace <ed.eustace@gmail.com>
2013-07-01 18:23:52 +0100

    remove secure social keys too on logout

--- app/web/controllers/Main.scala
+++ app/web/controllers/Main.scala
@@ -2,28 +2,30 @@ package web.controllers
 
 import common.controllers.session.SessionHandler
 import controllers.auth.BaseApi
+import models.{User, Organization}
 import play.api.mvc._
 import player.accessControl.cookies.{PlayerCookieKeys, PlayerCookieWriter}
 import scala.Some
+import securesocial.core.{SecureSocial, SecuredRequest}
 import web.controllers.utils.ConfigLoader
 import web.models.QtiTemplate
-import models.{User, Organization}
-import com.mongodb.casbah.commons.MongoDBObject
-import securesocial.core.SecuredRequest
 
 
 object Main extends BaseApi with PlayerCookieWriter with SessionHandler {
 
-  def logout(s: Session) : Session = {
-    s - PlayerCookieKeys.RENDER_OPTIONS
+  def logout(s: Session): Session = {
+    s -
+      PlayerCookieKeys.RENDER_OPTIONS -
+      SecureSocial.UserKey -
+      SecureSocial.ProviderKey
   }
 
   def index = SecuredAction {
-    implicit request : SecuredRequest[AnyContent]=>
+    implicit request: SecuredRequest[AnyContent] =>
 
       val (dbServer, dbName) = getDbName(ConfigLoader.get("mongodb.default.uri"))
       val userId = request.user.id
-      val user : User = User.getUser(request.user.id).getOrElse(throw new RuntimeException("Unknown user"))
+      val user: User = User.getUser(request.user.id).getOrElse(throw new RuntimeException("Unknown user"))
       Organization.findOneById(user.org.orgId) match {
         case Some(userOrg) => Ok(web.views.html.index(QtiTemplate.findAll().toList, dbServer, dbName, request.user.fullName, userOrg))
           .withSession(
