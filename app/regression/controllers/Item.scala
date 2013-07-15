57db5d806e24054797f191dc1d8211c569410884
benburton <benjamin.joseph.burton@gmail.com>
2013-07-15 10:05:41 -0400

    Set cookies for organization

--- app/regression/controllers/Item.scala
+++ app/regression/controllers/Item.scala
@@ -2,8 +2,12 @@ package regression.controllers
 
 import play.api.mvc.{Action, Controller}
 import org.bson.types.ObjectId
+import player.accessControl.models.{RenderOptions, RequestedAccess}
+import common.config.AppConfig
+import player.accessControl.cookies.PlayerCookieWriter
+import securesocial.core.java.SecureSocial.UserAwareAction
 
-object Item extends Controller {
+object Item extends Controller with PlayerCookieWriter {
 
   def Secured[A](username: String, password: String)(action: Action[A]) = Action(action.parser) { request =>
     request.headers.get("Authorization").flatMap { authorization =>
@@ -18,11 +22,12 @@ object Item extends Controller {
     }
   }
 
-  def player(itemId: ObjectId) = Secured("admin", "1234secret") {
-    Action { request =>
-      Ok(regression.views.html.player(itemId.toString() + ":0"))
+  def player(orgId: ObjectId, itemId: ObjectId) = Secured("admin", "1234secret") {
+    Action { implicit request =>
+      val newCookies : Seq[(String,String)] = playerCookies(orgId, Some(RenderOptions.ANYTHING)) :+ activeModeCookie(RequestedAccess.Mode.Preview)
+      val newSession = sumSession(request.session, newCookies : _*)
+      Ok(regression.views.html.player(itemId.toString() + ":0")).withSession(newSession)
     }
   }
-
 
 }
