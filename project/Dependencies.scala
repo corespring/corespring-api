325e9373141740ab9d1cf55d9d8c0cdb54e61257
edeustace <ed.eustace@gmail.com>
2013-08-05 09:13:05 +0200

    remove duplicate snapshots

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
     val spy = "Spy Repository" at "http://files.couchbase.com/maven2"
     val sonatypeSnapshots= "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
     val sonatypeReleases = "Sonatype OSS" at "https://oss.sonatype.org/content/repositories/releases"
-    val sbtPluginSnapshots = Resolver.url("sbt-plugin-snapshots", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
+    val sbtPluginSnapshots = Resolver.url("sbt-plugin-releases", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
     val sbtPluginReleases =  Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
     val all: Seq[Resolver] = Seq(
       spy,
