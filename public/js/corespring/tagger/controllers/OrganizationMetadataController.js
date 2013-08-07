0ee3893d87ee6a2a176f0cfeb7f49505a5570c22
Attila Miklosi <attila.miklosi@gmail.com>
2013-08-07 18:28:33 +0200

    integrating external metadata editor into tagger POC WIP

--- public/js/corespring/tagger/controllers/OrganizationMetadataController.js
+++ public/js/corespring/tagger/controllers/OrganizationMetadataController.js
@@ -1,11 +1,20 @@
 function OrganizationMetadataController($scope, MessageBridge) {
   MessageBridge.addMessageListener(function (message) {
-    if (message && message.data && message.data.type == 'setMetadata') {
-      console.log("message received: ", message.data.message);
+    if (message && message.data && message.data.type == 'updateMetadata') {
+      console.log("updating metadata: ", message.data.message);
     }
 
     if (message && message.data && message.data.type == 'requestMetadata') {
       console.log("Metadata Requested");
+
+      // TODO: Send current metadata
+
+      var current = {
+        "rabbit": "fast",
+        "snail": "slow"
+      };
+
+      MessageBridge.sendMessage("externalMetadataEditor", {type: "currentMetadata", message: current}, true);
     }
 
 
