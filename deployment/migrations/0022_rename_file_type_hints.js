c2490256d27cdf1c43097adbc018d6433b7a6261
edeustace <ed.eustace@gmail.com>
2013-08-27 09:59:10 +0200

    fixed migration script (wasn't finding all items)

--- deployment/migrations/0022_rename_file_type_hints.js
+++ deployment/migrations/0022_rename_file_type_hints.js
@@ -1,47 +1,14 @@
 //repackaging has taken place - update type hints
 //models.item.resource => org.corespring.platform.core.models.item.resource
 
-function run(processor){
-
-    function runProcessorAgainstFiles(filesArray, processor) {
-
-        if (!filesArray) {
-            return false;
-        }
+function up() {
+    var processor = function (f) {
         var changed = false;
-        for (var i = 0; i < filesArray.length; i++) {
-            var f = filesArray[i];
-            changed = changed ? true : processor(f);
+        if (f._t && f._t.indexOf("models") == 0) {
+            f._t = "org.corespring.platform.core." + f._t;
+            changed = true;
         }
         return changed;
-    }
-
-    db.content.find({"data.files._t": {$exists: true}}).forEach(function (item) {
-        var changed = runProcessorAgainstFiles(item.data.files, processor );
-        if (changed) {
-            db.content.save(item);
-        }
-    });
-
-    db.content.find({"supportingMaterials.files._t": {$exists: true}}).forEach(function (item) {
-        for( var i = 0 ; i < item.supportingMaterials.length; i++){
-            var sm = item.supportingMaterials[i];
-            var changed = runProcessorAgainstFiles(sm.files, processor);
-            if(changed){
-                db.content.save(item);
-            }
-        }
-    });
-}
-
-
-function up() {
-    var processor = function(f){
-        if(f._t && f._t.indexOf("models") == 0){
-            f._t = "org.corespring.platform.core." + f._t;
-            return true;
-        }
-        return false;
     }
     run(processor);
 }
@@ -55,7 +22,56 @@ function down(){
         return false;
     }
     run(processor);
+}
 
+function run(processor) {
+
+    function runProcessorAgainstFiles(filesArray, processor) {
+
+        if (!filesArray) {
+            return false;
+        }
+        var changed = false;
+        for (var i = 0; i < filesArray.length; i++) {
+            var f = filesArray[i];
+            var fileChanged = processor(f);
+            if(fileChanged){
+                changed = true;
+            }
+        }
+        return changed;
+    }
+
+    var dataFile = {"data.files._t": {$exists: true}};
+    var suFile = {"supportingMaterials.files._t": {$exists: true}};
+
+    var together = { $or: [ dataFile, suFile ]};
+
+    db.content.find(together).forEach(function (i) {
+
+        var changed = false;
+
+        var fileChange = runProcessorAgainstFiles(i.data.files, processor);
+
+        if(fileChange){
+            changed = true;
+        }
+
+        if (i.supportingMaterials) {
+            for (var x = 0; x < i.supportingMaterials.length; x++) {
+                var sm = i.supportingMaterials[x];
+                var smChanged = runProcessorAgainstFiles(sm.files, processor);
+
+                if (smChanged) {
+                    changed = true;
+                }
+            }
+        }
+
+        if (changed) {
+            db.content.save(i);
+        }
+    });
 }
 
 
