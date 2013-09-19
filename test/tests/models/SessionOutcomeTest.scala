ee188593ce72c22a8e9e7150e8541ce0ce163fa6
benburton <benjamin.joseph.burton@gmail.com>
2013-09-19 11:00:20 -0400

    Add support for default scoring values in JS

--- test/tests/models/SessionOutcomeTest.scala
+++ test/tests/models/SessionOutcomeTest.scala
@@ -475,6 +475,67 @@ class SessionOutcomeTest extends Specification {
 
   }
 
+  "contain default response in outcome" in {
+    val qtiItem = itemWithResponseJs(
+      """
+        var returnValue = {
+          score: (Q_01.outcome.score + Q_02.outcome.score) / 2,
+          isCorrect: (Q_01.outcome.isCorrect && Q_02.outcome.isCorrect),
+          isComplete: (Q_01.outcome.isComplete && Q_02.outcome.isComplete),
+          Q_01: {
+            score: parseInt(Q_01.value),
+            isCorrect: false,
+            isComplete: false
+          },
+          Q_02: {
+            score: parseInt(Q_02.value),
+            isCorrect: true,
+            isComplete: true
+          }
+        };
+        returnValue;
+      """)
+
+    val itemSession = ItemSession(
+      itemId = VersionedId("50180807e4b0b89ebc0153b0").get,
+      responses = Seq(StringResponse(id = "Q_01", responseValue = "2"), StringResponse(id = "Q_02", responseValue = "1"))
+    )
+
+    SessionOutcome.processSessionOutcome(itemSession, qtiItem) match {
+      case Success(sessionOutcome: SessionOutcome) => {
+        sessionOutcome.score === 1
+        sessionOutcome.isComplete === true
+        sessionOutcome.isCorrect === true
+        sessionOutcome.identifierOutcomes match {
+          case Some(outcomes) => {
+            outcomes.get("Q_01") match {
+              case Some(q1Outcome) => {
+                q1Outcome.score === 2
+                q1Outcome.isCorrect === false
+                q1Outcome.isComplete === false
+
+                outcomes.get("Q_02") match {
+                  case Some(q2Outcome) => {
+                    q2Outcome.score === 1
+                    q2Outcome.isCorrect === true
+                    q2Outcome.isComplete === true
+                    success
+                  }
+                  case _ => failure("No outcome for identifier Q_02")
+                }
+              }
+              case _ => failure("No outcome for identifier Q_01")
+            }
+          }
+          case _ => failure("No identifier outcomes found")
+        }
+        success
+      }
+      case _ => failure
+    }
+
+  }
+
   private def itemWithResponseJs(js: String): QtiItem = {
     QtiItem(
       <assessmentItem>
@@ -496,10 +557,10 @@ class SessionOutcomeTest extends Specification {
             <inlineChoice identifier="1"/>
             <inlineChoice identifier="2"/>
           </inlineChoiceInteraction>
-          <inlineChiceInteraction responseIdentifier="Q_02">
+          <inlineChoiceInteraction responseIdentifier="Q_02">
             <inlineChoice identifier="1"/>
             <inlineChoice identifier="2"/>
-          </inlineChiceInteraction>
+          </inlineChoiceInteraction>
         </itemBody>
       </assessmentItem>)
   }
