bb23958bae7cc284f821120c7d94de0104359289
bleezmo <joshuarogers10@gmail.com>
2013-10-01 19:15:09 -0400

    item session api calls looks for render options in order to determine the role. if the role is instructor, then correct responses are computed, even if session settings says otherwise.

--- test/tests/models/SessionOutcomeTest.scala
+++ test/tests/models/SessionOutcomeTest.scala
@@ -2,12 +2,15 @@ package tests.models
 
 import org.specs2.mutable.Specification
 import org.corespring.qti.models.QtiItem
-import org.corespring.platform.core.models.itemSession.{ResponseProcessingOutputValidator, SessionOutcome, ItemSession}
+import org.corespring.platform.core.models.itemSession.{DefaultItemSession, ResponseProcessingOutputValidator, SessionOutcome, ItemSession}
 import org.corespring.qti.models.responses.{ArrayResponse, StringResponse}
 import scalaz.{Failure, Success}
 import org.corespring.platform.data.mongo.models.VersionedId
 import org.corespring.platform.core.models.error.InternalError
 import play.api.libs.json.{JsBoolean, JsObject, JsNumber}
+import org.bson.types.ObjectId
+import play.api.test._
+import play.api.test.Helpers._
 
 class SessionOutcomeTest extends Specification {
 
@@ -17,7 +20,11 @@ class SessionOutcomeTest extends Specification {
     attempts = 1)
 
   "SessionOutcome" should {
-
+    "be retrievable from database after persisting" in new WithApplication{
+      val sessionId = DefaultItemSession.insert(ItemSession(itemId = VersionedId(new ObjectId()), outcome = Some(SessionOutcome(1,true,true,Some(Map("blergl" -> SessionOutcome(1,true,true))))))).get
+      val itemSession = DefaultItemSession.findOneById(sessionId).get
+      itemSession.outcome.map(_.identifierOutcomes.get("blergl")) must beSome(SessionOutcome(1,true,true))
+    }
     "return default scoring with no response processing" in {
       val qtiItem =
         QtiItem(
