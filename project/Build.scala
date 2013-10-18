bab579f94ca33314e53cd1e808d5592876d1fca0
edeustace <ed.eustace@gmail.com>
2013-10-18 15:47:32 +0100

    disable buildInfo task

--- project/Build.scala
+++ project/Build.scala
@@ -114,8 +114,8 @@ object Build extends sbt.Build {
 
 
   val commonViews = builders.web("common-views").settings(
-    buildInfo,
-    resourceGenerators in Compile <+= buildInfoTask,
+    //buildInfo,
+    //resourceGenerators in Compile <+= buildInfoTask,
     libraryDependencies ++= Seq(playJson % "test")
   ).dependsOn(core).settings(disableDocsSettings: _*)
 
