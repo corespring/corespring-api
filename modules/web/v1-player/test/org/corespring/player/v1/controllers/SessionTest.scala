cc6e2450628669bbf9066735fbdec0ea686a89c8
edeustace <ed.eustace@gmail.com>
2013-12-12 11:30:37 +0000

    mod work

---
+++ modules/web/v1-player/test/org/corespring/player/v1/controllers/SessionTest.scala
@@ -0,0 +1,62 @@
+
+package org.corespring.player.v1.controllers
+
+import org.bson.types.ObjectId
+import org.corespring.platform.core.models.itemSession.{DefaultItemSession, PreviewItemSessionCompanion, ItemSessionCompanion, ItemSession}
+import org.corespring.player.accessControl.cookies.PlayerCookieWriter
+import org.corespring.player.v1.controllers.controllers.{TestBuilder, TestIds}
+import org.corespring.test.PlaySingleton
+import org.specs2.mutable.Specification
+import play.api.libs.json.Json
+import play.api.mvc.{Result, Action, AnyContentAsJson, AnyContent}
+import play.api.test.Helpers._
+import play.api.test.{FakeRequest, FakeHeaders}
+import org.corespring.player.accessControl.models.RequestedAccess.Mode._
+
+class SessionTest extends Specification with PlayerCookieWriter {
+
+
+  PlaySingleton.start()
+
+  private def request(m: Mode, content: AnyContent) = {
+    implicit val request = FakeRequest("method", "url", FakeHeaders(), content)
+    request.withSession(activeModeCookie(m))
+  }
+
+  private def itemSession = AnyContentAsJson(Json.toJson(ItemSession(TestIds.testId)))
+
+  import org.specs2.execute.{Result => SpecsResult}
+
+  private def assertAction(msg: String, action: Action[AnyContent], r: FakeRequest[AnyContent], idFn: (Result => ObjectId), inPreview: Boolean): SpecsResult = {
+
+    val result = action(r)
+    val id = idFn(result)
+
+    def check(companion: ItemSessionCompanion, expectedToBeThere: Boolean) = {
+      companion.findOneById(id).map {
+        s =>
+          if (expectedToBeThere) success else failure(msg + ": we found the item but didn't expect to")
+      }.getOrElse(if (expectedToBeThere) failure(msg + ": we couldn't find the item") else success)
+    }
+    check(PreviewItemSessionCompanion, inPreview)
+    check(DefaultItemSession, !inPreview)
+  }
+
+  val session = new Session(new TestBuilder)
+  "session" should {
+    "use the preview collection for preview mode only" in {
+
+      "create" in {
+        val action = session.create(TestIds.testId)
+
+        def idFromCreate(r: Result): ObjectId = Json.parse(contentAsString(r)).as[ItemSession].id
+        assertAction("preview::create", action, request(Preview, itemSession), idFromCreate, inPreview = true)
+
+        forallWhen(List(Render, Administer, Aggregate)) {
+          case m => assertAction(m + "::create", action, request(m, itemSession), idFromCreate, inPreview = false).isSuccess === true
+        }
+      }
+    }
+  }
+
+}
---
+++ modules/web/v1-player/test/org/corespring/player/v1/controllers/package.scala
@@ -0,0 +1,28 @@
+package org.corespring.player.v1.controllers
+
+import play.api.mvc.{BodyParser, Action, Result, AnyContent}
+import org.bson.types.ObjectId
+import org.corespring.platform.data.mongo.models.VersionedId
+import org.corespring.player.accessControl.auth.TokenizedRequestActionBuilder
+import org.corespring.player.accessControl.auth.requests.TokenizedRequest
+import org.corespring.player.accessControl.models.RequestedAccess
+
+package object controllers {
+
+  class TestBuilder extends TokenizedRequestActionBuilder[RequestedAccess] {
+    def ValidatedAction(access: RequestedAccess)(block: (TokenizedRequest[AnyContent]) => Result) =
+      ValidatedAction(play.api.mvc.BodyParsers.parse.anyContent)(access)(block)
+
+    def ValidatedAction(p: BodyParser[AnyContent])(access: RequestedAccess)(block: (TokenizedRequest[AnyContent]) => Result): Action[AnyContent] = Action {
+      request =>
+        block(TokenizedRequest("test_token", request))
+    }
+  }
+
+  object TestIds{
+    val testId = VersionedId(new ObjectId("50b653a1e4b0ec03f29344b0"))
+    val testSessionId = new ObjectId("51116bc7a14f7b657a083c1d")
+    val testQuizId = new ObjectId("000000000000000000000001")
+    val testQuizItemId = VersionedId(new ObjectId("5153eee1aa2eefdc1b7a5570"))
+  }
+}
