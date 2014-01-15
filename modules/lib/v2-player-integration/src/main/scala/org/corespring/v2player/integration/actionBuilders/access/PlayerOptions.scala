7c4876f2ed4ba3806be25a10d0c2b350420c6708
edeustace <ed.eustace@gmail.com>
2014-01-15 21:46:25 +0000

    json formatting for PlayerOptions

--- modules/lib/v2-player-integration/src/main/scala/org/corespring/v2player/integration/actionBuilders/access/PlayerOptions.scala
+++ modules/lib/v2-player-integration/src/main/scala/org/corespring/v2player/integration/actionBuilders/access/PlayerOptions.scala
@@ -1,19 +1,19 @@
 package org.corespring.v2player.integration.actionBuilders.access
 
 import org.corespring.v2player.integration.actionBuilders.access.Mode.Mode
-import play.api.libs.json.{Json, JsValue, Writes}
+import play.api.libs.json._
 
 case class PlayerOptions(itemId: String,
                          sessionId: String,
                          secure: Boolean,
                          expires: Option[Long] = None,
-                         mode : Option[String] = None) {
+                         mode: Option[String] = None) {
 
   def allowSessionId(sessionId: String): Boolean = this.sessionId == PlayerOptions.STAR || this.sessionId == sessionId
 
   def allowItemId(itemId: String): Boolean = this.itemId == PlayerOptions.STAR || this.itemId == itemId
 
-  def allowMode(mode:Mode) = this.mode.map{
+  def allowMode(mode: Mode) = this.mode.map {
     m =>
       m == PlayerOptions.STAR || Mode.withName(m) == mode
   }.getOrElse(true)
@@ -23,13 +23,16 @@ object PlayerOptions {
   val STAR = "*"
   val ANYTHING = PlayerOptions(STAR, STAR, false)
 
-  def fromJson(s:String) : Option[PlayerOptions] = None
-
-  implicit object Writes extends Writes[PlayerOptions]{
-    def writes(o: PlayerOptions): JsValue = {
-      Json.obj()
+  def fromJson(s: String) = try {
+    optionsFormat.reads(Json.parse(s)) match {
+      case JsSuccess(o, _) => Some(o)
+      case JsError(errs) => None
     }
+  } catch {
+    case e: Throwable => None
   }
+
+  implicit val optionsFormat = Json.format[PlayerOptions]
 }
 
 
