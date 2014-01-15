ac38822a038f3552d4f7da6b410e613a76a36c27
edeustace <ed.eustace@gmail.com>
2014-01-15 21:45:44 +0000

    plug in decryption and cookie writing

--- modules/lib/v2-player-integration/src/main/scala/org/corespring/v2player/integration/actionBuilders/PlayerLauncherActionBuilder.scala
+++ modules/lib/v2-player-integration/src/main/scala/org/corespring/v2player/integration/actionBuilders/PlayerLauncherActionBuilder.scala
@@ -1,18 +1,40 @@
 package org.corespring.v2player.integration.actionBuilders
 
 import org.bson.types.ObjectId
-import org.corespring.container.client.actions.{PlayerLauncherActionBuilder => Builder, PlayerJsRequest}
+import org.corespring.container.client.actions.PlayerJsRequest
+import org.corespring.container.client.actions.{PlayerLauncherActionBuilder => Builder}
 import org.corespring.v2player.integration.actionBuilders.access.{V2PlayerCookieWriter, PlayerOptions}
 import play.api.mvc.Results._
-import play.api.mvc.{Session, AnyContent, Action, Result}
+import play.api.mvc._
+import scala.Some
 import scalaz.Scalaz._
 import scalaz._
 
+object PlayerLauncherActionBuilder{
+  object Errors{
+    val noClientId = "You must specify 'apiClient'"
+    val noOptions = "You must specify 'options'"
+    val noOrgId = "Error getting orgId"
+    val cantDecrypt = "Error decrypting"
+    val badJson = "Error reading json"
+  }
+}
+
 trait PlayerLauncherActionBuilder extends Builder[AnyContent] with V2PlayerCookieWriter {
 
-  def decrypt(s: String): Option[String]
+  def decrypt(orgId: ObjectId, encrypted: String): Option[String]
 
-  def toOrgId(apiClientId: String): Option[ObjectId]
+  def toOrgId(orgId: String): Option[ObjectId]
+
+  protected def getOrgIdAndOptions(request:Request[AnyContent]) : Validation[String,(ObjectId,PlayerOptions)] = {
+    for {
+      apiClientId <- request.getQueryString("apiClient").toSuccess("You must specify 'apiClient'")
+      encryptedOptions <- request.getQueryString("options").toSuccess("You must specify 'options'")
+      orgId <- toOrgId(apiClientId).toSuccess("Error getting orgId")
+      decryptedOptions <- decrypt(orgId, encryptedOptions).toSuccess("Error decrypting")
+      playerOptions <- PlayerOptions.fromJson(decryptedOptions).toSuccess("Error reading json")
+    } yield (orgId, playerOptions)
+  }
 
   /** A helper method to allow you to create a new session out of the existing and a variable number of Key values pairs */
   override def sumSession(s: Session, keyValues: (String, String)*): Session = {
@@ -21,16 +43,7 @@ trait PlayerLauncherActionBuilder extends Builder[AnyContent] with V2PlayerCooki
 
   def playerJs(block: (PlayerJsRequest[AnyContent]) => Result): Action[AnyContent] = Action {
     request =>
-
-      val result: Validation[String, (ObjectId, PlayerOptions)] = for {
-        apiClientId <- request.getQueryString("apiClient").toSuccess("You must specify 'apiClient'")
-        encryptedOptions <- request.getQueryString("options").toSuccess("You must specify 'options'")
-        decryptedOptions <- decrypt(encryptedOptions).toSuccess("Error decrypting")
-        orgId <- toOrgId(apiClientId).toSuccess("Error getting orgId")
-        playerOptions <- PlayerOptions.fromJson(decryptedOptions).toSuccess("Error reading json")
-      } yield (orgId, playerOptions)
-
-      result match {
+      getOrgIdAndOptions(request) match {
         case Success((orgId, opts)) => {
           val newSession = sumSession(request.session, playerCookies(orgId, Some(opts)): _*)
           block(new PlayerJsRequest(opts.secure, request)).withSession(newSession)
---
+++ modules/lib/v2-player-integration/src/test/scala/org/corespring/v2player/integration/actionBuilders/PlayerLauncherActionBuilderTest.scala
@@ -0,0 +1,7 @@
+package org.corespring.v2player.integration.actionBuilders
+
+import org.specs2.mutable.Specification
+
+class PlayerLauncherActionBuilderTest extends Specification {
+
+}
