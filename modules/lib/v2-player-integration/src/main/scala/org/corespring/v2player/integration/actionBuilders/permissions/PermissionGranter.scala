6afc04ec5183ed04357d053c2bc7f72df4c7b3fd
edeustace <ed.eustace@gmail.com>
2014-01-16 12:40:04 +0000

    test for simple wildcard checker

--- modules/lib/v2-player-integration/src/main/scala/org/corespring/v2player/integration/actionBuilders/permissions/PermissionGranter.scala
+++ modules/lib/v2-player-integration/src/main/scala/org/corespring/v2player/integration/actionBuilders/permissions/PermissionGranter.scala
@@ -2,17 +2,30 @@ package org.corespring.v2player.integration.actionBuilders.permissions
 
 import org.corespring.v2player.integration.actionBuilders.access.PlayerOptions
 import org.corespring.v2player.integration.actionBuilders.access.Mode.Mode
+import org.corespring.v2player.integration.actionBuilders.permissions.SimpleWildcardChecker.Errors
 
 trait PermissionGranter {
   def allow(itemId: String, sessionId: Option[String], mode: Mode, options: PlayerOptions ) : Either[String,Boolean]
 }
 
+object SimpleWildcardChecker{
+  object Errors {
+    val notGrantedMsg = "Permission not granted"
+  }
+}
+
 class SimpleWildcardChecker extends PermissionGranter{
+
   def allow(itemId: String, sessionId: Option[String], mode : Mode, options: PlayerOptions): Either[String, Boolean] = {
 
-    (options.allowItemId(itemId) && sessionId.map(options.allowSessionId(_)).getOrElse(true)) match {
+    (options.allowItemId(itemId)
+      &&
+      sessionId.map(options.allowSessionId(_)).getOrElse(true)
+      &&
+      options.allowMode(mode)
+      ) match {
       case true => Right(true)
-      case false => Left("Permission not granted")
+      case false => Left(Errors.notGrantedMsg)
     }
   }
 }
