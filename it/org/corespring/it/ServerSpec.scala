136abb20342319b7267afa8529442c6df96e8d88
edeustace <ed.eustace@gmail.com>
2014-01-16 20:26:19 +0000

    disable app logging

--- it/org/corespring/it/ServerSpec.scala
+++ it/org/corespring/it/ServerSpec.scala
@@ -5,7 +5,9 @@ import play.api.test.{Port, Helpers, TestServer, FakeApplication}
 trait ServerSpec {
 
   implicit val app: FakeApplication = FakeApplication(
-    additionalPlugins = Seq("se.radley.plugin.salat.SalatPlugin")
+
+    additionalPlugins = Seq("se.radley.plugin.salat.SalatPlugin"),
+    additionalConfiguration = Map("logger" -> Map( "play" -> "OFF", "application" -> "OFF"))
   )
 
   implicit def port: Port = Helpers.testServerPort
