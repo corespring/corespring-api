935b9c56cb060affb4d693b81c3b45d19cb282f7
edeustace <ed.eustace@gmail.com>
2014-01-27 20:41:36 +0000

    get image loading working in new player

--- modules/lib/v2-player-integration/src/main/scala/org/corespring/v2player/integration/V2PlayerIntegration.scala
+++ modules/lib/v2-player-integration/src/main/scala/org/corespring/v2player/integration/V2PlayerIntegration.scala
@@ -1,46 +1,48 @@
 package org.corespring.v2player.integration
 
-import _root_.securesocial.core.{SecureSocial, Identity}
+import _root_.securesocial.core.{ SecureSocial, Identity }
 import com.mongodb.casbah.MongoDB
 import org.bson.types.ObjectId
 import org.corespring.amazon.s3.ConcreteS3Service
 import org.corespring.common.config.AppConfig
-import org.corespring.container.client.controllers.{Assets, ComponentsFileController, Rig, Icons}
+import org.corespring.container.client.controllers.{ Assets, ComponentsFileController, Rig, Icons }
 import org.corespring.container.components.model.Component
 import org.corespring.container.components.model.Library
 import org.corespring.container.components.model.UiComponent
-import org.corespring.container.components.outcome.{DefaultScoreProcessor, ScoreProcessor}
+import org.corespring.container.components.outcome.{ DefaultScoreProcessor, ScoreProcessor }
 import org.corespring.container.components.processing.PlayerItemPreProcessor
-import org.corespring.container.components.processing.rhino.{PlayerItemPreProcessor => RhinoPreProcessor}
+import org.corespring.container.components.processing.rhino.{ PlayerItemPreProcessor => RhinoPreProcessor }
 import org.corespring.container.components.response.OutcomeProcessor
-import org.corespring.container.components.response.rhino.{OutcomeProcessor => RhinoProcessor}
+import org.corespring.container.components.response.rhino.{ OutcomeProcessor => RhinoProcessor }
 import org.corespring.mongo.json.services.MongoService
 import org.corespring.platform.core.controllers.AssetResource
 import org.corespring.platform.core.models.Organization
 import org.corespring.platform.core.models.item.Item
 import org.corespring.platform.core.models.itemSession.PreviewItemSessionCompanion
 import org.corespring.platform.core.services.UserServiceWired
-import org.corespring.platform.core.services.item.{ItemServiceWired, ItemService}
+import org.corespring.platform.core.services.item.{ ItemServiceWired, ItemService }
 import org.corespring.v2player.integration.actionBuilders.access.Mode.Mode
 import org.corespring.v2player.integration.actionBuilders.access.PlayerOptions
 import org.corespring.v2player.integration.actionBuilders.permissions.SimpleWildcardChecker
-import org.corespring.v2player.integration.actionBuilders.{AuthenticatedSessionActionsCheckUserAndPermissions, AuthenticatedSessionActions}
+import org.corespring.v2player.integration.actionBuilders.{ AuthenticatedSessionActionsCheckUserAndPermissions, AuthenticatedSessionActions }
 import org.corespring.v2player.integration.controllers.PlayerLauncher
-import org.corespring.v2player.integration.controllers.editor.{ItemWithBuilder, EditorHooksWithBuilder}
-import org.corespring.v2player.integration.controllers.player.{ClientSessionWithBuilder, PlayerHooksWithBuilder}
+import org.corespring.v2player.integration.controllers.editor.{ ItemWithBuilder, EditorHooksWithBuilder }
+import org.corespring.v2player.integration.controllers.player.{ ClientSessionWithBuilder, PlayerHooksWithBuilder }
 import org.corespring.v2player.integration.securesocial.SecureSocialService
 import org.corespring.v2player.integration.transformers.ItemTransformer
-import play.api.Configuration
+import play.api.{ Logger, Configuration }
 import play.api.libs.json.JsValue
 import play.api.mvc._
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scalaz.Failure
-import scalaz.{Success, Validation}
+import scalaz.{ Success, Validation }
+import org.corespring.platform.data.mongo.models.VersionedId
+import org.corespring.platform.core.models.item.resource.StoredFile
 
+class V2PlayerIntegration(comps: => Seq[Component], rootConfig: Configuration, db: MongoDB) {
 
-class V2PlayerIntegration(comps: => Seq[Component], rootConfig: Configuration, db: MongoDB) extends AssetResource {
-
+  lazy val logger = Logger("v2player.integration")
 
   private lazy val secureSocialService = new SecureSocialService {
     def currentUser(request: Request[AnyContent]): Option[Identity] = SecureSocial.currentUser(request)
@@ -55,10 +57,9 @@ class V2PlayerIntegration(comps: => Seq[Component], rootConfig: Configuration, d
   lazy val controllers: Seq[Controller] = Seq(playerHooks, editorHooks, items, sessions, assets, icons, rig, libs, playerLauncher)
 
   private lazy val playerLauncher: PlayerLauncher = new PlayerLauncher(
-      secureSocialService,
-      UserServiceWired,
-      rootConfig)
-
+    secureSocialService,
+    UserServiceWired,
+    rootConfig)
 
   private lazy val mainSessionService: MongoService = new MongoService(db("v2.itemSessions"))
 
@@ -67,8 +68,7 @@ class V2PlayerIntegration(comps: => Seq[Component], rootConfig: Configuration, d
     UserServiceWired,
     mainSessionService,
     ItemServiceWired,
-    Organization
-  ) {
+    Organization) {
 
     val permissionGranter = new SimpleWildcardChecker()
 
@@ -77,7 +77,6 @@ class V2PlayerIntegration(comps: => Seq[Component], rootConfig: Configuration, d
       case Right(allowed) => Success(true)
     }
   }
-
 
   private lazy val icons = new Icons {
     def loadedComponents: Seq[Component] = comps
@@ -105,15 +104,26 @@ class V2PlayerIntegration(comps: => Seq[Component], rootConfig: Configuration, d
 
     lazy val playS3 = new ConcreteS3Service(key, secret)
 
-    def loadAsset(id: String, file: String)(request: Request[AnyContent]): SimpleResult = {
-      Await.result(getDataFile(id, file)(request), Duration(3000, SECONDS))
+    def loadAsset(itemId: String, file: String)(request: Request[AnyContent]): SimpleResult = {
+
+      val storedFile = for {
+        vid <- VersionedId(itemId)
+        item <- ItemServiceWired.findOneById(vid)
+        data <- item.data
+        asset <- data.files.find(_.name == file)
+      } yield asset.asInstanceOf[StoredFile]
+
+      storedFile.map { sf =>
+        logger.debug(s"loadAsset: itemId: $itemId -> file: $file")
+        playS3.download(bucket, storedFile.get.storageKey, Some(request.headers))
+      }.getOrElse(NotFound(s"Can't find file: $file for itemId $itemId"))
     }
 
     //TODO: Need to look at a way of pre-validating before we upload - look at the predicate?
     def uploadBodyParser(id: String, file: String): BodyParser[Int] = playS3.upload(bucket, s"$id/$file", (rh) => None)
 
-    def getItemId(sessionId: String): Option[String] = PreviewItemSessionCompanion.findOneById(new ObjectId(sessionId)).map {
-      s => s.itemId.toString()
+    def getItemId(sessionId: String): Option[String] = mainSessionService.load(sessionId).map {
+      s => (s \ "itemId").as[String]
     }
   }
 
