202caaf5700e71cc9aaa8deace77e5f693f3b0d1
benburton <benjamin.joseph.burton@gmail.com>
2014-01-28 13:47:59 -0500

    QTI transformer 4 text entry w/ base type equation [#64682332]

--- modules/lib/v2-player-integration/src/test/scala/org/corespring/v2player/integration/transformers/qti/interactions/TextEntryInteractionTransformerTest.scala
+++ modules/lib/v2-player-integration/src/test/scala/org/corespring/v2player/integration/transformers/qti/interactions/TextEntryInteractionTransformerTest.scala
@@ -2,10 +2,13 @@ package org.corespring.v2player.integration.transformers.qti.interactions
 
 import org.specs2.mutable.Specification
 import scala.xml.Node
+import org.corespring.v2player.integration.transformers.qti.interactions.equation.DomainParser
+import play.api.libs.json.Json
 
-class TextEntryInteractionTransformerTest extends Specification {
+class TextEntryInteractionTransformerTest extends Specification with DomainParser {
 
   val identifier = "Q_01"
+  val equationIdentifier = "Q_02"
 
   def qti(correctResponses: Seq[String], correctFeedback: String, incorrectFeedback: String): Node =
     <assessmentItem>
@@ -20,6 +23,22 @@ class TextEntryInteractionTransformerTest extends Specification {
       </itemBody>
     </assessmentItem>
 
+  def equationQti(equation: String, vars: String, domain: String, sigfigs: Int): Node = {
+    val baseType = s"eqn: vars:$vars domain:$domain sigfigs:$sigfigs"
+    <assessmentItem>
+      <responseDeclaration identifier={equationIdentifier} cardinality="single"
+                           baseType={baseType}>
+        <correctResponse>
+          <value>{equation}</value>
+        </correctResponse>
+      </responseDeclaration>
+      <itemBody>
+        <p>This is some info that's in the prompt</p>
+        <textEntryInteraction responseIdentifier={equationIdentifier} expectedLength="15"/>
+      </itemBody>
+    </assessmentItem>
+  }
+
   "TextEntryInteractionTransformer" should {
 
     val correctResponses = Seq("a", "b", "c")
@@ -32,17 +51,42 @@ class TextEntryInteractionTransformerTest extends Specification {
       incorrectFeedback = incorrectFeedback
     )
 
-    val componentsJson = TextEntryInteractionTransformer.interactionJs(input)
+    val interactionResult = TextEntryInteractionTransformer.interactionJs(input).get(identifier)
+      .getOrElse(throw new RuntimeException(s"No component called $identifier"))
 
-    val interactionResult =
-      componentsJson.get(identifier).getOrElse(throw new RuntimeException(s"No component called $identifier"))
+    val equation = "y=2x+7"
+    val vars = "x,y"
+    val domain = "-10->10,0"
+    val sigfigs = 3
+
+    val equationInput = equationQti(equation, vars, domain, sigfigs)
+
+    val equationInteractionResult = TextEntryInteractionTransformer.interactionJs(equationInput).get(equationIdentifier)
+      .getOrElse(throw new RuntimeException(s"No component called $equationIdentifier"))
 
     "return the correct interaction component type" in {
       (interactionResult \ "componentType").as[String] must be equalTo "corespring-text-entry"
+      (equationInteractionResult \ "componentType").as[String] must be equalTo "corespring-function-entry"
     }
 
     "return the correct answers for the interaction" in {
       (interactionResult \ "correctResponse").as[Seq[String]] diff correctResponses must beEmpty
+    }
+
+    "returns the correct correct response vars" in {
+      (equationInteractionResult \ "correctResponse" \ "vars").as[String] must be equalTo vars
+    }
+
+    "returns the correct correct response domain" in {
+      (equationInteractionResult \ "correctResponse" \ "domain") must be equalTo parseDomain(domain)
+    }
+
+    "returns the correct correct response sigfigs" in {
+      (equationInteractionResult \ "correctResponse" \ "sigfigs").as[Int] must be equalTo sigfigs
+    }
+
+    "returns the correct correct response equation" in {
+      (equationInteractionResult \ "correctResponse" \ "equation").as[String] must be equalTo equation
     }
 
   }
