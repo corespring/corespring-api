3a7150745268fa66a18c4b893806597162d66cf2
benburton <benjamin.joseph.burton@gmail.com>
2014-01-28 15:31:19 -0500

    QTI transformer for select text interaction [#64685636]

---
+++ modules/lib/v2-player-integration/src/main/scala/org/corespring/v2player/integration/transformers/qti/interactions/SelectTextInteractionTransformer.scala
@@ -0,0 +1,37 @@
+package org.corespring.v2player.integration.transformers.qti.interactions
+
+import scala.xml._
+import play.api.libs.json._
+
+object SelectTextInteractionTransformer extends InteractionTransformer {
+
+  object Defaults {
+    val shuffle = false
+  }
+
+  override def interactionJs(qti: Node): Map[String, JsObject] = (qti \\ "selectTextInteraction").map(implicit node => {
+    (node \ "@responseIdentifier").text ->
+      Json.obj(
+        "componentType" -> "corespring-select-text",
+        "model" -> Json.obj(
+          "text" -> node.child.mkString,
+          "config" -> partialObj(
+            "selectionUnit" -> optForAttr[JsString]("selectionType"),
+            "shuffle" -> Some(JsBoolean(Defaults.shuffle)),
+            "checkIfCorrect" -> optForAttr[JsString]("checkIfCorrect"),
+            "minSelections" -> optForAttr[JsNumber]("minSelections"),
+            "maxSelections" -> optForAttr[JsNumber]("maxSelections")
+          )
+        )
+      )
+  }).toMap
+
+  override def transform(node: Node): Seq[Node] = node match {
+    case elem: Elem if elem.label == "selectTextInteraction" => {
+      val identifier = (elem \ "@responseIdentifier").text
+        <corespring-select-text id={identifier}>{elem.child}</corespring-select-text>
+    }
+    case _ => node
+  }
+
+}
---
+++ modules/lib/v2-player-integration/src/test/scala/org/corespring/v2player/integration/transformers/qti/interactions/SelectTextInteractionTransformerTest.scala
@@ -0,0 +1,86 @@
+package org.corespring.v2player.integration.transformers.qti.interactions
+
+import org.specs2.mutable.Specification
+import scala.xml.transform.RuleTransformer
+
+class SelectTextInteractionTransformerTest extends Specification {
+
+  val identifier = "Q_01"
+
+  val selectionType = "sentence"
+  val checkIfCorrect = "no"
+  val minSelections = 2
+  val maxSelections = 10
+
+  def qti(selectionText: String) =
+    <assessmentItem>
+      <responseDeclaration identifier={identifier}>
+      </responseDeclaration>
+      <itemBody>
+        <selectTextInteraction responseIdentifier={identifier} selectionType={selectionType}
+                               checkIfCorrect={checkIfCorrect} minSelections={minSelections.toString}
+                               maxSelections={maxSelections.toString}>{selectionText}</selectTextInteraction>
+      </itemBody>
+    </assessmentItem>
+
+  val selectionText =
+    """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+       aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+       fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+       anim id est laborum."""
+
+
+  "OrderInteractionTransformer" should {
+
+    val input = qti(selectionText)
+    val componentsJson = SelectTextInteractionTransformer.interactionJs(input)
+    val output = new RuleTransformer(SelectTextInteractionTransformer).transform(input)
+
+    val interactionResult =
+      componentsJson.get(identifier).getOrElse(throw new RuntimeException(s"No component called $identifier"))
+
+    val config = interactionResult \ "model" \ "config"
+
+    "return the correct interaction component type" in {
+      (interactionResult \ "componentType").as[String] must be equalTo "corespring-select-text"
+    }
+
+    "return the correct config selectionUnit value" in {
+      (config \ "selectionUnit").as[String] must be equalTo selectionType
+    }
+
+    "return the correct config shuffle value" in {
+      (config \ "shuffle").as[Boolean] must be equalTo SelectTextInteractionTransformer.Defaults.shuffle
+    }
+
+    "return the correct config checkIfCorrect value" in {
+      (config \ "checkIfCorrect").as[String] must be equalTo checkIfCorrect
+    }
+
+    "return the correct minSelections value" in {
+      (config \ "minSelections").as[Int] must be equalTo minSelections
+    }
+
+    "return the correct maxSelections value" in {
+      (config \ "maxSelections").as[Int] must be equalTo maxSelections
+    }
+
+
+    "should remove <selectTextInteraction/>" in {
+      (output \\ "selectTextInteraction") must beEmpty
+    }
+
+    "should add <corespring-select-text />" in {
+      (output \\ "corespring-select-text").find(n => (n \ "@id").text == identifier) must not beEmpty
+    }
+
+    "should produce <corespring-select-text/> with all children of previous <selectTextInteraction/>" in {
+      val children = (output \\ "corespring-select-text").find(n => (n \ "@id").text == identifier).get.child
+      children must be equalTo (input \\ "selectTextInteraction").head.child
+    }
+
+
+  }
+
+}
