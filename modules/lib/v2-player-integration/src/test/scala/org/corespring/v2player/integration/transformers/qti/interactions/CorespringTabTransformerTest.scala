4e432e39d1360cee2dca6fe8f9da677666566aab
benburton <benjamin.joseph.burton@gmail.com>
2014-01-29 08:59:35 -0500

    QTI transformer for cs-tabs [#64693500]

---
+++ modules/lib/v2-player-integration/src/test/scala/org/corespring/v2player/integration/transformers/qti/interactions/CorespringTabTransformerTest.scala
@@ -0,0 +1,46 @@
+package org.corespring.v2player.integration.transformers.qti.interactions
+
+import org.specs2.mutable.Specification
+import scala.xml.Node
+import scala.xml.transform.RuleTransformer
+
+class CorespringTabTransformerTest extends Specification {
+
+  def qti(tabData: Map[String, Seq[Node]]) =
+    <assessmentItem>
+      <itemBody>
+        <cs-tabs>
+          { tabData.map{ case(title, child) => <cs-tab title={title}>{child}</cs-tab>} }
+        </cs-tabs>
+      </itemBody>
+    </assessmentItem>
+
+  val tabData = Map(
+    "tabOne" -> <div>This is the content of tab one</div>,
+    "tabTwo" -> <div>...and this is the content of tab two</div>
+  )
+
+  "CorespringTabTransformer" should {
+
+    val input = qti(tabData)
+    val output = new RuleTransformer(CorespringTabTransformer).transform(input)
+
+    "replace <cs-tabs/> with <corespring-tabs/>" in {
+      (output \ "cs-tabs").length must be equalTo 0
+      (input \ "cs-tabs").length must be equalTo (output \ "corespring-tabs").length
+    }
+
+    "replace <cs-tab/> with with <corespring-tab/>" in {
+      (output \ "cs-tab").length must be equalTo 0
+      (input \ "cs-tab").length must be equalTo (output \ "corespring-tab").length
+    }
+
+    "preserve children of <cs-tab/> in <corespring-tab/>" in {
+      ((input \ "cs-tab") zip (output \ "corespring-tab")).map{ case(oldNode, newNode) => {
+        oldNode.child must be equalTo newNode.child
+      }}
+    }
+
+  }
+
+}
