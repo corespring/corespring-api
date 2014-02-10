0433bda59c697143ff8d7cca79ee74edef15198b
benburton <benjamin.joseph.burton@gmail.com>
2014-02-10 17:18:01 -0500

    Improve query to have collections look for their owners

--- deployment/migrations/0039_set_owner_org_id_on_collections.js
+++ deployment/migrations/0039_set_owner_org_id_on_collections.js
@@ -1,23 +1,79 @@
-function up() {
-    var orgs = db.orgs.find();
+/**
+ * Static reference of collection names to known ObjectIds. Used for reability in orgs reference.
+ */
+var collections = {
+    "CoreSpring ELA": ObjectId("4ff2e4cae4b077b9e31689fd"),
+    "CoreSpring Mathematics": ObjectId("4ff2e56fe4b077b9e3168a05"),
+    "archiveColl": ObjectId("500ecfc1036471f538f24bdc"),
+    "Beta Items (Public)": ObjectId("505777f5e4b05f7845735bc1"),
+    "Demo Collection": ObjectId("51114b127fc1eaa866444647"),
+    "LearnZillion": ObjectId("5162d9b9ac1f68b4461138d9")
+};
 
-    orgs.forEach(function(org) {
-        var orgId = org._id;
-        var colls = org.contentcolls;
-        var collsOwnedByOrg = [];
-        for(var i = 0; i < colls.length; i++) {
-            var coll = colls[i];
-            if (coll['pval'] == 3) {
-                collsOwnedByOrg.push(coll.collectionId);
-            }
+/**
+ * Static reference of orgs, their ids, and collections that we know they should own.
+ */
+var orgs = {
+    CoreSpring: {
+        id: ObjectId("502404dd0364dc35bb393398"),
+        owns: [
+            collections["CoreSpring ELA"],
+            collections["CoreSpring Mathematics"],
+            collections["archiveColl"],
+            collections["Beta Items (Public)"],
+            collections["Demo Collection"]
+        ]
+    },
+    LearnZillion: {
+        id: ObjectId("516ffdb6e4b0547bcd4ffdc0"),
+        owns: [
+            collections["LearnZillion"]
+        ]
+    }
+};
+
+/**
+ * Returns true if the collection belongs to one organization, false otherwise
+ */
+function belongsToOneOrg(c) {
+    return db.orgs.count({"contentcolls.collectionId": c._id}) == 1;
+}
+
+/**
+ * Returns the ObjectId of the first found org related to the collection.
+ */
+function getOnlyOrgForCollection(c) {
+    return db.orgs.find({"contentcolls.collectionId": c._id}).next()._id;
+}
+
+/**
+ * Returns the statically available ObjectId of the org owning the collection
+ * (should check belongsToOneOrg first).
+ */
+function getKnownOwnerOfCollection(inputC) {
+    for (org in orgs) {
+        if (orgs[org].owns.map(function(c) { return c.valueOf(); }).indexOf(inputC._id.valueOf()) >= 0) {
+            return orgs[org].id;
         }
-        db.contentcolls.update({"_id": {$in: collsOwnedByOrg} }, { $set: {"ownerOrgId": orgId} }, { multi: true });
-    });
+    }
+    return undefined;
+}
 
-    // handle special collections
-    var corespringOrg = ObjectId("502404dd0364dc35bb393398");
-    var specialCollectionNames = ["archiveColl", "default", "Demo Collection"];
-    db.contentcolls.update({"name": {$in: specialCollectionNames} }, { $set: {"ownerOrgId": corespringOrg} }, { multi: true });
+/**
+ * Returns the ObjectID of the org which should own the collection
+ */
+function getOwnerId(collection) {
+    if (belongsToOneOrg(collection)) {
+        return getOnlyOrgForCollection(collection);
+    } else {
+        return getKnownOwnerOfCollection(collection);
+    }
+}
+
+function up() {
+    db.contentcolls.find().forEach(function(collection) {
+        db.contentcolls.update(collection._id, { $set: { "ownerOrgId": getOwnerId(collection) }});
+    });
 }
 
 function down() {
