ec091d82d1b1ac0b838a6043816222a013475568
rbokel <rbokel.github@qlod.org>
2014-03-05 11:04:15 +0100

    Remove dependency from tests to external unzip tool by adding a unzip implementation

---
+++ test/tests/api/v1/UnzipUtil.scala
@@ -0,0 +1,54 @@
+package tests.api.v1
+
+import java.io._
+import java.util.jar._
+import play.api.Logger
+
+class UnzipUtil {
+
+  val logger = Logger("UnzipUtil")
+
+  def externalUnzip(path:String, folder:String){
+    import sys.process._
+    Seq("unzip", path, "-d", folder).!
+  }
+
+  def unzip(filePath: String, toDir: String): Unit = {
+    unzip(new File(filePath), new File(toDir))
+  }
+
+  def unzip(file: File, todir: File): Unit = {
+    todir.mkdirs()
+
+    logger.info("Extracting " + file + " to " + todir)
+    val jar = new JarFile(file)
+    val enu = jar.entries
+    while (enu.hasMoreElements) {
+      val entry = enu.nextElement
+      val entryPath = entry.getName
+
+      logger.info("Extracting to " + todir + "/" + entryPath)
+      if (entry.isDirectory) {
+        new File(todir, entryPath).mkdirs
+      } else {
+        val istream = jar.getInputStream(entry)
+        val ostream = new FileOutputStream(new File(todir, entryPath))
+        copyStream(istream, ostream)
+        ostream.close
+        istream.close
+      }
+    }
+  }
+
+  private def copyStream(istream: InputStream, ostream: OutputStream): Unit = {
+    var bytes = new Array[Byte](1024)
+    var len = -1
+    while ( {
+      len = istream.read(bytes, 0, 1024);
+      len != -1
+    })
+      ostream.write(bytes, 0, len)
+  }
+
+
+}
