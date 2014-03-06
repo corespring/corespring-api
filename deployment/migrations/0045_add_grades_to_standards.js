3fd073bf0256e7563cefb68ea20708f76113c478
benburton <benjamin.joseph.burton@gmail.com>
2014-03-06 09:14:44 -0500

    Update grades in standards as migration

--- deployment/migrations/0045_add_grades_to_standards.js
+++ deployment/migrations/0045_add_grades_to_standards.js
@@ -1,7 +1,18 @@
+load("bin/javascript.scripts/get_grades.js");
+
 function up() {
-  //'../../conf/seed-data/common/ccstandards.json'
+  var grades = module.exports;
+  db.ccstandards.find().forEach(function(standard) {
+    if (standard.dotNotation && grades.getGrades(standard.dotNotation)) {
+      standard.grades = grades.getGrades(standard.dotNotation);
+      db.ccstandards.save(standard);
+    }
+  });
 }
 
 function down() {
-  // no
+  db.ccstandards.find().forEach(function(standard) {
+    delete standard.grades;
+    db.ccstandards.save(standard);
+  });
 }
