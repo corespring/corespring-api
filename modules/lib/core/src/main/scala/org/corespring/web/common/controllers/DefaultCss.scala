2e96bfdde9e4273e3e8b4e8591518093c7aa0888
edeustace <ed.eustace@gmail.com>
2014-03-20 20:33:56 +0000

    remove Bootstrap class

--- modules/lib/core/src/main/scala/org/corespring/web/common/controllers/DefaultCss.scala
+++ modules/lib/core/src/main/scala/org/corespring/web/common/controllers/DefaultCss.scala
@@ -2,17 +2,15 @@ package org.corespring.web.common.controllers
 
 object DefaultCss {
 
-  private def css(url: String): String = """<link rel="stylesheet" type="text/css" href="%s"/>""".format(url)
-
-  private val BYTE_BUREAU = css("/assets/stylesheets/bytebureau/styles.css")
 
   val DEFAULT_CSS = Seq(BYTE_BUREAU).mkString("\n")
-  val BOOTSTRAP = css(Bootstrap.path("css", "2.0.3"))
+  val BOOTSTRAP = css(bootstrap("css", "2.0.3"))
   val UBUNTU = css("//fonts.googleapis.com/css?family=Ubuntu:400,700,700italic,400italic")
 
+  def bootstrap(suffix: String, version: String = "2.1.1") = s"/assets/bootstrap/$version/$suffix/bootstrap.$suffix"
+
+  private def css(url: String): String = s"""<link rel="stylesheet" type="text/css" href="$url"/>"""
+
+  private val BYTE_BUREAU = css("/assets/stylesheets/bytebureau/styles.css")
 }
 
-object Bootstrap {
-  def path(suffix: String, version: String = "2.1.1") = "/assets/bootstrap/%s/%s/bootstrap.%s".format(version, suffix, suffix)
-
-}
