0b99abdb5d3e59b558a50c76b36a450f8067eb83
edeustace <ed.eustace@gmail.com>
2014-04-10 16:33:07 +0100

    fix failing tests

--- modules/lib/core/src/test/scala/org/corespring/platform/core/models/item/PlayerDefinitionTransformerTest.scala
+++ modules/lib/core/src/test/scala/org/corespring/platform/core/models/item/PlayerDefinitionTransformerTest.scala
@@ -1,6 +1,6 @@
 package org.corespring.platform.core.models.item
 
-import com.mongodb.DBObject
+import com.mongodb.{ BasicDBList, DBObject }
 import com.mongodb.casbah.commons.{ MongoDBList, MongoDBObject }
 import org.corespring.platform.core.models.item.resource.{ StoredFile, VirtualFile }
 import org.corespring.platform.core.models.mongoContext
@@ -13,9 +13,14 @@ class PlayerDefinitionTransformerTest extends Specification {
 
   PlaySingleton.start
 
+  implicit def unwrapDBList(in: MongoDBList): BasicDBList = in.underlying
+
+  val files: BasicDBList = MongoDBList(MongoDBObject())
+
   "transformer" should {
 
     "serialize + deserialize with virtual files" in new serialize(
+
       new PlayerDefinition(
         Seq(VirtualFile("test.js", "text/javascript", false, "hello")),
         "",
@@ -23,13 +28,13 @@ class PlayerDefinitionTransformerTest extends Specification {
       MongoDBObject(
         "xhtml" -> "",
         "components" -> MongoDBObject(),
-        "files" -> MongoDBList(
+        "files" -> unwrapDBList(MongoDBList(
           MongoDBObject(
             "_t" -> "org.corespring.platform.core.models.item.resource.VirtualFile",
             "name" -> "test.js",
             "contentType" -> "text/javascript",
             "isMain" -> false,
-            "content" -> "hello")))) {
+            "content" -> "hello"))))) {
       serialized === dbo
       deserialized === pd
     }
@@ -42,13 +47,13 @@ class PlayerDefinitionTransformerTest extends Specification {
       MongoDBObject(
         "xhtml" -> "",
         "components" -> MongoDBObject(),
-        "files" -> MongoDBList(
+        "files" -> unwrapDBList(MongoDBList(
           MongoDBObject(
             "_t" -> "org.corespring.platform.core.models.item.resource.StoredFile",
             "name" -> "test.js",
             "contentType" -> "text/javascript",
             "isMain" -> false,
-            "storageKey" -> "key")))) {
+            "storageKey" -> "key"))))) {
       serialized === dbo
       deserialized === pd
     }
@@ -56,7 +61,7 @@ class PlayerDefinitionTransformerTest extends Specification {
     "serialize + deserialize json" in new serialize(
       new PlayerDefinition(Nil, "", Json.obj("2" -> Json.obj("name" -> "ed"))),
       MongoDBObject(
-        "files" -> MongoDBList(),
+        "files" -> unwrapDBList(MongoDBList()),
         "xhtml" -> "",
         "components" -> MongoDBObject("2" -> MongoDBObject("name" -> "ed")))) {
       serialized === dbo
