c6f4b40837fda9ba9a14c2840d8b0d160cc6053d
rbokel <rbokel.github@qlod.org>
2014-05-27 19:56:40 +0200

    uncomment js tests

--- project/Commands.scala
+++ project/Commands.scala
@@ -4,7 +4,7 @@ object Commands {
     import scala.sys.process._
     println("!! JS-Tests---------------------------------------------")
     val logger = ProcessLogger(line => println(line), line => println(line))
-    val exitCode: Int = 0 //"./js-test-runner" ! logger
+    val exitCode: Int = "./js-test-runner" ! logger
     println("!! Exit code: " + exitCode)
     println("!! End JS-Tests---------------------------------------------")
     if (exitCode != 0) sys.error("error")
