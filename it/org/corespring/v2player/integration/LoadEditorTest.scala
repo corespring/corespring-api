277107690783d74ed4e6eaf93ffa8b2f99ea4711
Ed Eustace <ed.eustace@gmail.com>
2014-06-11 16:02:23 +0100

    update tests to match url based auth (wip)

--- it/org/corespring/v2player/integration/LoadEditorTest.scala
+++ it/org/corespring/v2player/integration/LoadEditorTest.scala
@@ -1,46 +1,43 @@
 package org.corespring.v2player.integration
 
-import org.corespring.container.client.controllers.routes.PlayerLauncher
-import org.corespring.it.{ IntegrationHelpers, IntegrationSpecification }
+import org.corespring.it.{IntegrationHelpers, IntegrationSpecification}
 import org.corespring.v2player.integration.errors.Errors.noOrgIdAndOptions
 import org.corespring.v2player.integration.scopes.orgWithAccessTokenAndItem
 import org.slf4j.LoggerFactory
 import play.api.mvc._
 import play.api.test.FakeRequest
 
-class LoadEditorJsThenEditorTest
+class LoadEditorTest
   extends IntegrationSpecification
   with IntegrationHelpers {
 
   override val logger: org.slf4j.Logger = LoggerFactory.getLogger("it.load-editor")
 
   "when I load the editor js with orgId and encrypted options" should {
-    "fail if i don't pass in the session" in new loader(false) {
+
+    "fail if i don't add credentials to the url" in new loader(false) {
       status(result) === UNAUTHORIZED
       val err = noOrgIdAndOptions(FakeRequest("", ""))
       contentAsString(result) === org.corespring.container.client.views.html.error.main(err.code, err.message).toString
     }
+
     "allow me to load the editor" in new loader(true) {
       status(result) === OK
     }
   }
 
-  class loader(val addCookies: Boolean) extends orgWithAccessTokenAndItem {
+  class loader(val addCredentials: Boolean) extends orgWithAccessTokenAndItem {
 
     lazy val result = {
-      val url = urlWithEncryptedOptions(PlayerLauncher.editorJs, apiClient)
-
       import org.corespring.container.client.controllers.apps.routes.Editor
 
-      val editItemCall = Editor.editItem(itemId.toString)
-
-      val r = for {
-        jsResult <- getResultFor(FakeRequest("GET", url))
-        requestCookies <- if (addCookies) Some(cookies(jsResult)) else Some(Cookies(None))
-        loadEditorResult <- route(makeRequest(editItemCall, requestCookies))
-      } yield loadEditorResult
-
-      r.getOrElse(throw new RuntimeException("no result"))
+      val editItemCall = {
+        val call = Editor.editItem(itemId.toString)
+        if(addCredentials){
+          Call(call.method, urlWithEncryptedOptions(call, apiClient))
+        } else call
+      }
+      route(makeRequest(editItemCall, Cookies(None))).getOrElse(throw new RuntimeException("Error calling route"))
     }
   }
 
--- it/org/corespring/v2player/integration/LoadPlayerJsThenLoadPlayerTest.scala
+++ it/org/corespring/v2player/integration/LoadPlayerJsThenLoadPlayerTest.scala
@@ -17,19 +17,18 @@ class LoadPlayerJsThenLoadPlayerTest
   import org.corespring.container.client.controllers.PlayerLauncher
   import org.corespring.container.client.controllers.apps.Player
 
-  "when I load the player js with orgId and options" should {
+  "when I load the player with orgId and options" should {
 
-    "load js with no errors" in new LoadJsAndCreateSession("js no errors") {
+    /*"load with no errors" in new LoadJsAndCreateSession("js no errors") {
       contentAsString(jsResult).contains("exports.hasErrors = false;") === true
-    }
+    }*/
 
-    "fail if i don't pass in the session" in new LoadJsAndCreateSession("fail 1", false) {
-      contentAsString(jsResult).contains("exports.hasErrors = false;") === true
+    "fail if i don't pass credentials to the query params" in new LoadJsAndCreateSession("fail 1", false) {
       status(createSessionResult) === SEE_OTHER
       headers(createSessionResult).get("Location") === Some("/login")
     }
 
-    "allow me to create a session" in new LoadJsAndCreateSession("allow 1", true) {
+    /*"allow me to create a session" in new LoadJsAndCreateSession("allow 1", true) {
       status(createSessionResult) === SEE_OTHER
       headers(createSessionResult).get("Location").get.contains("/player")
     }
@@ -38,34 +37,34 @@ class LoadPlayerJsThenLoadPlayerTest
       status(loadPlayerResult) === OK
       val expected = scala.io.Source.fromURL(Play.resource("/container-client/player.dev.html").get).getLines.mkString("\n")
       contentAsString(loadPlayerResult) === expected
-    }
+    }*/
   }
 
-  class LoadJsAndCreateSession(name: String, addCookies: Boolean = false) extends orgWithAccessTokenAndItem {
+  class LoadJsAndCreateSession(name: String, addCredentials: Boolean = false) extends orgWithAccessTokenAndItem {
 
     protected def global: GlobalSettings = Play.current.global
 
-    lazy val jsResult = {
+    /*lazy val jsResult = {
       val url = urlWithEncryptedOptions("", apiClient)
       val launcher = global.getControllerInstance(classOf[PlayerLauncher])
       launcher.playerJs(FakeRequest(GET, url))
-    }
+    }*/
 
     lazy val createSessionResult: Future[SimpleResult] = {
       val player = global.getControllerInstance(classOf[Player])
       val createSession = player.createSessionForItem(itemId.toString)
-      val jsCookies = if (addCookies) cookies(jsResult) else Cookies(None)
-      createSession(FakeRequest("", "").withCookies(jsCookies.toSeq: _*))
+      val url = if (addCredentials) urlWithEncryptedOptions("", apiClient) else ""
+      createSession(FakeRequest("", url))
     }
 
-    def urlWithEncryptedOptions(call: String, apiClient: ApiClient, options: PlayerOptions = PlayerOptions.ANYTHING) = {
+    def urlWithEncryptedOptions(url: String, apiClient: ApiClient, options: PlayerOptions = PlayerOptions.ANYTHING) = {
       val options = Json.stringify(Json.toJson(PlayerOptions.ANYTHING))
       val encrypted = AESCrypto.encrypt(options, apiClient.clientSecret)
-      s"${call}?apiClient=${apiClient.clientId}&options=$encrypted"
+      s"${url}?apiClient=${apiClient.clientId}&options=$encrypted"
     }
   }
 
-  class LoadJsAndCreateSessionAndLoadPlayer(name: String, addCookies: Boolean) extends LoadJsAndCreateSession(name, addCookies) {
+  /*class LoadJsAndCreateSessionAndLoadPlayer(name: String, addCookies: Boolean) extends LoadJsAndCreateSession(name, addCookies) {
     lazy val loadPlayerResult: Future[SimpleResult] = {
       val redirect = headers(createSessionResult).get("Location").getOrElse(throw new RuntimeException("Error getting location"))
       //TODO: We are avoiding calling `route` here - is there a nicer way to get the Action?
@@ -76,5 +75,5 @@ class LoadPlayerJsThenLoadPlayerTest
       val out = c.loadPlayerForSession(id)(FakeRequest(GET, redirect).withCookies(cookies(jsResult).toSeq: _*))
       out
     }
-  }
+  }*/
 }
