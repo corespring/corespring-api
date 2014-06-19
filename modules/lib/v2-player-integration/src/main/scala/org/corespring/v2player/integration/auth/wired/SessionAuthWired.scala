ed7fc5a088f2fed94a3c73b8be37c0cf0d089c47
Ed Eustace <ed.eustace@gmail.com>
2014-06-19 13:56:30 +0100

    SessionAuthWired test

--- modules/lib/v2-player-integration/src/main/scala/org/corespring/v2player/integration/auth/wired/SessionAuthWired.scala
+++ modules/lib/v2-player-integration/src/main/scala/org/corespring/v2player/integration/auth/wired/SessionAuthWired.scala
@@ -3,10 +3,12 @@ package org.corespring.v2player.integration.auth.wired
 import org.corespring.mongo.json.services.MongoService
 import org.corespring.platform.core.models.item.Item
 import org.corespring.v2player.integration.auth.{ ItemAuth, SessionAuth }
+import org.corespring.v2player.integration.errors.Errors.{ noItemIdInSession, cantLoadSession }
 import play.api.libs.json.JsValue
 import play.api.mvc.RequestHeader
 
 import scalaz.{ Failure, Validation }
+import scalaz.Scalaz._
 
 trait SessionAuthWired extends SessionAuth {
 
@@ -15,18 +17,22 @@ trait SessionAuthWired extends SessionAuth {
   def sessionService: MongoService
 
   override def loadForRead(sessionId: String)(implicit header: RequestHeader): Validation[String, (JsValue, Item)] = {
-    sessionService.load(sessionId).map { json =>
-      val v: Validation[String, Item] = itemAuth.loadForRead((json \ "itemId").as[String])
-      v.map(i => (json, i))
-    }.getOrElse(Failure(s"Can't find session with id: $sessionId"))
+    loadFor(sessionId, itemAuth.loadForRead)
   }
 
   override def loadForWrite(sessionId: String)(implicit header: RequestHeader): Validation[String, (JsValue, Item)] = {
-    sessionService.load(sessionId).map { json =>
-      val v: Validation[String, Item] = itemAuth.loadForWrite((json \ "itemId").as[String])
-      v.map(i => (json, i))
-    }.getOrElse(Failure(s"Can't find session with id: $sessionId"))
+    loadFor(sessionId, itemAuth.loadForWrite)
   }
 
-  override def canCreate(itemId: String)(implicit header: RequestHeader): Validation[String, Boolean] = Failure("TODO")
+  private def loadFor(sessionId: String, loadItem: (String) => Validation[String, Item])(implicit header: RequestHeader): Validation[String, (JsValue, Item)] = {
+    for {
+      json <- sessionService.load(sessionId).toSuccess(cantLoadSession(sessionId).message)
+      itemId <- (json \ "itemId").asOpt[String].toSuccess(noItemIdInSession(sessionId).message)
+      item <- loadItem(itemId)
+    } yield (json, item)
+  }
+
+  override def canCreate(itemId: String)(implicit header: RequestHeader): Validation[String, Boolean] = {
+    itemAuth.loadForRead(itemId).map { i => true }
+  }
 }
