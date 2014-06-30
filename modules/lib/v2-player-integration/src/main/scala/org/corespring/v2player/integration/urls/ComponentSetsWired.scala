0a02b8746bcfcc59275d76a3ae970a361a02fc49
Attila Miklosi <attila.miklosi@gmail.com>
2014-06-30 16:20:14 +0200

    use Cache instead of Cached as that was not working with memcache

--- modules/lib/v2-player-integration/src/main/scala/org/corespring/v2player/integration/urls/ComponentSetsWired.scala
+++ modules/lib/v2-player-integration/src/main/scala/org/corespring/v2player/integration/urls/ComponentSetsWired.scala
@@ -1,11 +1,13 @@
 package org.corespring.v2player.integration.urls
 
-import org.corespring.container.client.component.{ CatalogGenerator, EditorGenerator, PlayerGenerator, SourceGenerator }
+import org.corespring.container.client.component.{CatalogGenerator, EditorGenerator, PlayerGenerator, SourceGenerator}
 import org.corespring.container.client.controllers.ComponentSets
 import org.corespring.container.components.model.Component
 import org.corespring.container.components.model.dependencies.DependencyResolver
 import play.api.cache.Cached
-import play.api.{ Mode, Play }
+import play.api.{Mode, Play}
+import play.api.cache.Cache
+import play.api.mvc._
 
 trait ComponentSetsWired extends ComponentSets {
 
@@ -18,8 +20,19 @@ trait ComponentSetsWired extends ComponentSets {
       super.resource(context, directive, suffix)
     } else {
       implicit val current = play.api.Play.current
-      Cached(s"$context-$directive-$suffix") {
-        super.resource(context, directive, suffix)
+
+      val cacheKey = s"$context-$directive-$suffix"
+
+      val res = Cache.get(cacheKey) match {
+        case Some(value) => value.asInstanceOf[(String, String)]
+        case None =>
+          val bodyAndContentType = super.generateBodyAndContentType(context, directive, suffix)
+          Cache.set(cacheKey, bodyAndContentType)
+          bodyAndContentType
+      }
+
+      Action {
+        Ok(res._1).as(res._2)
       }
     }
   }
