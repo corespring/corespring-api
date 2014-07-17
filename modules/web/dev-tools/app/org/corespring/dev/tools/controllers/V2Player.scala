9978f6f1f5e951970c88a248d108cfc5f446a7cc
benburton <benjamin.joseph.burton@gmail.com>
2014-07-17 14:41:20 -0400

    Reference BasePlayer; new corespring-container name

--- modules/web/dev-tools/app/org/corespring/dev/tools/controllers/V2Player.scala
+++ modules/web/dev-tools/app/org/corespring/dev/tools/controllers/V2Player.scala
@@ -8,7 +8,7 @@ object V2Player extends Controller {
   def v2Player(itemId: String) = Action {
     request =>
       if (DevTools.enabled) {
-        val call = org.corespring.container.client.controllers.apps.routes.Player.createSessionForItem(itemId)
+        val call = org.corespring.container.client.controllers.apps.routes.BasePlayer.createSessionForItem(itemId)
         SeeOther(s"${call.url}?file=container-player.html").withSession((DevTools.key -> "true"))
       } else NotFound("?")
   }
