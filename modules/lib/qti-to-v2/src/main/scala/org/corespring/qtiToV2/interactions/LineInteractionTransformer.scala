bc1a760b81220fe381a59ad94b689f3eee3d55e6
Ed Eustace <ed.eustace@gmail.com>
2014-08-04 18:48:52 +0100

    tweak transformer to handle missing response declaration

--- modules/lib/qti-to-v2/src/main/scala/org/corespring/qtiToV2/interactions/LineInteractionTransformer.scala
+++ modules/lib/qti-to-v2/src/main/scala/org/corespring/qtiToV2/interactions/LineInteractionTransformer.scala
@@ -7,31 +7,46 @@ import play.api.libs.json._
 object LineInteractionTransformer extends InteractionTransformer {
 
   override def interactionJs(qti: Node) = (qti \\ "lineInteraction").map(implicit node => {
+
     val exhibit = booleanFor("locked", default = false)
-    (node \\ "@responseIdentifier").text ->
-      Json.obj(
-        "componentType" -> "corespring-line",
-        "correctResponse" -> (responseDeclaration(node, qti) \ "correctResponse" \ "value").map(_.text).mkString(""),
-        "model" -> Json.obj(
-          "config" -> partialObj(
-            "domain" -> optForAttr[JsNumber]("domain"),
-            "range" -> optForAttr[JsNumber]("range"),
-            "scale" -> optForAttr[JsNumber]("scale"),
-            "domainLabel" -> optForAttr[JsString]("domain-label"),
-            "rangeLabel" -> optForAttr[JsString]("range-label"),
-            "tickLabelFrequency" -> optForAttr[JsNumber]("tick-label-frequency"),
-            "sigfigs" -> optForAttr[JsNumber]("sigfigs"),
-            "initialValues" -> ((node \ "graphline" \\ "point") match {
-              case empty: Seq[Node] if empty.isEmpty => None
-              case nodes: Seq[Node] => Some(JsArray(nodes.map(n => JsString(n.text))))
-            }),
-            "initialCurve" -> ((node \ "graphcurve").text match {
-              case curve: String if curve.nonEmpty => Some(JsString(curve))
-              case _ => None
-            }),
-            "exhibitOnly" -> Some(JsBoolean(exhibit)),
-            "showInputs" -> Some(JsBoolean(booleanFor("show-inputs") && !exhibit)),
-            "showLabels" -> Some(JsBoolean(booleanFor("show-labels") && !exhibit)))))
+
+    /**
+     * Correct Response aren't required if locked=""
+     * or noninteractive="" is an attribute.
+     */
+    val correctResponse: JsObject = {
+      try {
+        Json.obj("correctResponse" ->
+          (responseDeclaration(node, qti) \ "correctResponse" \ "value").map(_.text).mkString(""))
+      } catch {
+        case e: Throwable => if (exhibit) Json.obj() else throw e
+      }
+    }
+
+    val main = Json.obj(
+      "componentType" -> "corespring-line",
+      "model" -> Json.obj(
+        "config" -> partialObj(
+          "domain" -> optForAttr[JsNumber]("domain"),
+          "range" -> optForAttr[JsNumber]("range"),
+          "scale" -> optForAttr[JsNumber]("scale"),
+          "domainLabel" -> optForAttr[JsString]("domain-label"),
+          "rangeLabel" -> optForAttr[JsString]("range-label"),
+          "tickLabelFrequency" -> optForAttr[JsNumber]("tick-label-frequency"),
+          "sigfigs" -> optForAttr[JsNumber]("sigfigs"),
+          "initialValues" -> ((node \ "graphline" \\ "point") match {
+            case empty: Seq[Node] if empty.isEmpty => None
+            case nodes: Seq[Node] => Some(JsArray(nodes.map(n => JsString(n.text))))
+          }),
+          "initialCurve" -> ((node \ "graphcurve").text match {
+            case curve: String if curve.nonEmpty => Some(JsString(curve))
+            case _ => None
+          }),
+          "exhibitOnly" -> Some(JsBoolean(exhibit)),
+          "showInputs" -> Some(JsBoolean(booleanFor("show-inputs") && !exhibit)),
+          "showLabels" -> Some(JsBoolean(booleanFor("show-labels") && !exhibit)))))
+
+    (node \\ "@responseIdentifier").text -> (main ++ correctResponse)
   }).toMap
 
   override def transform(node: Node): Seq[Node] = node match {
