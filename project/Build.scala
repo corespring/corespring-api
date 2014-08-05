2f26c7b98fc85a491e39bcfdd86740da045491d9
Ed Eustace <ed.eustace@gmail.com>
2014-08-05 14:29:47 +0100

    rm js processing dep

--- project/Build.scala
+++ project/Build.scala
@@ -131,8 +131,7 @@ object Build extends sbt.Build {
 
   /** Qti -> v2 transformers */
   val qtiToV2 = builders.lib("qti-to-v2").settings(
-    libraryDependencies ++= Seq(
-      playJson, containerJsProcessing % "test")).dependsOn(core, qti)
+    libraryDependencies ++= Seq(playJson)).dependsOn(core, qti)
 
   val v1Api = builders.web("v1-api").settings(
     libraryDependencies ++= Seq(casbah),
