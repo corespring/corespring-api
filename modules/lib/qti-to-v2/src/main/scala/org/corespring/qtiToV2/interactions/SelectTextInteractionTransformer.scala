ca1fe024f3efab5e0d25765c8f05aaaf2cc14bc5
Attila Miklosi <attila.miklosi@gmail.com>
2014-08-12 19:03:59 +0200

    select text interaction transformer now correctly transforms words and sentences to v2 data format

--- modules/lib/qti-to-v2/src/main/scala/org/corespring/qtiToV2/interactions/SelectTextInteractionTransformer.scala
+++ modules/lib/qti-to-v2/src/main/scala/org/corespring/qtiToV2/interactions/SelectTextInteractionTransformer.scala
@@ -22,19 +22,19 @@ object SelectTextInteractionTransformer extends InteractionTransformer {
             "selectionUnit" -> optForAttr[JsString]("selectionType"),
             "checkIfCorrect" -> optForAttr[JsString]("checkIfCorrect"),
             "minSelections" -> optForAttr[JsNumber]("minSelections"),
-            "maxSelections" -> optForAttr[JsNumber]("maxSelections"))))
+            "maxSelections" -> optForAttr[JsNumber]("maxSelections"),
+            "showFeedback" -> Some(JsBoolean(false)))))
   }).toMap
 
   override def transform(node: Node): Seq[Node] = node match {
     case elem: Elem if elem.label == "selectTextInteraction" => {
       val identifier = (elem \ "@responseIdentifier").text
-      <corespring-select-text id={ identifier }>{ clearNamespace(elem.child) }</corespring-select-text>
+      <corespring-select-text id={ identifier }></corespring-select-text>
     }
     case _ => node
   }
 
   private def choices(implicit node: Node): JsArray = {
-
     def isCorrect(string: String) = {
       XML.loadString("<div>" + string + "</div>") \ "correct" match {
         case empty: NodeSeq if empty.length == 0 => false
@@ -42,11 +42,11 @@ object SelectTextInteractionTransformer extends InteractionTransformer {
       }
     }
     def stripCorrectness(string: String) =
-      if (isCorrect(string)) (XML.loadString("<div>" + string + "</div>") \ "correct").text else string
+      string.replaceAll("<[/]*correct>","")
 
     val text = clearNamespace(node.child).mkString
     val choices = optForAttr[JsString]("selectionType") match {
-      case Some(selection) if selection == "word" => TextSplitter.words(text)
+      case Some(selection) if selection.equals(JsString("word")) => TextSplitter.words(text)
       case _ => TextSplitter.sentences(text)
     }
 
@@ -62,16 +62,32 @@ object TextSplitter {
 
   def sentences(s: String): Seq[String] = {
     val regExp = new Regex("(?s)(.*?[.!?]([^ \\t])*)", "match")
-    var idx = 0
     // Filter out names like Vikram S. Pandit as they break the sentence parsing
     val namesParsed = "([A-Z][a-z]+ [A-Z])\\.( [A-Z][a-z]+)".r.replaceAllIn(s, "$1&#46;$2")
     regExp.findAllMatchIn(namesParsed).map({m => m.group("match").trim }).toList
   }
 
   def words(s: String): Seq[String] = {
-    val regExp = new Regex("(?<![</&])\\b([a-zA-Z_']+)\\b", "match")
-    var idx = 0
-    regExp.findAllMatchIn(s).map({m => m.group("match").trim }).toList
+    def getWordsWithXmlTags(node: Node, path: Seq[Node]):Seq[String] = {
+      if (node.isInstanceOf[Text]) {
+        val l = path.map(_.label).dropRight(1).reverse
+        def textWithNeighbouringTags(left:Seq[String], res:String):String = {
+          if (left.isEmpty) {
+            res
+          } else {
+            val elem = left.head
+            s"<${elem}>${textWithNeighbouringTags(left.tail, res)}</${elem}>"
+          }
+        }
+
+        val textWithTags = textWithNeighbouringTags(l, node.text.trim)
+        s"\\S+".r.findAllIn(textWithTags).toList
+      } else {
+        node.child.map { n => getWordsWithXmlTags(n, Seq(node) ++ path) }.flatten
+      }
+    }
+    val inputXml = XML.loadString(s"<span>${s}</span>")
+    getWordsWithXmlTags(inputXml, Seq())
   }
 
 }
