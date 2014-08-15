6a153c47b597a6369f7e8c35f81b44b8b4be7c39
edeustace <ed.eustace@gmail.com>
2014-08-15 17:52:55 +0100

    update to use 0.5 salat versioning dao

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
   val rhino = "org.mozilla" % "rhino" % "1.7R4"
   val salat = "com.novus" %% "salat" % "1.9.4"
   val salatPlay = "se.radley" %% "play-plugins-salat" % "1.4.0"
-  val salatVersioningDao = "org.corespring" %% "salat-versioning-dao" % "0.4"
+  val salatVersioningDao = "org.corespring" %% "salat-versioning-dao" % "0.5"
   val scalaFaker = "it.justwrote" %% "scala-faker" % "0.2"
   val scalaz = "org.scalaz" %% "scalaz-core" % "7.0.6"
   val securesocial = "org.corespring" %% "securesocial" % "master-22044d6"
