eaec4ff0bed08c697f87aaac6167e99ee9174fc9
benburton <benjamin.joseph.burton@gmail.com>
2014-08-19 10:25:23 -0400

    Bump container version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val playVersion = "2.2.1"
 
   //V2 Player
-  val containerVersion = "0.5.5"
+  val containerVersion = "0.5.7"
   val containerClientWeb = "org.corespring" %% "container-client-web" % containerVersion
   val containerJsProcessing = "org.corespring" %% "js-processing" % containerVersion
   val componentModel = "org.corespring" %% "component-model" % containerVersion
