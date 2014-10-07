95fb2db9596188e1305f3bd8606c4a974c4d53ba
Ed Eustace <ed.eustace@gmail.com>
2014-10-07 13:58:28 +0100

    new integration test for CA-1947

---
+++ it/org/corespring/tagger/CreateNewVersionTest.scala
@@ -0,0 +1,13 @@
+package org.corespring.tagger
+
+import org.corespring.it.IntegrationSpecification
+
+class CreateNewVersionTest extends IntegrationSpecification {
+
+  "When saving a new version" should {
+
+    "there should be a versioned item and a current item" in {
+
+    }
+  }
+}
