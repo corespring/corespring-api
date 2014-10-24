421355047b54deeb0551996f883b828f67d12fee
ed eustace <ed.eustace@gmail.com>
2014-10-24 19:17:40 +0100

    Update Dependencies.scala
    Update release to 0.8.4 of container as requested by @dmitry

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val playVersion = "2.2.1"
 
   //V2 Player
-  val containerVersion = "0.8.3"
+  val containerVersion = "0.8.4"
   val containerClientWeb = "org.corespring" %% "container-client-web" % containerVersion
   val containerJsProcessing = "org.corespring" %% "js-processing" % containerVersion
   val componentModel = "org.corespring" %% "component-model" % containerVersion
