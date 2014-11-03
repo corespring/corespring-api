03c4715bd31944e6da93359335317b4837e52e29
edeustace <ed.eustace@gmail.com>
2014-11-03 14:16:50 +0000

    slim down logic - was fine before

--- modules/lib/v2-auth/src/main/scala/org/corespring/v2/auth/services/caching/CachingTokenService.scala
+++ modules/lib/v2-auth/src/main/scala/org/corespring/v2/auth/services/caching/CachingTokenService.scala
@@ -2,20 +2,19 @@ package org.corespring.v2.auth.services.caching
 
 import org.corespring.platform.core.models.Organization
 import org.corespring.v2.auth.services.TokenService
-import org.corespring.v2.errors.Errors.generalError
 import org.corespring.v2.errors.V2Error
 import org.corespring.v2.log.V2LoggerFactory
 import play.api.mvc.RequestHeader
 import spray.caching.Cache
 
-import scala.concurrent.{Future, ExecutionContext, Await}
+import scala.concurrent.Await
 import scala.concurrent.duration._
-import scalaz.{Success, Validation}
+import scalaz.Validation
 
 trait CachingTokenService extends TokenService{
 
 
-  import ExecutionContext.Implicits.global
+  import scala.concurrent.ExecutionContext.Implicits.global
 
   private val logger = V2LoggerFactory.getLogger("CachingTokenService")
 
@@ -25,26 +24,16 @@ trait CachingTokenService extends TokenService{
 
   private def timeToLive : Duration = timeToLiveInMinutes.minutes
 
-  private val cache: Cache[Organization] = spray.caching.LruCache(timeToLive = timeToLive)
+  private val cache: Cache[Validation[V2Error,Organization]] = spray.caching.LruCache(timeToLive = timeToLive)
 
   override def orgForToken(token: String)(implicit rh: RequestHeader): Validation[V2Error, Organization] = {
 
-    cache.get(token) match {
-      case Some(futureOrg) => {
-        logger.debug(s"found org in cache for token: $token")
-        Await.result(futureOrg.map(Success(_)), 5.seconds)
-      }
-      case _ => {
-        val out = underlying.orgForToken(token)
-        out match {
-          case Success(o) => {
-            logger.debug(s"found org in underlying for token $token - putting into cache")
-            cache.apply(token, () => Future{o})
-          }
-          case _ => //do nothing
-        }
-        out
-      }
+    def callUnderlying = {
+      logger.debug(s"token=$token - call underlying service")
+      underlying.orgForToken(token)
     }
+
+    Await.result(cache(token){ callUnderlying }, 5.seconds)
   }
+
 }
