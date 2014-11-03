fa8cf9a1c0fd98b02b4d1b2686bb88bc631d6f41
edeustace <ed.eustace@gmail.com>
2014-11-03 13:58:58 +0000

    add logging

--- modules/lib/v2-auth/src/main/scala/org/corespring/v2/auth/services/caching/CachingTokenService.scala
+++ modules/lib/v2-auth/src/main/scala/org/corespring/v2/auth/services/caching/CachingTokenService.scala
@@ -2,17 +2,22 @@ package org.corespring.v2.auth.services.caching
 
 import org.corespring.platform.core.models.Organization
 import org.corespring.v2.auth.services.TokenService
+import org.corespring.v2.errors.Errors.generalError
 import org.corespring.v2.errors.V2Error
+import org.corespring.v2.log.V2LoggerFactory
 import play.api.mvc.RequestHeader
 import spray.caching.Cache
 
 import scala.concurrent.{Future, ExecutionContext, Await}
 import scala.concurrent.duration._
-import scalaz.Validation
+import scalaz.{Success, Validation}
 
 trait CachingTokenService extends TokenService{
 
+
   import ExecutionContext.Implicits.global
+
+  private val logger = V2LoggerFactory.getLogger("CachingTokenService")
 
   def underlying : TokenService
 
@@ -20,9 +25,26 @@ trait CachingTokenService extends TokenService{
 
   private def timeToLive : Duration = timeToLiveInMinutes.minutes
 
-  private val cache: Cache[Validation[V2Error,Organization]] = spray.caching.LruCache(timeToLive = timeToLive)
+  private val cache: Cache[Organization] = spray.caching.LruCache(timeToLive = timeToLive)
 
   override def orgForToken(token: String)(implicit rh: RequestHeader): Validation[V2Error, Organization] = {
-    Await.result(cache(token,  () => Future{underlying.orgForToken(token)} ), 5.seconds)
+
+    cache.get(token) match {
+      case Some(futureOrg) => {
+        logger.debug(s"found org in cache for token: $token")
+        Await.result(futureOrg.map(Success(_)), 5.seconds)
+      }
+      case _ => {
+        val out = underlying.orgForToken(token)
+        out match {
+          case Success(o) => {
+            logger.debug(s"found org in underlying for token $token - putting into cache")
+            cache.apply(token, () => Future{o})
+          }
+          case _ => //do nothing
+        }
+        out
+      }
+    }
   }
 }
