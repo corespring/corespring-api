79520781b3aee798092789f576495f72debd2e9d
benburton <benjamin.joseph.burton@gmail.com>
2014-11-13 12:02:11 -0500

    Add transformer for match interaction

--- modules/lib/qti-to-v2/src/main/scala/org/corespring/qtiToV2/kds/interactions/MatchInteractionTransformer.scala
+++ modules/lib/qti-to-v2/src/main/scala/org/corespring/qtiToV2/kds/interactions/MatchInteractionTransformer.scala
@@ -2,13 +2,58 @@ package org.corespring.qtiToV2.kds.interactions
 
 import org.corespring.qtiToV2.interactions.InteractionTransformer
 import play.api.libs.json._
-
-import scala.xml.Node
+import scala.xml._
 
 object MatchInteractionTransformer extends InteractionTransformer {
 
-  override def interactionJs(qti: Node): Map[String, JsObject] = (qti \ "matchInteraction").map(node => {
-    (node \ "responseIdentifier").text -> Json.obj()
+  val DefaultCornerText = ""
+
+  override def interactionJs(qti: Node): Map[String, JsObject] = (qti \\ "matchInteraction").map(implicit node => {
+    (node \ "@responseIdentifier").text -> Json.obj(
+      "componentType" -> "corespring-match",
+      "correctResponse" -> answers(qti)(node),
+      "model" -> Json.obj(
+        "columns" -> (Json.obj("labelHtml" -> ((node \ "cornerText").text.toString match {
+          case empty if (empty.isEmpty) => DefaultCornerText
+          case nonEmpty: String => nonEmpty
+        })) +: columns.values.map(text => Json.obj("labelHtml" -> text)).toSeq),
+        "rows" -> rows.map { case(id, text) => Json.obj("id" -> id, "labelHtml" -> text) }.toSeq,
+        "answerType" -> "YES_NO"
+      )
+    )
   }).toMap
+
+  private def columns(implicit node: Node) = filter("Col.*", (choices, acc) => choices.size > acc.size)
+  private def rows(implicit node: Node) = filter("Row.*", (choices, acc) => choices.size <= acc.size)
+  private def answers(qti: Node)(implicit node: Node) = {
+    (qti \\ "responseDeclaration").find(rd => (rd \ "@identifier").text == (node \ "@responseIdentifier").text)
+      .map(rd => (rd \ "correctResponse" \ "value").toSeq.map(_.text)).getOrElse(Seq.empty)
+      .foldLeft(Map.empty[String, Seq[String]]){ case (acc, text) => text.split(" ") match {
+        case Array(one, two) => acc.get(one) match {
+          case Some(list) => acc + (one -> (list :+ two))
+          case _ => acc + (one -> Seq(two))
+        }
+        case _ => throw new IllegalArgumentException("Whoa")
+      }}.map{ case (row, cols) => Json.obj("id" -> row, "matchSet" -> columns.keySet.map(cols.contains(_))) }.toSeq
+  }
+
+  private def filter(regex: String, comparator: (Seq[Node], Seq[Node]) => Boolean)(implicit node: Node) =
+    (node \ "simpleMatchSet").find(matchSet =>
+      (matchSet \ "simpleAssociableChoice").find(choice => regexMatch(regex, (choice \ "@identifier").text)).nonEmpty
+    ).map(_ \ "simpleAssociableChoice").getOrElse((node \ "simpleMatchSet").foldLeft(Seq.empty[Node])((acc, n) =>
+      (n \ "simpleAssociableChoice") match {
+        case choices if comparator(choices, acc) => choices
+        case _ => acc
+      }
+    )).map(choice => (choice \ "@identifier").text -> choice.text).toMap
+
+  private def regexMatch(regex: String, string: String) = {
+    val regexX = regex.r
+    string match {
+      case regexX(_*) => true
+      case _ => false
+    }
+  }
+
 
 }
--- modules/lib/qti-to-v2/src/test/scala/org/corespring/qtiToV2/kds/interactions/MatchInteractionTransformerTest.scala
+++ modules/lib/qti-to-v2/src/test/scala/org/corespring/qtiToV2/kds/interactions/MatchInteractionTransformerTest.scala
@@ -1,8 +1,61 @@
 package org.corespring.qtiToV2.kds.interactions
 
-/**
- * Created by bburton on 11/12/14.
- */
-class MatchInteractionTransformerTest {
+import org.specs2.mutable.Specification
+import play.api.libs.json._
+
+class MatchInteractionTransformerTest extends Specification {
+
+  "MatchInteractionTransformer" should {
+
+    val identifier = "RESPONSE1"
+    val cornerText: String = "Statement"
+    val columns: Map[String, String] = Map("Col1" -> "Yes", "Col2" -> "No")
+    val choices = Map("Row1" -> "3 times 7", "Row2" -> "4 times 4")
+    val answers: Map[String, Seq[String]] = choices.keys.zip(columns.keys).map{ case (a,b) => a -> (Seq(b)) }.toMap
+
+    def qti(identifier: String = identifier,
+            cornerText: String = cornerText,
+            columns: Map[String, String] = columns,
+            choices: Map[String, String] = choices,
+            answers: Map[String, Seq[String]] = answers) =
+      <assessmentItem>
+        <responseDeclaration identifier={identifier} cardinality="multiple" baseType="directedPair">
+          <correctResponse>{ answers.map{ case(one, seq) => seq.map(two => <value>{s"$one $two"}</value> )}.flatten }</correctResponse>
+        </responseDeclaration>
+        <itemBody>
+          <matchInteraction responseIdentifier={identifier} shuffle="false" maxAssociations="0">
+            <cornerText>{cornerText}</cornerText>
+            <simpleMatchSet>
+              {choices.map{case(id, choice) => <simpleAssociableChoice identifier={id} matchMax="0">{choice}</simpleAssociableChoice>}}
+            </simpleMatchSet>
+            <simpleMatchSet>
+              {columns.map{case(id, column) => <simpleAssociableChoice identifier={id} matchMax="0">{column}</simpleAssociableChoice>}}
+            </simpleMatchSet>
+          </matchInteraction>
+        </itemBody>
+        <responseProcessing template="http://www.imsglobal.org/question/qti_v2p1/rptemplates/match_correct"/>
+      </assessmentItem>
+
+    val result = MatchInteractionTransformer.interactionJs(qti()).get(identifier)
+      .getOrElse(throw new Exception("Result did not contain interaction"))
+
+    "transform rows" in {
+      (result \ "model" \ "rows").as[Seq[JsObject]]
+        .map(row => (row \ "id").as[String] -> (row \ "labelHtml").as[String]).toMap must be equalTo(choices)
+    }
+
+    "transform columns" in {
+      (result \ "model" \ "columns").as[Seq[JsObject]]
+        .map(column => (column \ "labelHtml").as[String]) must be equalTo(cornerText +: columns.values.toSeq)
+    }
+
+    "transform answers" in {
+      (result \ "correctResponse").as[Seq[JsObject]]
+        .map(r => (r \ "id").as[String] -> columns.keySet.zipWithIndex
+          .filter{ case(col, index) => val bools = ((r \ "matchSet").as[Seq[Boolean]]); bools(index) }
+          .map(_._1).toSeq).toMap must be equalTo(answers)
+    }
+
+  }
 
 }
