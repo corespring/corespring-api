fe5c1070593f3ba7051d010f9cbee8abf5e76e81
rbokel <rbokel.github@qlod.org>
2014-11-17 20:13:34 +0100

    add integration tests for demonstrating the "cannot cast int to long" issue

---
+++ it/org/corespring/api/v1/CannotCastIntegerToLongTest.scala
@@ -0,0 +1,44 @@
+package org.corespring.api.v1
+
+import org.bson.types.ObjectId
+import org.corespring.it.IntegrationSpecification
+import org.corespring.mongo.json.services.MongoService
+import org.corespring.platform.core.models.itemSession._
+import org.corespring.platform.data.mongo.models.VersionedId
+import org.corespring.test.helpers.models._
+import org.specs2.mutable._
+import play.api.Play
+import play.api.libs.json.Json
+import se.radley.plugin.salat.SalatPlugin
+
+class CannotCastIntegerToLongTest extends IntegrationSpecification {
+
+  private trait withAppContext extends After {
+    val itemId = ObjectId.get
+    val sessionItemJson = Json.obj(
+      "itemId" -> Json.obj("_id" -> itemId.toString, "version" -> 0),
+      "title" -> "some title",
+      "question" -> "some question")
+
+    val sessionService = new MongoService(db("itemsessions"))
+    val sessionId = sessionService.create(sessionItemJson)
+
+    def db = Play.current.plugin[SalatPlugin].get.db()
+
+    def after = {
+      println(s"[withV2Session] after")
+      sessionService.delete(sessionId.get.toString)
+    }
+  }
+
+  "This test" should {
+
+    "throw a ClassCastException" in new withAppContext {
+      val sessionItem: ItemSession = DefaultItemSession.get(sessionId.get)(false).get
+      val version = sessionItem.itemId.version
+      version === Some(0)
+      (version.get.toLong === 0L) must throwA[ClassCastException]
+    }
+
+  }
+}
