f319f52cfeb7ec4adc31741749817b675efbf170
benburton <benjamin.joseph.burton@gmail.com>
2014-11-18 13:14:37 -0500

    Fix passage import

--- modules/lib/qti-to-v2/src/main/scala/org/corespring/qtiToV2/kds/ManifestReader.scala
+++ modules/lib/qti-to-v2/src/main/scala/org/corespring/qtiToV2/kds/ManifestReader.scala
@@ -89,18 +89,24 @@ object ManifestResourceType extends Enumeration {
     "passage" -> Passage
   )
 
-  private val extensionMap = Map(
-    Seq("gif", "jpeg", "jpg", "png") -> Image
+  private val extensionMap = Map(Seq("gif", "jpeg", "jpg", "png") -> Image)
+  private val pathFunctions: Seq[String => Option[ManifestResourceType.Value]] = Seq(
+    (path => path.startsWith("passages/") match {
+      case true => Some(ManifestResourceType.Passage)
+      case _ => None
+    })
   )
 
-  private def fromExtension(path: String) = {
+  private def fromPathString(path: String): ManifestResourceType.Value = {
     def getExtension(path: String) = path.split("\\.").lastOption.getOrElse("")
-    extensionMap.find{ case(extensions, resourceType) => extensions.contains(getExtension(path)) }
-      .map(_._2).getOrElse(Unknown)
+    pathFunctions.map(_(path)).find(_.nonEmpty).flatten.getOrElse(
+      extensionMap.find{ case(extensions, resourceType) => extensions.contains(getExtension(path)) }
+        .map(_._2).getOrElse(Unknown))
   }
+
 
   def fromPath(path: String)(implicit xml: Node): ManifestResourceType.Value =
     (xml \ "resources" \\ "resource").find(resource => (resource \ "@href").text.toString == path)
-      .map(resource => (resource \ "@type").text.toString).map(typeMap.get(_)).flatten.getOrElse(fromExtension(path))
+      .map(resource => (resource \ "@type").text.toString).map(typeMap.get(_)).flatten.getOrElse(fromPathString(path))
 
 }
