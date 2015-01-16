12080b40cb97e925e549aa751d068d6a4450212c
benburton <benjamin.joseph.burton@gmail.com>
2015-01-16 15:32:31 -0500

    Migration to add domains to data

---
+++ deployment/migrations/0052_add_domains.js
@@ -0,0 +1,44 @@
+function up() {
+  function DomainHelper() {
+    var domains = (function() {
+      var domains = {};
+      var domainMapper = {
+        'ELA-Literacy' : 'subCategory',
+        'ELA' : 'subCategory',
+        'Math' : 'category'
+      };
+
+      db.ccstandards.find().forEach(function(standard) {
+        domains[standard.dotNotation] = standard[domainMapper[standard.subject]];
+      });
+      return domains;
+    })();
+
+    this.domainsFor = function(item) {
+      var returnValue = [];
+      if (item.standards) {
+        for (var i in item.standards) {
+          var domain = domains[item.standards[i]];
+          if (returnValue.indexOf(domain) < 0) {
+            returnValue.push(domain);
+          }
+        }
+      }
+      return returnValue;
+    }
+  }
+
+  var domainHelper = new DomainHelper();
+
+  db.content.find({'contentType': 'item'}).forEach(function(item) {
+    item.domains = domainHelper.domainsFor(item);
+    db.content.save(item);
+  });
+
+}
+
+function down() {
+  db.content.update({}, {$unset: {'domains' : ''}}, {'multi': true});
+}
+
+up();
