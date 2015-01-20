aec514a6163619bd54216686d5820868648436a6
rbokel <rbokel.github@qlod.org>
2015-01-20 15:59:25 +0100

    PE-132 added StandardQueryService and SubjectQueryService, UnitTests and IntegrationTests

--- it/org/corespring/platform/core/services/SubjectQueryServiceTest.scala
+++ it/org/corespring/platform/core/services/SubjectQueryServiceTest.scala
@@ -3,20 +3,29 @@ package org.corespring.platform.core.services
 import org.corespring.it.IntegrationSpecification
 import org.corespring.test.helpers.models.SubjectHelper
 import org.specs2.mutable.BeforeAfter
+import play.api.libs.json.Json
 
 class SubjectQueryServiceTest extends IntegrationSpecification {
 
   "Subject Query Service" should {
 
+    def makeQuery(s:String) = Json.obj("searchTerm" -> s).toString()
+
     "handle" in new SubjectData("some test subject") {
-      val result = SubjectQueryService.query("some test")
+      val result = SubjectQueryService.query(makeQuery("some test"))
       result.length === 1
     }
 
     "handle multiple" in new SubjectData("!! 1", "!! 2") {
-      SubjectQueryService.query("!!").length === 2
-      SubjectQueryService.query("!!!").length === 0
+      SubjectQueryService.query(makeQuery("!!")).length === 2
+      SubjectQueryService.query(makeQuery("!!!")).length === 0
     }
+
+    "ignores case" in new SubjectData("UPPERCASE", "lowercase") {
+      SubjectQueryService.query(makeQuery("case")).length === 2
+      SubjectQueryService.query(makeQuery("CASE")).length === 2
+    }
+
   }
 
   class SubjectData(val subjects: String*) extends BeforeAfter {
