1ee2b945ded2f52d8d1d5c432c3bf6c44494df0e
dimabur <dimabur@gmail.com>
2015-02-02 15:31:39 +0200

    Optimisation PR change : more tests for org.corespring.v2.player.hooks.SessionHooks

--- modules/lib/v2-player-integration/src/test/scala/org/corespring/v2/player/integration/hooks/SessionHooksTest.scala
+++ modules/lib/v2-player-integration/src/test/scala/org/corespring/v2/player/integration/hooks/SessionHooksTest.scala
@@ -1,6 +1,7 @@
 package org.corespring.v2.player.integration.hooks
 
-import org.corespring.container.client.hooks.FullSession
+import akka.util.internal.Timeout
+import org.corespring.container.client.hooks.{SaveSession, SessionOutcome, FullSession}
 import org.corespring.platform.core.models.Organization
 import org.corespring.platform.core.models.item.resource.StoredFile
 import org.corespring.platform.core.models.item.{Item, PlayerDefinition}
@@ -16,8 +17,11 @@ import org.specs2.matcher.{Expectable, Matcher}
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
+import play.api.GlobalSettings
 import play.api.libs.json.{Json, JsValue}
 import play.api.mvc.RequestHeader
+import play.api.test.FakeApplication
+import play.api.test.Helpers._
 
 import scalaz.{Failure, Success, Validation}
 
@@ -27,15 +31,11 @@ class SessionHooksTest extends Specification with Mockito with RequestMatchers {
 
   val defaultFailure = generalError("Default failure")
 
-  trait MockResults {
+  abstract class SessionContext extends Scope {
+
     def getOrgAndOptionsResult: Validation[V2Error, OrgAndOpts]
-
     def authSaveSessionResult: Validation[V2Error, (String, Session) => Option[Session]]
-
     def authLoadForReadResult: Validation[V2Error, (Session, PlayerDefinition)]
-  }
-
-  abstract class SessionContext extends Scope with MockResults {
 
     lazy val hooks = new SessionHooks {
 
@@ -66,64 +66,147 @@ class SessionHooksTest extends Specification with Mockito with RequestMatchers {
     }
   }
 
-  case class returnFullSession[D](expectedStatus: Int, body: String) extends Matcher[Either[(Int, String), D]] {
-    def apply[S <: Either[(Int, String), D]](s: Expectable[S]) = {
+  class IdentificationFailureContext extends SessionContext{
+    override def getOrgAndOptionsResult = Failure(invalidToken(mock[RequestHeader]))
+    override def authSaveSessionResult = Success((s: String, session: Session) => Some(mock[Session]))
+    override def authLoadForReadResult = Success((mock[Session], mock[PlayerDefinition]))
+  }
 
-      println(s" --> ${s.value}")
-      def callResult(success: Boolean) = result(success, s"${s.value} matches $expectedStatus & $body", s"${s.value} doesn't match $expectedStatus & $body", s)
-      s.value match {
-        case Left((code, msg)) => callResult(code == expectedStatus && msg == body)
-        case Right(_) => callResult(false)
-      }
+  class AuthenticationFailureContext extends SessionContext{
+    override def getOrgAndOptionsResult = Success(mock[OrgAndOpts])
+    override def authSaveSessionResult = Success((s: String, session: Session) => Some(mock[Session]))
+    override def authLoadForReadResult = Failure(cantLoadSession("sessionId"))
+  }
+
+  class Successful_Identification_And_Authentication_Context extends SessionContext {
+    override def getOrgAndOptionsResult = Success(OrgAndOpts(mock[Organization],
+      PlayerAccessSettings(itemId = "itemId"), mock[AuthMode]))
+
+    override def authSaveSessionResult = Success((s: String, session: Session) => Some(mock[Session]))
+
+    override def authLoadForReadResult = {
+      val session = Json.obj("isComplete" -> true)
+      val playerDef = new PlayerDefinition(
+        Seq(StoredFile("test.js", "text/javascript", false, "key")),
+        "",
+        Json.obj(),
+        "", Some("function(){}")
+      )
+
+      Success((session, playerDef))
     }
   }
 
-  "loadItemAndSession" should {
+  "loadItemAndSession method" should {
 
-    "returns error if can't resolve identity" in new SessionContext {
-      override def getOrgAndOptionsResult = Failure(invalidToken(mock[RequestHeader]))
-      override def authSaveSessionResult = Success((s: String, session: Session) => Some(mock[Session]))
-      override def authLoadForReadResult = Success((mock[Session], mock[PlayerDefinition]))
+    "return error if can't resolve identity" in new IdentificationFailureContext {
 
       hooks.loadItemAndSession("test_session_id")(mock[RequestHeader]) must
         returnStatusMessage(getOrgAndOptionsResult.toEither.left.get.statusCode,
           getOrgAndOptionsResult.toEither.left.get.message)
     }
 
-    "returns error if can't authenticate" in new SessionContext {
-      override def getOrgAndOptionsResult = Success(mock[OrgAndOpts])
-      override def authSaveSessionResult = Success((s: String, session: Session) => Some(mock[Session]))
-      override def authLoadForReadResult = Failure(cantLoadSession("sessionId"))
+    "return error if can't authenticate" in new AuthenticationFailureContext {
 
       hooks.loadItemAndSession("test_session_id")(mock[RequestHeader]) must
         returnStatusMessage(authLoadForReadResult.toEither.left.get.statusCode,
           authLoadForReadResult.toEither.left.get.message)
     }
 
-    "returns full session if when all went good" in new SessionContext {
-      override def getOrgAndOptionsResult = Success(OrgAndOpts(mock[Organization],
-          PlayerAccessSettings(itemId="itemId"),mock[AuthMode]))
+    "return full session if when all went good" in new Successful_Identification_And_Authentication_Context {
 
-      override def authSaveSessionResult = Success((s: String, session: Session) => Some(mock[Session]))
-      override def authLoadForReadResult = {
-        val session = mock[Session]
-        val playerDef = new PlayerDefinition(
-          Seq(StoredFile("test.js", "text/javascript", false, "key")),
-          "",
-          Json.obj(),
-          "", Some("function(){}")
-        )
-
-        Success((session,playerDef))
-      }
-
-      val res = hooks.loadItemAndSession("test_session_id")(mock[RequestHeader])
-      res.isRight === true
-      res match {
+      val res = hooks.loadItemAndSession("test_session_id")(mock[RequestHeader]) match {
         case Right(FullSession(js,isSecure)) => success
         case _ => failure("Error")
       }
     }
   }
+
+    "loadOutcome method" should {
+
+      "return error if can't resolve identity" in new IdentificationFailureContext {
+
+        hooks.loadOutcome("test_session_id")(mock[RequestHeader]) must
+          returnStatusMessage(getOrgAndOptionsResult.toEither.left.get.statusCode,
+            getOrgAndOptionsResult.toEither.left.get.message)
+      }
+
+      "return error if can't authenticate" in new AuthenticationFailureContext {
+
+        hooks.loadOutcome("test_session_id")(mock[RequestHeader]) must
+          returnStatusMessage(authLoadForReadResult.toEither.left.get.statusCode,
+            authLoadForReadResult.toEither.left.get.message)
+      }
+
+      "return SessionOutcome if all went good" in new Successful_Identification_And_Authentication_Context {
+
+        hooks.loadOutcome("test_session_id")(mock[RequestHeader]) match {
+          case Right(SessionOutcome(item, session, secure, isComplete)) => success
+          case _ => failure("Error")
+        }
+      }
+    }
+
+    "getScore method" should {
+
+      "return error if can't resolve identity" in new IdentificationFailureContext {
+
+        hooks.getScore("test_session_id")(mock[RequestHeader]) must
+          returnStatusMessage(getOrgAndOptionsResult.toEither.left.get.statusCode,
+            getOrgAndOptionsResult.toEither.left.get.message)
+      }
+
+      "return error if can't authenticate" in new AuthenticationFailureContext {
+
+        hooks.getScore("test_session_id")(mock[RequestHeader]) must
+          returnStatusMessage(authLoadForReadResult.toEither.left.get.statusCode,
+            authLoadForReadResult.toEither.left.get.message)
+      }
+
+      "return SessionOutcome if all went good" in new Successful_Identification_And_Authentication_Context {
+
+        hooks.getScore("test_session_id")(mock[RequestHeader]) match {
+          case Right(SessionOutcome(item, session, secure, isComplete)) => success
+          case _ => failure("Error")
+        }
+      }
+    }
+
+  object mockGlobal extends GlobalSettings {
+    sequential
+      "save method" should {
+
+        running( FakeApplication( withGlobal = Some(mockGlobal))) {
+          "return error if can't resolve identity" in new IdentificationFailureContext {
+
+            val futureResult = hooks.save("test_session_id")(mock[RequestHeader])
+
+            futureResult must
+              returnStatusMessage(getOrgAndOptionsResult.toEither.left.get.statusCode,
+                getOrgAndOptionsResult.toEither.left.get.message).await
+          }
+
+          "return error if can't authenticate" in new AuthenticationFailureContext {
+
+            hooks.save("test_session_id")(mock[RequestHeader]) must
+              returnStatusMessage(authLoadForReadResult.toEither.left.get.statusCode,
+                authLoadForReadResult.toEither.left.get.message).await
+          }
+
+          "return SessionOutcome if all went good" in new Successful_Identification_And_Authentication_Context {
+
+            hooks.save("test_session_id")(mock[RequestHeader]) must new Matcher[Either[(Int, String), SaveSession]] {
+              def apply[S <: Either[(Int, String), SaveSession]](s: Expectable[S]) = {
+
+                s.value match {
+                  case Right(SaveSession(item, session, secure, isComplete)) => result(true," blah "," blah ",s)
+                  case Left(_) => result(false," blah "," blah ",s)
+                }
+              }
+            }.await
+          }
+        }
+      }
+    }
 
 }
