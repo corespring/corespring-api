77ab94bdef74b5fceaea489ee960eeeb8c01fe3e
Evan Eustace <evan.eustace@gmail.com>
2015-02-26 12:35:58 -0500

    bump container to hotfix version for spacing

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val playVersion = "2.2.1"
 
   //V2 Player
-  val containerVersion = "0.22.0"
+  val containerVersion = "0.22.1"
   val containerClientWeb = "org.corespring" %% "container-client-web" % containerVersion
   val containerJsProcessing = "org.corespring" %% "js-processing" % containerVersion
   val componentModel = "org.corespring" %% "component-model" % containerVersion
