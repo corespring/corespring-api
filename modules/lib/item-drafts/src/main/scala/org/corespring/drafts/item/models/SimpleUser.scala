5fab705d144314646fbe9682f15bac0785a8a390
Ed Eustace <ed.eustace@gmail.com>
2015-03-16 16:33:20 +0000

    add orgId to SimpleUser

--- modules/lib/item-drafts/src/main/scala/org/corespring/drafts/item/models/SimpleUser.scala
+++ modules/lib/item-drafts/src/main/scala/org/corespring/drafts/item/models/SimpleUser.scala
@@ -5,8 +5,8 @@ import org.corespring.platform.core.models.User
 
 object SimpleUser {
   def fromUser(u: User): SimpleUser = {
-    SimpleUser(u.id, u.userName, u.provider, u.fullName)
+    SimpleUser(u.id, u.userName, u.provider, u.fullName, u.org.orgId)
   }
 }
 
-case class SimpleUser(id: ObjectId, userName: String, provider: String, fullName: String)
+case class SimpleUser(id: ObjectId, userName: String, provider: String, fullName: String, orgId: ObjectId)
