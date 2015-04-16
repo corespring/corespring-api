e1f564cd1ca8f636544c749c19464f17532aec0a
Venus Ang <venusang24@gmail.com>
2015-04-15 22:19:59 -0700

    AC-79 CMS Front End Changes / AC-108 update Item Row on Search Reselts
    Moved edit button under preview button, put clone, publish and delete buttons in drop-down

---
+++ public/js/corespring/tagger/directives/item-edit-button-only.js
@@ -0,0 +1,91 @@
+angular.module('tagger.directives')
+  .directive('itemEditButtonOnly', [function () {
+
+    function link($scope, $element, $attrs, ngModel){
+      $scope.status = 'draft';
+      $scope.draftStatus = 'noDraft';
+
+      $scope.$watch('item.published', function(isPublished){
+        $scope.status = isPublished ? 'live' : 'draft';
+      });
+
+      function updateDraftStatus(){
+
+        if(!$scope.item.readOnly){
+          $scope.draftStatus = 'canMakeDraft';
+        }
+
+        if($scope.draft && !$scope.item.readOnly){
+          if($scope.org && 
+            $scope.draft.orgId === $scope.org.id && 
+            $scope.draft.user === $scope.userName){
+            $scope.draftStatus = 'ownsDraft';
+          } else {
+            $scope.draftStatus = 'draftExists';
+          }
+        }
+      }
+      
+      function isReadOnly(){
+        return $scope.item && $scope.item.readOnly;
+      }
+
+      function isPublished(){
+        return $scope.item && $scope.item.published;
+      }
+
+      function hasDraft(){
+        return $scope.draftStatus !== 'noDraft' && $scope.draftStatus !== 'canMakeDraft';
+      }
+
+      function updateFlags(){
+        updateDraftStatus();
+        $scope.canGoLive = !isReadOnly() && (!isPublished() || hasDraft());
+        $scope.canClone = !$scope.item.readOnly; 
+      }
+
+      $scope.$watch('userName', updateFlags);
+
+      $scope.$watch('orgDrafts', function(drafts){
+        $scope.draft = _.find(drafts, function(d){
+          return d.itemId == $scope.item.id;
+        });
+        updateFlags();
+      });
+
+      $scope.$watch('org', function(o){
+        if(o){
+          updateFlags();
+        }
+      });
+
+      $scope.$watch('item', updateFlags, true);
+
+    }
+
+    return {
+      restrict: 'AE',
+      link: link,
+      replace: true,
+      scope: {
+        userName: '=',
+        orgDrafts: '=',
+        org: '=',
+        item: '=',
+        edit: '&'
+      }, 
+      template: [
+      '<div>',
+      '  <div>Format: {{item.format.apiVersion}}</div>',
+      '  <div class="draft-buttons" ng-switch on="draftStatus">',
+      '    <div ng-switch-when="draftExists">',
+      '      <i class="icon icon-lock"></i>',
+      '      <span>{{draft.user}} is editing</span>',
+      '    </div>',
+      '    <div ng-switch-default>',
+      '      <button ng-disabled="item.readOnly" ng-click="edit(item)" class="btn btn-info btn-sm">edit</button>',
+      '    </div>',
+      '  </div>',
+      '</div>'].join('')
+    };
+  }]);
