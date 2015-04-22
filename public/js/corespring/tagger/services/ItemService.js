1f71a9abd5c0cbd50e6d1dd6b55dae53e794ad56
edeustace <ed.eustace@gmail.com>
2015-04-22 14:23:06 +0100

    fix v1 editor bug

--- public/js/corespring/tagger/services/ItemService.js
+++ public/js/corespring/tagger/services/ItemService.js
@@ -41,6 +41,20 @@ angular.module('tagger.services')
     ]);
 
 angular.module('tagger.services')
+    .factory('ItemSessionCountService', [
+        '$resource', 'ServiceLookup', function($resource, ServiceLookup){
+
+            return $resource(
+             ServiceLookup.getUrlFor('items') + '/sessions/count',
+                {},
+                {
+                  "get" :{ method:'GET' }
+                }
+            );
+        }
+    ]);
+
+angular.module('tagger.services')
     .factory('SupportingMaterial',
     [ '$resource', 'ServiceLookup', function ($resource, ServiceLookup) {
 
