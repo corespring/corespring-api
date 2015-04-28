c0bb4b3545c1b157209b737f7a73c4e3e8f6d35c
rbokel <rbokel.github@qlod.org>
2015-04-28 15:04:30 +0200

    AC-149 update item-action-buttons to close itself

--- public/js/corespring/tagger/directives/item-action-buttons.js
+++ public/js/corespring/tagger/directives/item-action-buttons.js
@@ -1,34 +1,49 @@
 angular.module('tagger.directives')
   .directive('itemActionButton', [function () {
 
-    function link($scope, $element, $attrs, ngModel){}
-
     return {
       restrict: 'AE',
-      link: link,
       replace: true,
+      template: template(),
       scope: {
         item: '=',
         publish: '&',
         cloneItem: '&',
         deleteItem: '&'
-      },
-      template: [
-      '<div ng-show="!item.readOnly">',
-      '<a class="btn btn-default btn-sm btn-action dropdown-toggle" data-toggle="dropdown" href="#menu-item-{{item.id}}">actions <b class="caret"></b></a>',
-      '<div class="dropdown" id="menu-item-{{item.id}}">',
-        '<ul class="dropdown-menu dropdown-menu-actions">',
-          '<li>',
-          '  <button class="btn btn-sm" ng-click="cloneItem(item)"><i class="fa fa-plus-circle"></i>&nbsp;clone</button>',
-          '</li>',
-          '<li ng-show="!item.published">',
-          '  <button class="btn btn-sm btn-info" ng-click="publish(item)"><i class="fa fa-bolt"></i>&nbsp;publish</button>',
-          '</li>',
-          '<li>',
-          '  <button ng-click="deleteItem(item)" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i>&nbsp;delete</button>',
-          '</li>',
-        '</ul>',
-      '</div>',
-      '</div>'].join('')
+      }
     };
+
+    function template() {
+      return [
+        '<div ng-show="!item.readOnly">',
+        '  <a class="btn btn-default btn-sm btn-action dropdown-toggle"',
+        '     id="menu-item-{{item.id}}"',
+        '     data-toggle="dropdown"',
+        '     aria-haspopup="true"',
+        '     aria-expanded="false"',
+        '     href="#">',
+        '    actions <b class="caret"></b>',
+        '  </a>',
+        '  <div class="dropdown" aria-labelledby="menu-item-{{item.id}}">',
+        '    <ul class="dropdown-menu dropdown-menu-actions">',
+        '      <li>',
+        '        <button class="btn btn-sm" ng-click="cloneItem(item)">',
+        '          <i class="fa fa-plus-circle"></i>&nbsp;clone',
+        '        </button>',
+        '      </li>',
+        '      <li ng-show="!item.published">',
+        '        <button class="btn btn-sm btn-info" ng-click="publish(item)">',
+        '          <i class="fa fa-bolt"></i>&nbsp;publish',
+        '        </button>',
+        '      </li>',
+        '      <li>',
+        '        <button class="btn btn-danger btn-sm" ng-click="deleteItem(item)">',
+        '          <i class="fa fa-trash-o"></i>&nbsp;delete',
+        '        </button>',
+        '      </li>',
+        '    </ul>',
+        '  </div>',
+        '</div>'
+      ].join('');
+    }
   }]);
