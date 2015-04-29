a6fbceb46b184cc01774e1b37a2aea8870953e0c
benburton <benjamin.joseph.burton@gmail.com>
2015-04-29 14:47:04 -0400

    Missed test

--- modules/lib/v2-auth/src/test/scala/org/corespring/v2/auth/identifiers/PlayerTokenInRefererIdentityTest.scala
+++ modules/lib/v2-auth/src/test/scala/org/corespring/v2/auth/identifiers/PlayerTokenInRefererIdentityTest.scala
@@ -1,8 +1,27 @@
 package org.corespring.v2.auth.identifiers
 
-/**
- * Created by bburton on 4/29/15.
- */
-class PlayerTokenInRefererIdentityTest {
+import org.specs2.mutable.Specification
+import play.api.test.FakeRequest
+
+class PlayerTokenInRefererIdentityTest extends Specification {
+
+  import PlayerTokenInRefererIdentity._
+
+  "RequestHeaderWithRefererQueryString" should {
+
+    val queryMap = Map("this" -> Seq("is"), "my" -> Seq("query"))
+    val queryString = {
+      val enc = (p: String) => java.net.URLEncoder.encode(p, "utf-8")
+      queryMap.map{ case(k, v) => s"${enc(k)}=${enc(v.head)}"}.mkString("&")
+    }
+    val url = "http://localhost:9000"
+    val referer = s"$url?$queryString"
+    val request = FakeRequest().withHeaders("Referer" -> referer)
+
+    "provide query string from header" in {
+      request.getRefererQueryString must be equalTo(queryMap)
+    }
+
+  }
 
 }
