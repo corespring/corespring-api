7c99b1c3711be1723061efeca2483df409ea70b3
Ed Eustace <ed.eustace@gmail.com>
2015-05-19 19:52:06 +0100

    fix clone from list view (wip)

--- modules/lib/core/src/main/scala/org/corespring/platform/core/files/ItemFiles.scala
+++ modules/lib/core/src/main/scala/org/corespring/platform/core/files/ItemFiles.scala
@@ -1,18 +1,20 @@
 package org.corespring.platform.core.files
 
 import org.corespring.assets.CorespringS3Service
-import org.corespring.common.log.PackageLogging
 import org.corespring.platform.core.models.item.Item
 import org.corespring.platform.core.models.item.resource.{ Resource, StoredFile }
+import play.api.Logger
 import scalaz.{ Validation, Failure, Success }
 
 case class CloneResourceResult(files: Seq[CloneFileResult])
 
-case class CloneFileResult(file: StoredFile, throwable: Option[Throwable]){
+case class CloneFileResult(file: StoredFile, throwable: Option[Throwable]) {
   def successful = throwable.isEmpty
 }
 
-trait ItemFiles extends PackageLogging {
+trait ItemFiles {
+
+  protected val logger = Logger(classOf[ItemFiles])
 
   def s3service: CorespringS3Service
 
@@ -26,42 +28,49 @@ trait ItemFiles extends PackageLogging {
    *         Failure -> a seq of files that were successfully cloned (to allow rollback)
    *         Success -> the updated item
    */
-  def cloneStoredFiles(implicit item: Item): Validation[Seq[CloneFileResult], Item] = {
+  protected def cloneStoredFiles(from: Item, to: Item): Validation[Seq[CloneFileResult], Item] = {
 
-    val resources: Seq[Resource] = item.supportingMaterials ++ item.data
+    require(from.id.version.isDefined, s"from item id.version must be defined: ${from.id}")
+    require(to.id.version.isDefined, s"to item id.version must be defined: ${from.id}")
 
+    def cloneResourceFiles(resource: Resource): CloneResourceResult = {
+      val result: Seq[CloneFileResult] = resource.files.filter(_.isInstanceOf[StoredFile]).map(f => processFile(resource, f.asInstanceOf[StoredFile]))
+      CloneResourceResult(result)
+    }
+
+    def processFile(resource: Resource, file: StoredFile): CloneFileResult = try {
+      val toKey = StoredFile.storageKey(to.id.id, to.id.version.get, resource, file.name)
+
+      val fromKey = if (file.storageKey == file.name || file.storageKey.isEmpty) {
+        StoredFile.storageKey(from.id.id, from.id.version.get, resource, file.name)
+      } else {
+        file.storageKey
+      }
+
+      if (fromKey != file.storageKey) {
+        logger.warn(s"This file has a bad key. id=${to.id}, resource=${resource.name}, file=${file.name}")
+      }
+
+      logger.debug("[ItemFiles] clone file: " + from + " --> " + to)
+      s3service.copyFile(bucket, fromKey, toKey)
+      file.storageKey = toKey
+      CloneFileResult(file, None)
+    } catch {
+      case e: Throwable => {
+        logger.debug("An error occurred cloning the file: " + e.getMessage)
+        CloneFileResult(file, Some(e))
+      }
+    }
+
+    val resources: Seq[Resource] = to.supportingMaterials ++ to.data
     val result: Seq[CloneResourceResult] = resources.map(cloneResourceFiles)
-
     val files: Seq[CloneFileResult] = result.map(r => r.files).flatten
 
     def successful = files.filterNot(_.successful).length == 0
 
     logger.trace(s"Failed clone result: $files")
 
-    if (successful) Success(item) else Failure(files)
-  }
-
-  def cloneResourceFiles(resource: Resource)(implicit item: Item): CloneResourceResult = {
-    val result: Seq[CloneFileResult] = resource.files.filter(_.isInstanceOf[StoredFile]).map(f => processFile(resource, f.asInstanceOf[StoredFile]))
-    CloneResourceResult(result)
-  }
-
-  def processFile(resource: Resource, file: StoredFile)(implicit item: Item): CloneFileResult = try {
-    val newStorageKey = StoredFile.storageKey(item.id, resource, file.name)
-
-    if (file.storageKey.isEmpty) {
-      throw new RuntimeException("this file has no storage key: " + file.name + " id: " + item.id + " resource: " + resource.name)
-    }
-
-    logger.debug("[ItemFiles] clone file: " + file.storageKey + " --> " + newStorageKey)
-    s3service.copyFile(bucket, file.storageKey, newStorageKey)
-    file.storageKey = newStorageKey
-    CloneFileResult(file, None)
-  } catch {
-    case e: Throwable => {
-      logger.debug("An error occurred cloning the file: " + e.getMessage)
-      CloneFileResult(file, Some(e))
-    }
+    if (successful) Success(to) else Failure(files)
   }
 
 }
--- modules/lib/item-drafts/src/test/scala/org/corespring/drafts/item/ItemDraftsTest.scala
+++ modules/lib/item-drafts/src/test/scala/org/corespring/drafts/item/ItemDraftsTest.scala
@@ -1,18 +1,22 @@
 package org.corespring.drafts.item
 
-import com.mongodb.{ CommandResult, WriteResult }
+import com.mongodb.casbah.MongoCollection
+import com.mongodb.casbah.commons.MongoDBObject
+import com.mongodb.{ WriteConcern, CommandResult, WriteResult }
 import org.bson.types.ObjectId
 import org.corespring.drafts.errors._
 import org.corespring.drafts.item.models._
 import org.corespring.drafts.item.services.{ ItemDraftService, CommitService }
-import org.corespring.platform.core.models.item.resource.Resource
-import org.corespring.platform.core.models.item.{TaskInfo, PlayerDefinition, Item}
+import org.corespring.platform.core.models.item.resource.{ StoredFile, Resource }
+import org.corespring.platform.core.models.item.{ TaskInfo, PlayerDefinition, Item }
 import org.corespring.platform.core.services.item.{ ItemPublishingService, ItemService }
 import org.corespring.platform.data.mongo.models.VersionedId
 import org.joda.time.DateTime
 import org.specs2.mock.Mockito
+import org.specs2.mock.mockito.ArgumentCapture
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
+import play.api.test.FakeApplication
 
 import scalaz.{ Validation, Success, Failure }
 
@@ -367,9 +371,57 @@ class ItemDraftsTest extends Specification with Mockito {
       }
 
       "return true if supportingMaterials has changed" in new __ {
-        val item2 = item1.copy(supportingMaterials = Seq(Resource(name="test", files=Seq.empty)))
+        val item2 = item1.copy(supportingMaterials = Seq(Resource(name = "test", files = Seq.empty)))
         hasSrcChanged(item1, item2) must_== true
       }
     }
+
+    "addFileToChangeSet" should {
+
+      class __(n: Int = 1) extends Scope with MockItemDrafts {
+        import com.mongodb.casbah.Imports._
+
+        val mockCollection = {
+          val m = mock[MongoCollection]
+          m.update(
+            any[MongoDBObject],
+            any[MongoDBObject],
+            any[Boolean],
+            any[Boolean],
+            any[WriteConcern])(
+              any[MongoDBObject => DBObject],
+              any[MongoDBObject => DBObject], any[DBEncoder]) returns {
+                mock[WriteResult].getN returns n
+              }
+          m
+        }
+        mockDraftService.collection returns mockCollection
+      }
+
+      import play.api.test.Helpers.running
+
+      "update the document in the db" in new __ {
+
+        running(FakeApplication()) {
+          import com.mongodb.casbah.Imports._
+
+          val captor = capture[MongoDBObject]
+          val draft = mkDraft(ed, item)
+          val file = StoredFile("test.png", "image/png", false)
+          addFileToChangeSet(draft, file)
+
+          implicit def captureToDbo(d: ArgumentCapture[MongoDBObject]): DBObject = {
+            d.value.asDBObject
+          }
+
+          there was one(mockCollection).update(captor, any[MongoDBObject], any[Boolean], any[Boolean], any[WriteConcern])(
+            any[ArgumentCapture[MongoDBObject] => DBObject],
+            any[MongoDBObject => DBObject], any[DBEncoder])
+
+        }
+      }
+    }
+
   }
+
 }
