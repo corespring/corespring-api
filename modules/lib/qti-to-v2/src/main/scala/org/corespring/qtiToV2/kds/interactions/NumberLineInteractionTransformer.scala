95b8699d59fd6e74492c008d79220c8e8b3ed33f
benburton <benjamin.joseph.burton@gmail.com>
2015-06-10 09:23:20 -0400

    Fix snapPerTick types

--- modules/lib/qti-to-v2/src/main/scala/org/corespring/qtiToV2/kds/interactions/NumberLineInteractionTransformer.scala
+++ modules/lib/qti-to-v2/src/main/scala/org/corespring/qtiToV2/kds/interactions/NumberLineInteractionTransformer.scala
@@ -14,8 +14,8 @@ object NumberLineInteractionTransformer extends InteractionTransformer with Numb
 
   override def transform(node: Node) = node match {
     case node: Node if (node.label == "numberLineInteraction") =>
-      <p class="prompt">{(node \ "prompt").map(_.child).flatten}</p> ++
-          <corespring-number-line id={(node \\ "@responseIdentifier").text}/>
+      <p class="prompt">{ (node \ "prompt").map(_.child).flatten }</p> ++
+        <corespring-number-line id={ (node \\ "@responseIdentifier").text }/>
     case _ => node
   }
 
@@ -25,8 +25,7 @@ object NumberLineInteractionTransformer extends InteractionTransformer with Numb
       "correctResponse" -> correctResponses(qti).map(value => Json.obj(
         "type" -> "point",
         "pointType" -> "full",
-        "domainPosition" -> value.toDouble
-      )),
+        "domainPosition" -> value.toDouble)),
       "model" -> Json.obj(
         "config" -> Json.obj(
           "domain" -> Seq((node \ "@lowerBound").text.toDouble, (node \ "@upperBound").text.toDouble),
@@ -36,17 +35,13 @@ object NumberLineInteractionTransformer extends InteractionTransformer with Numb
           "exhibitOnly" -> false,
           "maxNumberOfPoints" -> correctResponses(qti).length,
           "snapPerTick" -> parseInt((node \ "@minorTickMarkFreq").text).getOrElse(Defaults.SnapPerTick).asInstanceOf[Int],
-          "tickFrequency" -> (((node \ "@upperBound").text.toDouble - (node \ "@lowerBound").text.toDouble)/(node \ "@step").text.toDouble),
+          "tickFrequency" -> (((node \ "@upperBound").text.toDouble - (node \ "@lowerBound").text.toDouble) / (node \ "@step").text.toDouble),
           "availableTypes" -> Json.obj("PF" -> true),
           "initialElements" -> Json.arr(),
           "ticks" -> (node \ "hatchMark")
             .map(hatch => Json.obj(
               "label" -> (if ((hatch \ "@isVisibleLabel").text == "1") (hatch \ "@label").text else ""),
-              "value" -> BigDecimal((hatch \ "@value").text))
-            )
-        )
-      )
-    )
+              "value" -> BigDecimal((hatch \ "@value").text))))))
   }).toMap
 
   def correctResponses(qti: Node)(implicit node: Node) = (qti \\ "responseDeclaration")
