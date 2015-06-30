c768899e81223d26c9509395be1f65f227035fae
Attila Miklosi <attila.miklosi@gmail.com>
2015-06-30 19:34:34 +0200

    updated container and components dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val playVersion = "2.2.1"
 
   //V2 Player
-  val containerVersion = "0.42.0-SNAPSHOT"
+  val containerVersion = "0.42.0"
 
   val containerClientWeb = "org.corespring" %% "container-client-web" % containerVersion
   val containerJsProcessing = "org.corespring" %% "js-processing" % containerVersion
