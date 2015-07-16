64f0bce2129c3cf47ca5417b813f4245e6576d51
Ed Eustace <ed.eustace@gmail.com>
2015-07-16 17:26:07 +0100

    use container 0.43.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val playVersion = "2.2.1"
 
   //V2 Player
-  val containerVersion = "0.43.1"
+  val containerVersion = "0.43.2"
 
   val containerClientWeb = "org.corespring" %% "container-client-web" % containerVersion
   val containerJsProcessing = "org.corespring" %% "js-processing" % containerVersion
