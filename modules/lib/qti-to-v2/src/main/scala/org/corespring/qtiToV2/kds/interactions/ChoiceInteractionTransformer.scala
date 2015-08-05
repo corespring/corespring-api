01a1b11753fa01c01acdadba9a23fa3357438a6d
benburton <benjamin.joseph.burton@gmail.com>
2015-08-05 14:42:43 -0400

    Use default feedback for KDS inline choice items

--- modules/lib/qti-to-v2/src/main/scala/org/corespring/qtiToV2/kds/interactions/ChoiceInteractionTransformer.scala
+++ modules/lib/qti-to-v2/src/main/scala/org/corespring/qtiToV2/kds/interactions/ChoiceInteractionTransformer.scala
@@ -1,11 +1,12 @@
 package org.corespring.qtiToV2.kds.interactions
 
-import org.corespring.qtiToV2.interactions.{ChoiceInteractionTransformer => CorespringChoiceInteractionTransformer, InteractionTransformer}
+import org.corespring.platform.core.models.JsonUtil
+import org.corespring.qtiToV2.interactions.{ ChoiceInteractionTransformer => CorespringChoiceInteractionTransformer, InteractionTransformer }
 import org.corespring.qtiToV2.kds.XHTMLCleaner
 import play.api.libs.json._
 
-import scala.xml.{Elem, NodeSeq, Node}
-import scala.xml.transform.{RewriteRule, RuleTransformer}
+import scala.xml.{ Elem, NodeSeq, Node }
+import scala.xml.transform.{ RewriteRule, RuleTransformer }
 
 object ChoiceInteractionTransformer extends InteractionTransformer with XHTMLCleaner {
 
@@ -17,15 +18,21 @@ object ChoiceInteractionTransformer extends InteractionTransformer with XHTMLCle
   }).transform(CorespringChoiceInteractionTransformer.transform(node))
 
   override def interactionJs(qti: Node) = CorespringChoiceInteractionTransformer.interactionJs(qti).map {
-    case(id, json) => id -> json.deepMerge(Json.obj(
-      "model" -> Json.obj("shuffle" -> false,
+    case (id, json) => id -> json.deepMerge(partialObj(
+      "feedback" -> ((json \ "componentType").asOpt[String] match {
+        case Some("corespring-inline-choice") =>
+          Some(JsArray((json \ "model" \ "choices").as[Seq[JsObject]].map { choice =>
+            Json.obj("value" -> (choice \ "value").as[String], "feedbackType" -> "default")
+          }))
+        case _ => None
+      }),
+      "model" -> Some(Json.obj("shuffle" -> false,
         "choices" -> (json \ "model" \ "choices").asOpt[Seq[JsObject]].map(_.map(choice => {
           choice.deepMerge(
             partialObj("rationale" -> (choice \ "value").asOpt[String]
               .map(choiceId => rationale(qti, id, choiceId)).flatten))
-        })).getOrElse(throw new Exception(s"choiceInteraction $id was missing choices")))))
-    }.toMap
-
+        })).getOrElse(throw new Exception(s"choiceInteraction $id was missing choices"))))))
+  }.toMap
 
   private def rationale(qti: Node, id: String, choiceId: String): Option[JsString] =
     (qti \\ "choiceRationales" ++ qti \\ "inlineChoiceRationales").find(c => (c \ "@responseIdentifier").text == id)
