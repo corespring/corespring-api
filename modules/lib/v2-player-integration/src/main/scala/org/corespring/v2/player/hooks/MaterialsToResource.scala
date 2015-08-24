cfcb6b6b799f24407d2c2b201be5679cb905feca
Ed Eustace <ed.eustace@gmail.com>
2015-08-24 22:38:52 +0100

    use isMain

--- modules/lib/v2-player-integration/src/main/scala/org/corespring/v2/player/hooks/MaterialsToResource.scala
+++ modules/lib/v2-player-integration/src/main/scala/org/corespring/v2/player/hooks/MaterialsToResource.scala
@@ -5,8 +5,8 @@ import org.corespring.platform.core.models.item.resource.{ BaseFile, Resource, S
 
 private[hooks] trait MaterialToResource {
 
-  protected def binaryToFile(b: Binary, isMain: Boolean = false) = StoredFile(name = b.name, isMain = true, contentType = b.mimeType)
-  protected def htmlToFile(h: Html, isMain: Boolean = false) = VirtualFile(name = h.name, isMain = true, contentType = h.mimeType, content = h.content)
+  protected def binaryToFile(b: Binary, isMain: Boolean = false) = StoredFile(name = b.name, isMain = isMain, contentType = b.mimeType)
+  protected def htmlToFile(h: Html, isMain: Boolean = false) = VirtualFile(name = h.name, isMain = isMain, contentType = h.mimeType, content = h.content)
   protected def requestToFile[F <: File](sm: CreateNewMaterialRequest[F]): BaseFile = sm match {
     case CreateBinaryMaterial(_, _, binary) => binaryToFile(binary)
     case CreateHtmlMaterial(_, _, main, _) => htmlToFile(main)
