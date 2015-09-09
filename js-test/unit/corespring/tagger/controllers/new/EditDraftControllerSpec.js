15fb056ab57df1053defea5160c5fecfec7762cf
rbokel <rbokel.github@qlod.org>
2015-09-09 14:34:45 +0200

    AC-228 fix failing test

--- js-test/unit/corespring/tagger/controllers/new/EditDraftControllerSpec.js
+++ js-test/unit/corespring/tagger/controllers/new/EditDraftControllerSpec.js
@@ -396,7 +396,8 @@ describe('tagger.controllers.new.EditDraftController', function() {
             draftName: jasmine.any(String),
             devEditor: showEditorFn === 'showDevEditor',
             onItemChanged: scope.onItemChanged,
-            autosizeEnabled: false
+            autosizeEnabled: false,
+            hideSaveButton: true
           },
           jasmine.any(Function));
       };
