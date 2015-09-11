279104d445567c256445d918a837a97bdc845745
Ed Eustace <ed.eustace@gmail.com>
2015-09-11 12:45:23 +0100

    add legacy test, pending test + utils

---
+++ modules/web/v2-api/test/org/corespring/v2/api/CollectionApiTest.scala
@@ -0,0 +1,10 @@
+package org.corespring.v2.api
+
+import org.specs2.mutable.Specification
+
+class CollectionApiTest extends Specification {
+
+  "createCollection" should {
+    "work" in pending
+  }
+}
