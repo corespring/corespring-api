51499dd780adaba9ec9c330a649a7ae190b361e6
Ed Eustace <ed.eustace@gmail.com>
2015-09-21 21:19:17 +0100

    cache filter integration for cs-api

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val playVersion = "2.2.1"
 
   //V2 Player
-  val containerVersion = "0.46.0"
+  val containerVersion = "0.47.0-SNAPSHOT"
 
   def toModule(name: String) = "org.corespring" %% name % containerVersion
 
