e2be4944cb33181d4ceeae8623a30415f1037338
Ben Burton <benjamin.joseph.burton@gmail.com>
2015-10-08 15:20:15 -0400

    Toggleable session service implementations

--- app/org/corespring/wiring/sessiondb/SessionServiceFactoryImpl.scala
+++ app/org/corespring/wiring/sessiondb/SessionServiceFactoryImpl.scala
@@ -1,10 +1,27 @@
 package org.corespring.wiring.sessiondb
 
-import org.corespring.v2.sessiondb.webservice.RemoteSessionServiceFactory
+package org.corespring.wiring.sessiondb
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
+import com.mongodb.casbah.MongoDB
+import common.db.Db
+import org.corespring.common.config.AppConfig
 import org.corespring.v2.sessiondb.{ SessionService, SessionServiceFactory }
+import org.corespring.v2.sessiondb.dynamo.DynamoSessionServiceFactory
+import org.corespring.v2.sessiondb.mongo.MongoSessionServiceFactory
 
 object SessionServiceFactoryImpl extends SessionServiceFactory {
 
-  def create(tableName: String): SessionService = RemoteSessionServiceFactory.create(tableName)
+  private lazy val impl = {
+    AppConfig.sessionService match {
+      case "remote" => new RemoteSessionServiceFactory
+      case _ => new MongoSessionServiceFactory {
+        import play.api.Play.current
+        override protected val db: MongoDB = Db.salatDb()
+      }
+    }
+  }
+
+  def create(tableName: String): SessionService = impl.create(tableName)
 
 }
