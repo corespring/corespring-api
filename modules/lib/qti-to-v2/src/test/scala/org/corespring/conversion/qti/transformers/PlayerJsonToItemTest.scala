967e360d46638a5b1fd8d05d19496f14b602b514
edeustace <ed.eustace@gmail.com>
2015-11-13 22:56:38 +0000

    start fleshing out the tests

--- modules/lib/qti-to-v2/src/test/scala/org/corespring/conversion/qti/transformers/PlayerJsonToItemTest.scala
+++ modules/lib/qti-to-v2/src/test/scala/org/corespring/conversion/qti/transformers/PlayerJsonToItemTest.scala
@@ -1,15 +1,34 @@
 package org.corespring.conversion.qti.transformers
 
+import org.bson.types.ObjectId
+import org.corespring.models.item.{ PlayerDefinition, Item }
 import org.specs2.mutable.Specification
+import play.api.libs.json.Json
 
 class PlayerJsonToItemTest extends Specification {
+
+  val item = Item(collectionId = ObjectId.get.toString)
 
   "wholeItem" should {
     "convert" in pending
   }
 
   "playerDef" should {
-    "convert" in pending
+    import PlayerJsonToItem.playerDef
+    implicit val pdf = org.corespring.models.json.item.PlayerDefinitionFormat
+    val json = Json.obj(
+      "xhtml" -> "xhtml",
+      "summaryFeedback" -> "summaryFeedback",
+      "customScoring" -> "customScoring",
+      "components" -> Json.obj("1" -> Json.obj("componenType" -> "blah")))
+
+    "not convert if xhtml is empty" in {
+      playerDef(item, json - "xhtml") must_== item
+    }
+
+    "convert the json" in {
+      playerDef(item, json).playerDefinition.get must_== json.as[PlayerDefinition]
+    }
   }
 
   "profile" should {
