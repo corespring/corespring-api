ec5a651e1e92498d099e2a74d7a5a1ddd90f953e
rbokel <rbokel.github@qlod.org>
2015-12-09 16:10:51 +0100

    fix dep to container

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val playVersion = "2.2.1"
 
   //V2 Player
-  val containerVersion = "0.57.0-SNAPSHOT-PE-410"
+  val containerVersion = "0.57.0-SNAPSHOT"
   val qtiVersion = "0.8"
 
   def toModule(name: String) = "org.corespring" %% name % containerVersion
