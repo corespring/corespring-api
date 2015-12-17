97f84d50db4868efb11e062514c8ca3e1e3aecf3
Ben Burton <benjamin.joseph.burton@gmail.com>
2015-12-17 16:59:00 -0500

    Add test for PassageApi#get

---
+++ modules/web/v2-api/test/org/corespring/v2/api/PassageApiTest.scala
@@ -0,0 +1,96 @@
+package org.corespring.v2.api
+
+import org.bson.types.ObjectId
+import org.corespring.models.item.Passage
+import org.corespring.models.item.resource.{VirtualFile, BaseFile}
+import org.corespring.platform.data.mongo.models.VersionedId
+import org.corespring.services.PassageService
+import org.corespring.v2.auth.models.OrgAndOpts
+import org.corespring.v2.errors.Errors.generalError
+import org.corespring.v2.errors.V2Error
+import org.specs2.mock.Mockito
+import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
+import play.api.mvc.RequestHeader
+import play.api.test._
+import play.api.test.Helpers._
+
+import scala.concurrent.{ExecutionContext, Future}
+import scalaz.{Success, Validation, Failure}
+
+class PassageApiTest extends Specification with Mockito {
+
+  trait PassageApiScope extends Scope {
+    val passageService = mock[PassageService]
+    val v2ApiExecutionContext = V2ApiExecutionContext(ExecutionContext.global)
+    def authResponse: Validation[V2Error, OrgAndOpts] = Failure(generalError("Nope", UNAUTHORIZED))
+    lazy val getOrgAndOptionsFn: RequestHeader => Validation[V2Error, OrgAndOpts] = {
+      request => authResponse
+    }
+    val passageApi = new PassageApi(passageService, v2ApiExecutionContext, getOrgAndOptionsFn)
+    val passageId = new VersionedId[ObjectId](new ObjectId(), Some(0))
+    def file(): BaseFile = mock[VirtualFile]
+    lazy val passage = Passage(id = passageId, collectionId = new ObjectId().toString, file = file())
+  }
+
+  "get" should {
+
+    "should return 401" in new PassageApiScope {
+      val result = passageApi.get(passageId)(FakeRequest())
+      status(result) must be equalTo(UNAUTHORIZED)
+    }
+
+    "authorized request" should {
+
+      trait AuthorizedApiPassageScope extends PassageApiScope {
+        override def authResponse = Success(mock[OrgAndOpts])
+      }
+
+      "passage does not exist" should {
+
+        trait PassageMissingScope extends AuthorizedApiPassageScope {
+          passageService.get(passageId) returns Future.successful(None)
+        }
+
+        "return 404" in new PassageMissingScope {
+          val result = passageApi.get(passageId)(FakeRequest())
+          status(result) must be equalTo(NOT_FOUND)
+        }
+
+      }
+
+      "passage does exist" should {
+
+        trait HtmlPassageFoundScope extends AuthorizedApiPassageScope {
+          val contentType = "text/html"
+          val content = "Hey, this is some <strong>cool, cool</strong> html I wrote."
+          override def file() = {
+            val mockFile = mock[VirtualFile]
+            mockFile.contentType returns contentType
+            mockFile.content returns content
+            mockFile
+          }
+
+          passageService.get(passageId) returns Future.successful(Some(passage))
+          val result = passageApi.get(passageId)(FakeRequest())
+        }
+
+        "return 200" in new HtmlPassageFoundScope {
+          status(result) must be equalTo(OK)
+        }
+
+        "return Content-Type header from Passage file's contentType" in new HtmlPassageFoundScope {
+          header("Content-Type", result) must be equalTo(Some(contentType))
+        }
+
+        "return content from Passage file's content" in new HtmlPassageFoundScope {
+          contentAsString(result) must be equalTo(content)
+        }
+
+      }
+
+    }
+
+  }
+
+}
