4c64980ea38a582c65c9e5f7c6d4e32b8d57622b
Ed Eustace <ed.eustace@gmail.com>
2015-12-18 09:27:14 +0000

    rm async services - update calls in place instead - fix ItemApiClone test

--- modules/web/v2-api/test/org/corespring/v2/api/ItemApiCloneTest.scala
+++ modules/web/v2-api/test/org/corespring/v2/api/ItemApiCloneTest.scala
@@ -1,6 +1,7 @@
 package org.corespring.v2.api
 
 import org.bson.types.ObjectId
+import org.corespring.errors.PlatformServiceError
 import org.corespring.itemSearch.{ ItemIndexQuery, ItemIndexSearchResult }
 import org.corespring.models.item.Item
 import org.corespring.platform.data.mongo.models.VersionedId
@@ -8,7 +9,7 @@ import org.corespring.v2.auth.models.{ AuthMode, OrgAndOpts }
 import org.corespring.v2.errors.Errors.{ cantParseItemId, generalError }
 import org.corespring.v2.errors.V2Error
 import play.api.libs.json.Json
-import play.api.mvc.{AnyContent, AnyContentAsJson}
+import play.api.mvc.{Request, AnyContent, AnyContentAsJson}
 import play.api.test.{FakeHeaders, FakeRequest}
 
 import scala.concurrent._
@@ -19,6 +20,9 @@ class ItemApiCloneTest extends ItemApiSpec {
   import ExecutionContext.Implicits.global
 
   "ItemApi" should {
+
+    val req = FakeRequest("", "")
+
     "when calling clone" should {
 
       lazy val orgAndOpts = mockOrgAndOpts(AuthMode.UserSession)
@@ -30,53 +34,51 @@ class ItemApiCloneTest extends ItemApiSpec {
 
       case class ItemApiCloneScope(vid: String = vid.toString,
         id: Validation[V2Error, OrgAndOpts] = Success(orgAndOpts),
-        itemAuthLoadsItem: Boolean = true,
         itemServiceClones: Boolean = true,
         item: Option[Item] = Some(clonedItem)) extends ItemApiScope {
 
-        itemService.clone(any[Item]) returns (if (itemServiceClones) item else None)
+        cloneItemService.cloneItem(any[VersionedId[ObjectId]], any[ObjectId], any[Option[ObjectId]]) returns {
+          if (itemServiceClones) Success(clonedId) else Failure(PlatformServiceError("cloneItem failed"))
+        }
 
         itemIndexService.search(any[ItemIndexQuery]) returns future { Success(ItemIndexSearchResult.empty) }
 
-        itemAuth.loadForRead(anyString)(any[OrgAndOpts]) returns {
-          import scalaz.Scalaz._
-          val out = if (itemAuthLoadsItem) item else None
-          out.toSuccess(generalError("Test error: itemAuth.loadForRead"))
-        }
-
         override val orgAndOpts: Validation[V2Error, OrgAndOpts] = id
 
-        lazy val result = api.cloneItem(vid.toString)
+        def result(r:Request[AnyContent]) = api.cloneItem(vid.toString)(r)
       }
 
       "return 200" in new ItemApiCloneScope {
-        status(result) === OK
+        status(result(req)) === OK
       }
 
       s"return $BAD_REQUEST if a bad collection id is passed in" in new ItemApiCloneScope(
-        request = FakeRequest("", "", FakeHeaders(), AnyContentAsJson(Json.obj("collectionId" -> "bad-id")))
       ){
-        status(result) must_== BAD_REQUEST
+        val request = FakeRequest("", "", FakeHeaders(), AnyContentAsJson(Json.obj("collectionId" -> "bad-id")))
+        val r = result(request)
+        status(r) must_== BAD_REQUEST
       }
 
       "return the cloned id" in new ItemApiCloneScope {
-        (contentAsJson(result) \ "id").as[String] === clonedId.toString
+        (contentAsJson(result(req)) \ "id").as[String] === clonedId.toString
       }
 
       val testError = generalError("test error")
 
       "fail if there's no identity" in new ItemApiCloneScope(id = Failure(testError)) {
-        status(result) === testError.statusCode
+        status(result(req)) === testError.statusCode
       }
 
       "fail if the item id is unparseable" in new ItemApiCloneScope(vid = "?") {
-        status(result) === testError.statusCode
-        (contentAsJson(result) \ "message").as[String] === cantParseItemId("?").message
+        val r = result(req)
+        status(r) === testError.statusCode
+        (contentAsJson(r) \ "message").as[String] === cantParseItemId("?").message
       }
 
       "fail if clone fails" in new ItemApiCloneScope(itemServiceClones = false) {
-        status(result) === testError.statusCode
-        val json = contentAsJson(result)
+        val r = result(req)
+        status(r) === testError.statusCode
+        val json = contentAsJson(r)
         (json \ "message").as[String] must contain("Error cloning")
       }
     }
