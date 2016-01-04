0477cf9e2937f1bbdd543907689f2345738cf987
Ed Eustace <ed.eustace@gmail.com>
2016-01-04 16:34:39 +0000

    add note about NewProcfile

--- project/Tgz.scala
+++ project/Tgz.scala
@@ -8,8 +8,8 @@ import com.typesafe.sbt.packager.MappingsHelper.directory
 object Tgz{
   val settings = Seq(
     topLevelDirectory := None,
-    //Use NewProcfile to allow the old build to still function
     mappings in Universal += file(".env") -> ".env",
+    //Use NewProcfile to allow the old build to still function - point to main Procfile once we no longer use cs-builder + `play stage`.
     mappings in Universal += file("NewProcfile") -> "Procfile",
     mappings in Universal ++= directory("corespring-components/components").map({case (f,p) => f -> s"corespring-components/$p"}))
 }
