5eac7bcfb23cacc95a827b647afa9e8b7ff3dda9
edeustace <ed.eustace@gmail.com>
2016-01-13 21:12:26 +0000

    add some more tests

--- modules/web/v2-api/test/org/corespring/v2/api/OrganizationApiTest.scala
+++ modules/web/v2-api/test/org/corespring/v2/api/OrganizationApiTest.scala
@@ -1,11 +1,14 @@
 package org.corespring.v2.api
 
 import org.bson.types.ObjectId
+import org.corespring.models.{ ContentCollRef, Organization }
+import org.corespring.models.auth.Permission
 import org.corespring.services.OrgCollectionService
 import org.corespring.v2.auth.models.{ AuthMode, MockFactory }
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
+import play.api.libs.json.{ JsValue, Json }
 import play.api.mvc.RequestHeader
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
@@ -19,10 +22,17 @@ class OrganizationApiTest extends Specification with MockFactory with Mockito {
 
     val collectionId = ObjectId.get
 
-    lazy val orgAndOptsResult = Success(mockOrgAndOpts(AuthMode.UserSession))
+    lazy val mockedOrgAndOpts = mockOrgAndOpts(AuthMode.UserSession)
+    lazy val orgAndOptsResult = Success(mockedOrgAndOpts)
 
-    val orgCollectionService = {
+    protected def mkOrg(id: ObjectId, p: Permission) = {
+      Organization("test-org", contentcolls = Seq(ContentCollRef(id, p.value, true)))
+    }
+
+    lazy val orgCollectionService = {
       val m = mock[OrgCollectionService]
+      m.ownsCollection(any[Organization], any[ObjectId]) returns Success(true)
+      m.getOrgsWithAccessTo(any[ObjectId]) returns Stream.empty
       m
     }
 
@@ -40,9 +50,28 @@ class OrganizationApiTest extends Specification with MockFactory with Mockito {
 
   "getOrgsWithSharedCollection" should {
 
-    "return a list of orgs that have the collection shared with them" in new scope {
+    "return an empty list of orgs that have the collection shared with them" in new scope {
       val result = api.getOrgsWithSharedCollection(collectionId)(FakeRequest())
-      status(result) must_== OK
+      contentAsJson(result) must_== Json.arr()
+    }
+
+    trait withTwoOrgs extends scope {
+      val orgWithWrite = mkOrg(collectionId, Permission.Write)
+      orgCollectionService.getOrgsWithAccessTo(any[ObjectId]) returns Stream(orgWithWrite, mockedOrgAndOpts.org)
+      val result = api.getOrgsWithSharedCollection(collectionId)(FakeRequest())
+      val json = contentAsJson(result)
+    }
+
+    "not return the org that made the request" in new withTwoOrgs {
+      json.as[Seq[JsValue]].size must_== 1
+    }
+
+    "return json listing 1 org that has Write permission" in new withTwoOrgs {
+      json must_== Json.arr(
+        Json.obj(
+          "name" -> orgWithWrite.name,
+          "id" -> orgWithWrite.id.toString,
+          "permission" -> Permission.Write.name))
     }
   }
 }
