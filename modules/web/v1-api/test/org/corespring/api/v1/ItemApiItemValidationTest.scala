ad007921931792abb96502a9150f531930611817
edeustace <ed.eustace@gmail.com>
2016-01-21 19:37:54 +0000

    fix unreliable test 2 items with potentially different timestamps

--- modules/web/v1-api/test/org/corespring/api/v1/ItemApiItemValidationTest.scala
+++ modules/web/v1-api/test/org/corespring/api/v1/ItemApiItemValidationTest.scala
@@ -2,7 +2,7 @@ package org.corespring.api.v1
 
 import org.bson.types.ObjectId
 import org.corespring.models.item.Item
-import org.corespring.models.item.resource.{BaseFile, VirtualFile, Resource, StoredFile}
+import org.corespring.models.item.resource.{ BaseFile, VirtualFile, Resource, StoredFile }
 import org.corespring.platform.data.mongo.models.VersionedId
 import org.corespring.test.JsonAssertions
 import org.specs2.mock.Mockito
@@ -31,8 +31,7 @@ class ItemApiItemValidationTest
     name = "qti.xml",
     contentType = BaseFile.ContentTypes.XML,
     isMain = true,
-    content = "<qti></qti>"
-  )
+    content = "<qti></qti>")
 
   val dbItem = Item(
     collectionId = collectionId.toString,
@@ -50,15 +49,15 @@ class ItemApiItemValidationTest
 
       "insert storageKeys from db item into data.files" in new scope {
 
+        val initialResource = Resource(name = "data", files = Seq(storedFileWithoutStorageKey, virtualFile))
+        val expectedResource = initialResource.copy(files = Seq(storedFile, virtualFile))
+
         val item = Item(
           collectionId = collectionId.toString,
           id = itemId,
-          data = Some(Resource(name = "data", files = Seq(storedFileWithoutStorageKey, virtualFile))))
+          data = Some(initialResource))
 
-        val expected = Item(
-          collectionId = collectionId.toString,
-          id = itemId,
-          data = Some(Resource(name = "data", files = Seq(storedFile, virtualFile))))
+        val expected = item.copy(data = Some(expectedResource))
 
         sut.validateItem(dbItem, item) must_== Success(expected)
       }
