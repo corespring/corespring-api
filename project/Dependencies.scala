832a1bce99bfac6e26708113c683ca2af452b81e
edeustace <ed.eustace@gmail.com>
2016-06-15 16:09:38 +0100

    lock down elastic search play ws

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -82,7 +82,7 @@ object Dependencies {
   val sprayCaching = "io.spray" %% "spray-caching" % "1.3.1"
   val simplecsv = "net.quux00.simplecsv" % "simplecsv" % "1.0"
   val jsonValidator = "com.github.fge" % "json-schema-validator" % "2.2.4"
-  val elasticsearchPlayWS = ("org.corespring" %% "elasticsearch-play-ws" % "0.0.24-PLAY22-SNAPSHOT").exclude("org.mongodb", "mongo-java-driver")
+  val elasticsearchPlayWS = ("org.corespring" %% "elasticsearch-play-ws" % "0.0.24-PLAY22").exclude("org.mongodb", "mongo-java-driver")
   val jsoup = "org.jsoup" % "jsoup" % "1.8.1"
   val sessionServiceClient = "org.corespring" %% "session-service-client" % "0.4"
 
