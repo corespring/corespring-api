0e4fc56b160381eed1861ada38deb78d793daf14
benka <andrew.benkoczi@gmail.com>
2016-07-26 12:43:41 -0400

    bump versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val playVersion = "2.2.1"
 
   //V2 Player
-  val containerVersion = "3.4.0-SNAPSHOT"
+  val containerVersion = "4.1.0-SNAPSHOT"
   val qtiVersion = "0.16"
 
   def toModule(name: String) = "org.corespring" %% name % containerVersion
