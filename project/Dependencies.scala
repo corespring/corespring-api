eca5fd80bd2719bb57160053b7e31d64b5bdf2d3
benka <andrew.benkoczi@gmail.com>
2016-07-27 11:30:53 -0400

    target non-snapshot container

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val playVersion = "2.2.1"
 
   //V2 Player
-  val containerVersion = "4.0.0-SNAPSHOT"
+  val containerVersion = "4.0.0"
   val qtiVersion = "0.16"
 
   def toModule(name: String) = "org.corespring" %% name % containerVersion
