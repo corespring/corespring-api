1fa4614e47eae198d69fd28603dece49ba4e1aa8
edeustace <ed.eustace@gmail.com>
2019-04-24 21:32:16 +0100

    cdn resolver stuff

--- modules/lib/v2-player-integration/src/main/scala/org/corespring/v2/player/cdn/CdnItemAssetResolver.scala
+++ modules/lib/v2-player-integration/src/main/scala/org/corespring/v2/player/cdn/CdnItemAssetResolver.scala
@@ -3,7 +3,7 @@ package org.corespring.v2.player.cdn
 import org.corespring.common.url.EncodingHelper
 import play.api.Logger
 import play.api.mvc.RequestHeader
-
+import org.corespring.macros.DescribeMacro.describe
 class CdnItemAssetResolver(cdnResolver: CdnResolver) extends ItemAssetResolver {
 
   private val logger = Logger(classOf[CdnItemAssetResolver])
@@ -11,13 +11,14 @@ class CdnItemAssetResolver(cdnResolver: CdnResolver) extends ItemAssetResolver {
   val helper = new EncodingHelper
 
   override def bypass(request: RequestHeader): Boolean = {
+    logger.info(describe(request))
     val ua = request.headers.toMap.get("User-Agent").map(s => s.mkString)
-    logger.info(s"[bypass] user agent: ${ua}")
+    logger.info(describe(ua))
     ua == "Amazon CloudFront"
   }
 
   override def resolve(itemId: String)(file: String): String = {
-    logger.info(s"[bypass] user agent: ${itemId}")
+    logger.info(describe(itemId))
     cdnResolver.resolveDomain(mkPath(itemId)(helper.encodedOnce(file)))
   }
 
